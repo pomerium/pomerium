25d76cd5c078d2203383dafbefdbbd35482ac46b
Bobby DeSimone <desimone@users.noreply.github.com>
2019-05-03 20:48:26 -0700

    proxy: make http redirect server configurable (#105)

--- internal/urlutil/url.go
+++ internal/urlutil/url.go
@@ -2,6 +2,11 @@ package urlutil // import "github.com/pomerium/pomerium/internal/urlutil"
 
 import "strings"
 
+// StripPort returns a host, without any port number.
+//
+// If Host is an IPv6 literal with a port number, Hostname returns the
+// IPv6 literal without the square brackets. IPv6 literals may include
+// a zone identifier.
 func StripPort(hostport string) string {
 	colon := strings.IndexByte(hostport, ':')
 	if colon == -1 {
