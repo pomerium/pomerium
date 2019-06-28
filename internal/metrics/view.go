6b9f0d3f57cbf9738f4db20c5352e5cbcc1ea157
Travis Groth <travis@netfoo.org>
2019-06-28 06:15:16 -0400

    Error logging

--- internal/metrics/view.go
+++ internal/metrics/view.go
@@ -1,23 +1,30 @@
 package metrics
 
 import (
+	"github.com/pomerium/pomerium/internal/log"
 	"go.opencensus.io/stats/view"
 )
 
 // RegisterHTTPClientView registers the standard HTTPClient view.
 // It must be called to see metrics in the configured exporters
 func RegisterHTTPClientView() {
-	view.Register(HTTPClientRequestCountView, HTTPClientRequestDurationView, HTTPClientRequestSizeView)
+	if err := view.Register(HTTPClientRequestCountView, HTTPClientRequestDurationView, HTTPClientRequestSizeView); err != nil {
+		log.Warn().Err(err).Msg("Could not register HTTPClientView")
+	}
 }
 
 // RegisterHTTPServerView registers the standard HTTPServer view.
 // It must be called to see metrics in the configured exporters
 func RegisterHTTPServerView() {
-	view.Register(HTTPServerRequestCountView, HTTPServerRequestDurationView, HTTPServerRequestSizeView)
+	if err := view.Register(HTTPServerRequestCountView, HTTPServerRequestDurationView, HTTPServerRequestSizeView); err != nil {
+		log.Warn().Err(err).Msg("Could not register HTTPServerView")
+	}
 }
 
 // RegisterGRPCClientView registers the standard GRPCClient view.
 // It must be called to see metrics in the configured exporters
 func RegisterGRPCClientView() {
-	view.Register(GRPCClientRequestCountView, GRPCClientRequestDurationView, GRPCClientResponseSizeView)
+	if err := view.Register(GRPCClientRequestCountView, GRPCClientRequestDurationView, GRPCClientResponseSizeView); err != nil {
+		log.Warn().Err(err).Msg("Could not register GRPCClientView")
+	}
 }
