3eb18c3468961a4e688041b1d18f95cfe07610a2
Travis Groth <travisgroth@users.noreply.github.com>
2019-08-14 20:38:39 -0400

    Remove race condition in tests

--- proxy/clients/clients_test.go
+++ proxy/clients/clients_test.go
@@ -13,22 +13,21 @@ func Test_grpcTimeoutInterceptor(t *testing.T) {
 	mockInvoker := func(sleepTime time.Duration, wantFail bool) grpc.UnaryInvoker {
 		return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
 			time.Sleep(sleepTime)
-			select {
-			case <-ctx.Done():
-				if !wantFail {
-					t.Error("Deadline should not have been exceeded")
-				}
-				return nil
-			default:
-				if wantFail {
-					t.Error("Deadline not exceeded but should have been")
-				}
+			deadline, ok := ctx.Deadline()
+			if !ok {
+				t.Fatal("No deadline set")
+			}
+
+			if ok && time.Now().After(deadline) && !wantFail {
+				t.Error("Deadline exceeded, but should not have")
+			} else if time.Now().Before(deadline) && wantFail {
+				t.Error("Deadline not exceeded, but should have")
 			}
 			return nil
 		}
 	}
 
-	timeOut := 5 * time.Millisecond
+	timeOut := 10 * time.Millisecond
 	to := grpcTimeoutInterceptor(timeOut)
 
 	to(context.Background(), "test", nil, nil, nil, mockInvoker(timeOut*2, true))
