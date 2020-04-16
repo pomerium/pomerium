5ad0e0ebdc8e80ba374642ef7b8f1282f3b8641d
Caleb Doxsey <caleb@doxsey.net>
2020-04-16 14:53:39 -0600

    authorize: build full URL from gRPC request

--- authorize/grpc_test.go
+++ authorize/grpc_test.go
@@ -47,3 +47,19 @@ func TestAuthorize_IsAuthorized(t *testing.T) {
 		})
 	}
 }
+
+func Test_getFullURL(t *testing.T) {
+	tests := []struct {
+		rawurl, host, expect string
+	}{
+		{"https://www.example.com/admin", "", "https://www.example.com/admin"},
+		{"https://www.example.com/admin", "example.com", "https://www.example.com/admin"},
+		{"/admin", "example.com", "http://example.com/admin"},
+	}
+	for _, tt := range tests {
+		actual := getFullURL(tt.rawurl, tt.host)
+		if actual != tt.expect {
+			t.Errorf("expected getFullURL(%s, %s) to be %s, but got %s", tt.rawurl, tt.host, tt.expect, actual)
+		}
+	}
+}
