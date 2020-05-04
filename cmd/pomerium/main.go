fb97bd1a6d26a50ecd3cd094958b4861aa495400
Caleb Doxsey <cdoxsey@pomerium.com>
2020-05-04 12:52:44 -0600

    envoy: update authenticate, main

--- cmd/pomerium/main.go
+++ cmd/pomerium/main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"net/http"
+	"net"
 	"sync"
-	"time"
 
 	"github.com/pomerium/pomerium/authenticate"
 	"github.com/pomerium/pomerium/authorize"
 	"github.com/pomerium/pomerium/cache"
 	"github.com/pomerium/pomerium/config"
-	"github.com/pomerium/pomerium/internal/frontend"
-	pgrpc "github.com/pomerium/pomerium/internal/grpc"
+	"github.com/pomerium/pomerium/internal/controlplane"
+	"github.com/pomerium/pomerium/internal/envoy"
 	pbAuthorize "github.com/pomerium/pomerium/internal/grpc/authorize"
 	pbCache "github.com/pomerium/pomerium/internal/grpc/cache"
 	"github.com/pomerium/pomerium/internal/httputil"
 	"github.com/pomerium/pomerium/internal/log"
-	"github.com/pomerium/pomerium/internal/middleware"
 	"github.com/pomerium/pomerium/internal/telemetry/metrics"
 	"github.com/pomerium/pomerium/internal/telemetry/trace"
 	"github.com/pomerium/pomerium/internal/urlutil"
@@ -26,9 +25,7 @@ import (
 
 	envoy_service_auth_v2 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v2"
 	"github.com/fsnotify/fsnotify"
-	"github.com/gorilla/mux"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
+	"golang.org/x/sync/errgroup"
 )
 
 var versionFlag = flag.Bool("version", false, "prints the version")
@@ -50,8 +47,10 @@ func run() error {
 	if err != nil {
 		return err
 	}
+	var optionsUpdaters []config.OptionsUpdater
+
 	log.Info().Str("version", version.FullVersion()).Msg("cmd/pomerium")
-	// since we can have multiple listeners, we create a wait group
+
 	var wg sync.WaitGroup
 	if err := setupMetrics(opt, &wg); err != nil {
 		return err
@@ -63,154 +62,94 @@ func run() error {
 		return err
 	}
 
-	r := newGlobalRouter(opt)
-	_, err = newAuthenticateService(*opt, r)
+	ctx := context.Background()
+
+	// setup the control plane
+	controlPlane, err := controlplane.NewServer()
 	if err != nil {
-		return err
+		return fmt.Errorf("error creating control plane: %w", err)
 	}
-	authz, err := newAuthorizeService(*opt)
+	optionsUpdaters = append(optionsUpdaters, controlPlane)
+	err = controlPlane.UpdateOptions(*opt)
 	if err != nil {
-		return err
+		return fmt.Errorf("error updating control plane options: %w", err)
 	}
 
-	cacheSvc, err := newCacheService(*opt)
+	_, grpcPort, _ := net.SplitHostPort(controlPlane.GRPCListener.Addr().String())
+	_, httpPort, _ := net.SplitHostPort(controlPlane.HTTPListener.Addr().String())
+
+	//
+	envoyServer, err := envoy.NewServer(grpcPort, httpPort)
 	if err != nil {
-		return err
-	}
-	if cacheSvc != nil {
-		defer cacheSvc.Close()
+		return fmt.Errorf("error creating envoy server")
 	}
 
-	proxy, err := newProxyService(*opt, r)
-	if err != nil {
-		return err
-	}
-	if proxy != nil {
-		defer proxy.AuthorizeClient.Close()
+	// add services
+	if config.IsAuthenticate(opt.Services) {
+		svc, err := authenticate.New(*opt)
+		if err != nil {
+			return fmt.Errorf("error creating authenticate service: %w", err)
+		}
+		host := urlutil.StripPort(opt.AuthenticateURL.Host)
+		sr := controlPlane.HTTPRouter.Host(host).Subrouter()
+		svc.Mount(sr)
+		log.Info().Str("host", host).Msg("enabled authenticate service")
 	}
 
+	if config.IsAuthorize(opt.Services) {
+		svc, err := authorize.New(*opt)
+		if err != nil {
+			return fmt.Errorf("error creating authorize service: %w", err)
+		}
+		pbAuthorize.RegisterAuthorizerServer(controlPlane.GRPCServer, svc)
+		envoy_service_auth_v2.RegisterAuthorizationServer(controlPlane.GRPCServer, svc)
+
+		log.Info().Msg("enabled authorize service")
+
+		optionsUpdaters = append(optionsUpdaters, svc)
+		err = svc.UpdateOptions(*opt)
+		if err != nil {
+			return fmt.Errorf("error updating authorize options: %w", err)
+		}
+	}
+
+	if config.IsCache(opt.Services) {
+		svc, err := cache.New(*opt)
+		if err != nil {
+			return fmt.Errorf("error creating config service: %w", err)
+		}
+		defer svc.Close()
+		pbCache.RegisterCacheServer(controlPlane.GRPCServer, svc)
+		log.Info().Msg("enabled cache service")
+	}
+
+	if config.IsProxy(opt.Services) {
+		svc, err := proxy.New(*opt)
+		if err != nil {
+			return fmt.Errorf("error creating proxy service: %w", err)
+		}
+		controlPlane.HTTPRouter.PathPrefix("/").Handler(svc)
+	}
+
+	// start the config change listener
 	opt.OnConfigChange(func(e fsnotify.Event) {
 		log.Info().Str("file", e.Name).Msg("cmd/pomerium: config file changed")
-		opt = config.HandleConfigUpdate(*configFile, opt, []config.OptionsUpdater{authz, proxy})
+		opt = config.HandleConfigUpdate(*configFile, opt, optionsUpdaters)
 	})
 
-	if err := newGRPCServer(*opt, authz, cacheSvc, &wg); err != nil {
-		return err
-	}
-
-	srv, err := httputil.NewServer(httpServerOptions(opt), r, &wg)
-	if err != nil {
-		return err
-	}
-	go httputil.Shutdown(srv)
-	// Blocks and waits until ALL WaitGroup members have signaled completion
-	wg.Wait()
-	return nil
-}
-
-func newAuthenticateService(opt config.Options, r *mux.Router) (*authenticate.Authenticate, error) {
-	if !config.IsAuthenticate(opt.Services) {
-		return nil, nil
-	}
-	service, err := authenticate.New(opt)
-	if err != nil {
-		return nil, err
-	}
-	sr := r.Host(urlutil.StripPort(opt.AuthenticateURL.Host)).Subrouter()
-	sr.PathPrefix("/").Handler(service.Handler())
-
-	return service, nil
-}
-
-func newAuthorizeService(opt config.Options) (*authorize.Authorize, error) {
-	if !config.IsAuthorize(opt.Services) {
-		return nil, nil
-	}
-	return authorize.New(opt)
-}
-
-func newCacheService(opt config.Options) (*cache.Cache, error) {
-	if !config.IsCache(opt.Services) {
-		return nil, nil
-	}
-	return cache.New(opt)
-}
-
-func newGRPCServer(opt config.Options, as *authorize.Authorize, cs *cache.Cache, wg *sync.WaitGroup) error {
-	if as == nil && cs == nil {
+	// run everything
+	eg, ctx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		wg.Wait()
 		return nil
-	}
-	regFn := func(s *grpc.Server) {
-		if as != nil {
-			pbAuthorize.RegisterAuthorizerServer(s, as)
-			envoy_service_auth_v2.RegisterAuthorizationServer(s, as)
-		}
-		if cs != nil {
-			pbCache.RegisterCacheServer(s, cs)
-
-		}
-	}
-	so := &pgrpc.ServerOptions{
-		Addr:        opt.GRPCAddr,
-		ServiceName: opt.Services,
-		KeepaliveParams: keepalive.ServerParameters{
-			MaxConnectionAge:      opt.GRPCServerMaxConnectionAge,
-			MaxConnectionAgeGrace: opt.GRPCServerMaxConnectionAgeGrace,
-		},
-	}
-	if !opt.GRPCInsecure {
-		so.TLSCertificate = opt.TLSCertificate
-	}
-	grpcSrv, err := pgrpc.NewServer(so, regFn, wg)
-	if err != nil {
-		return err
-	}
-	go pgrpc.Shutdown(grpcSrv)
-	return nil
-}
-
-func newProxyService(opt config.Options, r *mux.Router) (*proxy.Proxy, error) {
-	if !config.IsProxy(opt.Services) {
-		return nil, nil
-	}
-	service, err := proxy.New(opt)
-	if err != nil {
-		return nil, err
-	}
-	r.PathPrefix("/").Handler(service)
-	return service, nil
-}
-
-func newGlobalRouter(o *config.Options) *mux.Router {
-	mux := httputil.NewRouter()
-	mux.SkipClean(true)
-	mux.Use(metrics.HTTPMetricsHandler(o.Services))
-	mux.Use(log.NewHandler(log.Logger))
-	mux.Use(log.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
-		log.FromRequest(r).Debug().
-			Dur("duration", duration).
-			Int("size", size).
-			Int("status", status).
-			Str("method", r.Method).
-			Str("service", o.Services).
-			Str("host", r.Host).
-			Str("path", r.URL.String()).
-			Msg("http-request")
-	}))
-	if len(o.Headers) != 0 {
-		mux.Use(middleware.SetHeaders(o.Headers))
-	}
-	mux.Use(log.HeadersHandler(httputil.HeadersXForwarded))
-	mux.Use(log.RemoteAddrHandler("ip"))
-	mux.Use(log.UserAgentHandler("user_agent"))
-	mux.Use(log.RefererHandler("referer"))
-	mux.Use(log.RequestIDHandler("req_id", "Request-Id"))
-	mux.Use(middleware.Healthcheck("/ping", version.UserAgent()))
-	mux.HandleFunc("/healthz", httputil.HealthCheck)
-	mux.HandleFunc("/ping", httputil.HealthCheck)
-	mux.PathPrefix("/.pomerium/assets/").Handler(http.StripPrefix("/.pomerium/assets/", frontend.MustAssetHandler()))
-
-	return mux
+	})
+	eg.Go(func() error {
+		return controlPlane.Run(ctx)
+	})
+	eg.Go(func() error {
+		return envoyServer.Run(ctx)
+	})
+	return eg.Wait()
 }
 
 func setupMetrics(opt *config.Options, wg *sync.WaitGroup) error {
