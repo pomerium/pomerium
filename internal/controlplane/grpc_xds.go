831d1dd58b28e08e37200832bc2e61f58c5ea0f5
Caleb Doxsey <cdoxsey@pomerium.com>
2020-05-05 12:42:59 -0600

    envoy: breakup streaming method into steps

--- internal/controlplane/grpc_xds.go
+++ internal/controlplane/grpc_xds.go
@@ -1,6 +1,7 @@
 package controlplane
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -25,82 +26,107 @@ func (srv *Server) registerXDSHandlers() {
 //     and if any of them are different from the current version, we send
 //     the updated resource
 func (srv *Server) StreamAggregatedResources(stream envoy_service_discovery_v3.AggregatedDiscoveryService_StreamAggregatedResourcesServer) error {
-	versions := map[string]string{}
 	incoming := make(chan *envoy_service_discovery_v3.DiscoveryRequest)
 	outgoing := make(chan *envoy_service_discovery_v3.DiscoveryResponse)
 
 	eg, ctx := errgroup.WithContext(stream.Context())
 	// receive requests
 	eg.Go(func() error {
-		for {
-			req, err := stream.Recv()
-			if err != nil {
-				return err
-			}
-
-			select {
-			case incoming <- req:
-			case <-ctx.Done():
-				return ctx.Err()
-			}
-		}
+		return srv.streamAggregatedResourcesIncomingStep(ctx, stream, incoming)
 	})
 	eg.Go(func() error {
-		for {
-			select {
-			case req := <-incoming:
-				if req.ErrorDetail != nil {
-					bs, _ := json.Marshal(req.ErrorDetail.Details)
-					log.Error().
-						Err(errors.New(req.ErrorDetail.Message)).
-						Int32("code", req.ErrorDetail.Code).
-						RawJSON("details", bs).Msg("error applying configuration")
-					continue
-				}
-
-				// update the currently stored version
-				// if this version is different from the current version
-				// we will send the response below
-				versions[req.TypeUrl] = req.VersionInfo
-			case <-srv.configUpdated:
-			case <-ctx.Done():
-				return ctx.Err()
-			}
-
-			current := srv.currentConfig.Load().(versionedOptions)
-			for typeURL, version := range versions {
-				// the versions are different, so the envoy config needs to be updated
-				if version != fmt.Sprint(current.version) {
-					res, err := srv.buildDiscoveryResponse(fmt.Sprint(current.version), typeURL, current.Options)
-					if err != nil {
-						return err
-					}
-					select {
-					case outgoing <- res:
-					case <-ctx.Done():
-						return ctx.Err()
-					}
-				}
-			}
-		}
+		return srv.streamAggregatedResourcesProcessStep(ctx, incoming, outgoing)
 	})
 	// send responses
 	eg.Go(func() error {
-		for {
-			var res *envoy_service_discovery_v3.DiscoveryResponse
-			select {
-			case res = <-outgoing:
-			case <-ctx.Done():
-				return ctx.Err()
-			}
-
-			err := stream.Send(res)
-			if err != nil {
-				return err
-			}
-		}
+		return srv.streamAggregatedResourcesOutgoingStep(ctx, stream, outgoing)
 	})
 	return eg.Wait()
+}
+
+func (srv *Server) streamAggregatedResourcesIncomingStep(
+	ctx context.Context,
+	stream envoy_service_discovery_v3.AggregatedDiscoveryService_StreamAggregatedResourcesServer,
+	incoming chan<- *envoy_service_discovery_v3.DiscoveryRequest,
+) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+
+		select {
+		case incoming <- req:
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
+
+func (srv *Server) streamAggregatedResourcesProcessStep(
+	ctx context.Context,
+	incoming <-chan *envoy_service_discovery_v3.DiscoveryRequest,
+	outgoing chan<- *envoy_service_discovery_v3.DiscoveryResponse,
+) error {
+	versions := map[string]string{}
+
+	for {
+		select {
+		case req := <-incoming:
+			if req.ErrorDetail != nil {
+				bs, _ := json.Marshal(req.ErrorDetail.Details)
+				log.Error().
+					Err(errors.New(req.ErrorDetail.Message)).
+					Int32("code", req.ErrorDetail.Code).
+					RawJSON("details", bs).Msg("error applying configuration")
+				continue
+			}
+
+			// update the currently stored version
+			// if this version is different from the current version
+			// we will send the response below
+			versions[req.TypeUrl] = req.VersionInfo
+		case <-srv.configUpdated:
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+
+		current := srv.currentConfig.Load().(versionedOptions)
+		for typeURL, version := range versions {
+			// the versions are different, so the envoy config needs to be updated
+			if version != fmt.Sprint(current.version) {
+				res, err := srv.buildDiscoveryResponse(fmt.Sprint(current.version), typeURL, current.Options)
+				if err != nil {
+					return err
+				}
+				select {
+				case outgoing <- res:
+				case <-ctx.Done():
+					return ctx.Err()
+				}
+			}
+		}
+	}
+}
+
+func (srv *Server) streamAggregatedResourcesOutgoingStep(
+	ctx context.Context,
+	stream envoy_service_discovery_v3.AggregatedDiscoveryService_StreamAggregatedResourcesServer,
+	outgoing <-chan *envoy_service_discovery_v3.DiscoveryResponse,
+) error {
+	for {
+		var res *envoy_service_discovery_v3.DiscoveryResponse
+		select {
+		case res = <-outgoing:
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+
+		err := stream.Send(res)
+		if err != nil {
+			return err
+		}
+	}
 }
 
 // DeltaAggregatedResources is not implemented.
