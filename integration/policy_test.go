757fff5bd3bf3d4aff65a158cc28caf4f2b5f622
Caleb Doxsey <cdoxsey@pomerium.com>
2020-05-07 10:41:02 -0600

    integration-tests: add test for preserve_host_header

--- integration/policy_test.go
+++ integration/policy_test.go
@@ -55,6 +55,64 @@ func TestCORS(t *testing.T) {
 	})
 }
 
+func TestPreserveHostHeader(t *testing.T) {
+	ctx := mainCtx
+	ctx, clearTimeout := context.WithTimeout(ctx, time.Second*30)
+	defer clearTimeout()
+
+	t.Run("enabled", func(t *testing.T) {
+		client := testcluster.NewHTTPClient()
+
+		req, err := http.NewRequestWithContext(ctx, "GET", "https://httpdetails.localhost.pomerium.io/preserve-host-header-enabled", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		res, err := client.Do(req)
+		if !assert.NoError(t, err, "unexpected http error") {
+			return
+		}
+		defer res.Body.Close()
+
+		var result struct {
+			Headers map[string]string `json:"headers"`
+		}
+		err = json.NewDecoder(res.Body).Decode(&result)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		assert.Equal(t, "httpdetails.localhost.pomerium.io", result.Headers["host"],
+			"destination host should be preserved")
+	})
+	t.Run("disabled", func(t *testing.T) {
+		client := testcluster.NewHTTPClient()
+
+		req, err := http.NewRequestWithContext(ctx, "GET", "https://httpdetails.localhost.pomerium.io/preserve-host-header-disabled", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		res, err := client.Do(req)
+		if !assert.NoError(t, err, "unexpected http error") {
+			return
+		}
+		defer res.Body.Close()
+
+		var result struct {
+			Headers map[string]string `json:"headers"`
+		}
+		err = json.NewDecoder(res.Body).Decode(&result)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		assert.NotEqual(t, "httpdetails.localhost.pomerium.io", result.Headers["host"],
+			"destination host should not be preserved")
+	})
+
+}
+
 func TestSetRequestHeaders(t *testing.T) {
 	ctx := mainCtx
 	ctx, clearTimeout := context.WithTimeout(ctx, time.Second*30)
