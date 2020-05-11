e623d49fb2b4f50154e2f5eb9f017fc09f5b32d6
Caleb Doxsey <cdoxsey@pomerium.com>
2020-05-11 12:03:32 -0600

    proxy: remove pomerium cookie and authorization from upstream requests

--- internal/controlplane/xds_listeners.go
+++ internal/controlplane/xds_listeners.go
@@ -101,26 +101,11 @@ func (srv *Server) buildHTTPListener(options config.Options) *envoy_config_liste
 		},
 	})
 
-	luaConfig, _ := ptypes.MarshalAny(&envoy_extensions_filters_http_lua_v3.Lua{
-		InlineCode: `
-function envoy_on_request(request_handle)
-  local headers = request_handle:headers()
-  local dynamic_meta = request_handle:streamInfo():dynamicMetadata()
-  if headers:get("x-pomerium-set-cookie") ~= nil then
-    dynamic_meta:set("envoy.filters.http.lua", "pomerium_set_cookie", headers:get("x-pomerium-set-cookie"))
-    headers:remove("x-pomerium-set-cookie")
-  end
-end
-
-function envoy_on_response(response_handle)
-  local headers = response_handle:headers()
-  local dynamic_meta = response_handle:streamInfo():dynamicMetadata()
-  local tbl = dynamic_meta:get("envoy.filters.http.lua")
-  if tbl ~= nil and tbl["pomerium_set_cookie"] ~= nil then
-    headers:add("set-cookie", tbl["pomerium_set_cookie"])
-  end
-end
-`,
+	extAuthzSetCookieLua, _ := ptypes.MarshalAny(&envoy_extensions_filters_http_lua_v3.Lua{
+		InlineCode: luascripts.ExtAuthzSetCookie,
+	})
+	cleanUpstreamLua, _ := ptypes.MarshalAny(&envoy_extensions_filters_http_lua_v3.Lua{
+		InlineCode: luascripts.CleanUpstream,
 	})
 
 	tc, _ := ptypes.MarshalAny(&envoy_http_connection_manager.HttpConnectionManager{
@@ -142,7 +127,13 @@ end
 			{
 				Name: "envoy.filters.http.lua",
 				ConfigType: &envoy_http_connection_manager.HttpFilter_TypedConfig{
-					TypedConfig: luaConfig,
+					TypedConfig: extAuthzSetCookieLua,
+				},
+			},
+			{
+				Name: "envoy.filters.http.lua",
+				ConfigType: &envoy_http_connection_manager.HttpFilter_TypedConfig{
+					TypedConfig: cleanUpstreamLua,
 				},
 			},
 			{
