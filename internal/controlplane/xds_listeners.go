6f53731773223e5c6c002129280e87971cb9491f
Caleb Doxsey <cdoxsey@pomerium.com>
2020-05-21 13:45:52 -0600

    add TrustedCa to envoy config so that users are prompted for the correct client certificate

--- internal/controlplane/xds_listeners.go
+++ internal/controlplane/xds_listeners.go
@@ -1,6 +1,7 @@
 package controlplane
 
 import (
+	"encoding/base64"
 	"sort"
 	"time"
 
@@ -327,6 +328,17 @@ func (srv *Server) buildDownstreamTLSContext(options *config.Options, domain str
 		return nil
 	}
 
+	var trustedCA *envoy_config_core_v3.DataSource
+	if options.ClientCA != "" {
+		bs, err := base64.StdEncoding.DecodeString(options.ClientCA)
+		if err != nil {
+			log.Warn().Msg("client_ca does not appear to be a base64 encoded string")
+		}
+		trustedCA = inlineBytesAsFilename("client-ca", bs)
+	} else {
+		trustedCA = inlineFilename(options.ClientCAFile)
+	}
+
 	envoyCert := envoyTLSCertificateFromGoTLSCertificate(cert)
 	return &envoy_extensions_transport_sockets_tls_v3.DownstreamTlsContext{
 		CommonTlsContext: &envoy_extensions_transport_sockets_tls_v3.CommonTlsContext{
@@ -334,6 +346,7 @@ func (srv *Server) buildDownstreamTLSContext(options *config.Options, domain str
 			AlpnProtocols:   []string{"h2", "http/1.1"},
 			ValidationContextType: &envoy_extensions_transport_sockets_tls_v3.CommonTlsContext_ValidationContext{
 				ValidationContext: &envoy_extensions_transport_sockets_tls_v3.CertificateValidationContext{
+					TrustedCa:              trustedCA,
 					TrustChainVerification: envoy_extensions_transport_sockets_tls_v3.CertificateValidationContext_ACCEPT_UNTRUSTED,
 				},
 			},
