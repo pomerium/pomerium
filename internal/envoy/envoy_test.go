683084065f7d93fd6e8a5cb62d38e1c6c5588a02
Travis Groth <travisgroth@users.noreply.github.com>
2020-05-29 11:48:43 -0400

    testing

--- internal/envoy/envoy_test.go
+++ internal/envoy/envoy_test.go
@@ -5,6 +5,8 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/pomerium/pomerium/internal/testutil"
+
 	envoy_config_bootstrap_v3 "github.com/envoyproxy/go-control-plane/envoy/config/bootstrap/v3"
 	"github.com/golang/protobuf/proto"
 	"github.com/nsf/jsondiff"
@@ -64,6 +66,26 @@ func Test_addTraceConfig(t *testing.T) {
 
 			diff, diffStr := jsondiff.Compare([]byte(tt.want), jsonDump(t, baseCfg), &jsondiff.Options{})
 			assert.Equal(t, jsondiff.FullMatch, diff, fmt.Sprintf("%s: differences: %s", diff.String(), diffStr))
+		})
+	}
+}
+
+func Test_buildStatsConfig(t *testing.T) {
+	tests := []struct {
+		name string
+		opts *config.Options
+		want string
+	}{
+		{"all-in-one", &config.Options{Services: config.ServiceAll}, `{"statsTags":[{"tagName":"service","fixedValue":"pomerium"}]}`},
+		{"authorize", &config.Options{Services: config.ServiceAuthorize}, `{"statsTags":[{"tagName":"service","fixedValue":"pomerium-authorize"}]}`},
+		{"proxy", &config.Options{Services: config.ServiceProxy}, `{"statsTags":[{"tagName":"service","fixedValue":"pomerium-proxy"}]}`},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			srv := &Server{opts: tt.opts}
+
+			statsCfg := srv.buildStatsConfig()
+			testutil.AssertProtoJSONEqual(t, tt.want, statsCfg)
 		})
 	}
 }
