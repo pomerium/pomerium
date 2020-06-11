187b499098a929a41dcabd045762dae2b77003c7
Caleb Doxsey <cdoxsey@pomerium.com>
2020-06-11 13:41:56 -0600

    add azure provider

--- internal/directory/provider.go
+++ internal/directory/provider.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 
 	"github.com/pomerium/pomerium/config"
+	"github.com/pomerium/pomerium/internal/directory/azure"
 	"github.com/pomerium/pomerium/internal/directory/google"
 	"github.com/pomerium/pomerium/internal/directory/onelogin"
 	"github.com/pomerium/pomerium/internal/grpc/directory"
@@ -25,6 +26,18 @@ type Provider interface {
 // GetProvider gets the provider for the given options.
 func GetProvider(options *config.Options) Provider {
 	switch options.Provider {
+	case "azure":
+		serviceAccount, err := azure.ParseServiceAccount(options.ServiceAccount)
+		if err == nil {
+			return azure.New(azure.WithServiceAccount(serviceAccount))
+		}
+
+		log.Warn().
+			Str("service", "directory").
+			Str("provider", options.Provider).
+			Err(err).
+			Msg("invalid service account for azure directory provider")
+
 	case "google":
 		if options.ServiceAccount != "" {
 			return google.New(google.WithServiceAccount(options.ServiceAccount))
@@ -36,6 +49,7 @@ func GetProvider(options *config.Options) Provider {
 		}
 
 		log.Warn().
+			Str("service", "directory").
 			Str("provider", options.Provider).
 			Err(err).
 			Msg("invalid service account for onelogin directory provider")
