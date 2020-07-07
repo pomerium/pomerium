e295f083b6466557859c39b0a1752516f31d4ea7
Caleb Doxsey <cdoxsey@pomerium.com>
2020-07-07 14:39:21 -0600

    audit: add protobuf definitions

--- pkg/grpc/grpc.go
+++ pkg/grpc/grpc.go
@@ -4,3 +4,4 @@ package grpc
 //go:generate ../../scripts/protoc -I ./user/ --go_out=plugins=grpc:$GOPATH/src ./user/user.proto
 //go:generate ../../scripts/protoc -I ./databroker/ --go_out=plugins=grpc:$GOPATH/src ./databroker/databroker.proto
 //go:generate ../../scripts/protoc -I ./directory/ --go_out=plugins=grpc:$GOPATH/src ./directory/directory.proto
+//go:generate ../../scripts/protoc -I ./audit/ --go_out=plugins=grpc:$GOPATH/src ./audit/audit.proto
