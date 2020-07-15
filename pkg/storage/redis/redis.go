8a6fdc8dfe43f1488fcf09b365c3d8b3a269fb3d
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-07-15 22:54:34 +0700

    pkg/storage: add redis storage backend

---
+++ pkg/storage/redis/redis.go
@@ -0,0 +1,169 @@
+package redis
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"sync/atomic"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/gomodule/redigo/redis"
+	"google.golang.org/protobuf/types/known/anypb"
+
+	internal_databroker "github.com/pomerium/pomerium/internal/databroker"
+	"github.com/pomerium/pomerium/pkg/grpc/databroker"
+	"github.com/pomerium/pomerium/pkg/storage"
+)
+
+var _ storage.Backend = (*DB)(nil)
+
+// DB wraps redis conn to interact with redis server.
+type DB struct {
+	pool                   *redis.Pool
+	deletePermanentlyAfter int64
+	recordType             string
+	lastVersion            uint64
+}
+
+// New returns new DB instance.
+func New(address string) (*DB, error) {
+	db := &DB{
+		pool: &redis.Pool{
+			Wait: true,
+			DialContext: func(ctx context.Context) (redis.Conn, error) {
+				ctx, cancelFn := context.WithTimeout(ctx, 5*time.Second)
+				defer cancelFn()
+				c, err := redis.DialContext(ctx, "tcp", address)
+				if err != nil {
+					return nil, fmt.Errorf(`redis.DialURL(): %w`, err)
+				}
+				return c, nil
+			},
+			TestOnBorrow: func(c redis.Conn, t time.Time) error {
+				if time.Since(t) < time.Minute {
+					return nil
+				}
+				_, err := c.Do("PING")
+				if err != nil {
+					return fmt.Errorf(`c.Do("PING"): %w`, err)
+				}
+				return nil
+			},
+		},
+		deletePermanentlyAfter: int64(internal_databroker.DefaultDeletePermanentlyAfter.Seconds()),
+	}
+	return db, nil
+}
+
+func (db *DB) Put(ctx context.Context, id string, data *anypb.Any) error {
+	c := db.pool.Get()
+	defer c.Close()
+	exists, _ := redis.Bool(c.Do("EXISTS", id))
+
+	record := new(databroker.Record)
+	record.Data = data
+	if !exists {
+		record.CreatedAt = ptypes.TimestampNow()
+	}
+	record.ModifiedAt = ptypes.TimestampNow()
+	record.Type = db.recordType
+	record.Id = id
+	record.Version = fmt.Sprintf("%012X", atomic.AddUint64(&db.lastVersion, 1))
+	b, err := json.Marshal(record)
+	if err != nil {
+		return err
+	}
+	if _, err := c.Do("SET", id, string(b)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *DB) Get(ctx context.Context, id string) *databroker.Record {
+	c := db.pool.Get()
+	defer c.Close()
+
+	b, err := redis.Bytes(c.Do("GET", id))
+	if err != nil {
+		return nil
+	}
+
+	record := &databroker.Record{}
+	err = json.Unmarshal(b, record)
+	if err != nil {
+		return nil
+	}
+	return record
+}
+
+func (db *DB) GetAll(ctx context.Context) []*databroker.Record {
+	return db.getAll(ctx, func(record *databroker.Record) bool { return true })
+}
+
+func (db *DB) List(ctx context.Context, sinceVersion string) []*databroker.Record {
+	return db.getAll(ctx, func(record *databroker.Record) bool {
+		return record.Version > sinceVersion
+	})
+}
+
+func (db *DB) Delete(ctx context.Context, id string) error {
+	c := db.pool.Get()
+	defer c.Close()
+
+	r := db.Get(ctx, id)
+	if r == nil {
+		return errors.New("not found")
+	}
+	r.DeletedAt = ptypes.TimestampNow()
+	b, err := json.Marshal(r)
+	if err != nil {
+		return err
+	}
+	if _, err := c.Do("SET", id, string(b), "EX", db.deletePermanentlyAfter); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *DB) ClearDeleted(ctx context.Context, cutoff time.Time) {
+	return
+}
+
+func (db *DB) getAll(ctx context.Context, filter func(record *databroker.Record) bool) []*databroker.Record {
+	c := db.pool.Get()
+	defer c.Close()
+	iter := 0
+	records := make([]*databroker.Record, 0)
+	for {
+		arr, err := redis.Values(c.Do("SCAN", iter, "MATCH", "*"))
+		if err != nil {
+			return nil
+		}
+
+		iter, _ = redis.Int(arr[0], nil)
+		ids, _ := redis.Strings(arr[1], nil)
+
+		for _, id := range ids {
+			b, err := redis.Bytes(c.Do("GET", id))
+			if err != nil {
+				return nil
+			}
+
+			record := &databroker.Record{}
+			if err := json.Unmarshal(b, record); err != nil {
+				return nil
+			}
+			if filter(record) {
+				records = append(records, record)
+			}
+		}
+
+		if iter == 0 {
+			break
+		}
+	}
+
+	return records
+}
---
+++ pkg/storage/redis/redis_test.go
@@ -0,0 +1,79 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+func TestDB(t *testing.T) {
+	ctx := context.Background()
+	db, err := New(":6379")
+	require.NoError(t, err)
+	ids := []string{"a", "b", "c"}
+	id := ids[0]
+	c := db.pool.Get()
+	defer c.Close()
+	_, _ = c.Do("DEL", id)
+
+	t.Run("get missing record", func(t *testing.T) {
+		assert.Nil(t, db.Get(ctx, id))
+	})
+	t.Run("get record", func(t *testing.T) {
+		data := new(anypb.Any)
+		assert.NoError(t, db.Put(ctx, id, data))
+		record := db.Get(ctx, id)
+		if assert.NotNil(t, record) {
+			assert.NotNil(t, record.CreatedAt)
+			assert.Equal(t, data, record.Data)
+			assert.Nil(t, record.DeletedAt)
+			assert.Equal(t, "a", record.Id)
+			assert.NotNil(t, record.ModifiedAt)
+			assert.Equal(t, "000000000001", record.Version)
+		}
+	})
+	t.Run("delete record", func(t *testing.T) {
+		assert.NoError(t, db.Delete(ctx, id))
+		record := db.Get(ctx, id)
+		if assert.NotNil(t, record) {
+			assert.NotNil(t, record.DeletedAt)
+		}
+		ttl, err := redis.Int64(c.Do("TTL", id))
+		assert.NoError(t, err)
+		assert.Greater(t, ttl, int64(0))
+	})
+	t.Run("get all", func(t *testing.T) {
+		assert.Len(t, db.GetAll(ctx), 1)
+		data := new(anypb.Any)
+
+		for _, id := range ids {
+			assert.NoError(t, db.Put(ctx, id, data))
+		}
+		assert.Len(t, db.GetAll(ctx), len(ids))
+		for _, id := range ids {
+			_, _ = c.Do("DEL", id)
+		}
+	})
+	t.Run("list", func(t *testing.T) {
+		ids := make([]string, 0, 10)
+		for i := 0; i < 10; i++ {
+			id := fmt.Sprintf("%02d", i)
+			ids = append(ids, id)
+			data := new(anypb.Any)
+			assert.NoError(t, db.Put(ctx, id, data))
+		}
+
+		assert.Len(t, db.List(ctx, ""), 10)
+		assert.Len(t, db.List(ctx, "00000000000A"), 4)
+		assert.Len(t, db.List(ctx, "00000000000F"), 0)
+
+		for _, id := range ids {
+			_, _ = c.Do("DEL", id)
+		}
+	})
+}
