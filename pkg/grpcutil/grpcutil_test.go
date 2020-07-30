714363fb072e01e968fc051b32f14f2b9f9cb478
Caleb Doxsey <cdoxsey@pomerium.com>
2020-07-30 09:40:55 -0600

    grpcutil: add functions for JWTs in gRPC metadata (#1165)

--- pkg/grpcutil/grpcutil_test.go
+++ pkg/grpcutil/grpcutil_test.go
@@ -27,3 +27,25 @@ func TestSessionIDFromGRPCRequest(t *testing.T) {
 	assert.True(t, ok)
 	assert.Equal(t, "EXAMPLE", sessionID)
 }
+
+func TestWithOutgoingJWT(t *testing.T) {
+	rawjwt := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+	ctx := context.Background()
+	ctx = WithOutgoingJWT(ctx, rawjwt)
+	md, ok := metadata.FromOutgoingContext(ctx)
+	if !assert.True(t, ok) {
+		return
+	}
+	assert.Equal(t, []string{rawjwt}, md.Get("jwt"))
+}
+
+func TestJWTFromGRPCRequest(t *testing.T) {
+	rawjwt := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+	ctx := context.Background()
+	ctx = metadata.NewIncomingContext(ctx, metadata.MD{
+		"jwt": {rawjwt},
+	})
+	found, ok := JWTFromGRPCRequest(ctx)
+	assert.True(t, ok)
+	assert.Equal(t, rawjwt, found)
+}
