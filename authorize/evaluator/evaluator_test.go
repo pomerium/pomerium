5653a398de81ff5c9e2bd90c1485052a35e410c0
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-08-04 22:43:03 +0700

    authorize/evaluator: add more test cases (#1198)

--- authorize/evaluator/evaluator_test.go
+++ authorize/evaluator/evaluator_test.go
@@ -23,14 +23,28 @@ import (
 
 func TestJSONMarshal(t *testing.T) {
 	dbd := DataBrokerData{
+		"type.googleapis.com/session.Session": map[string]interface{}{
+			"SESSION_ID": &session.Session{
+				UserId: "user1",
+			},
+		},
 		"type.googleapis.com/directory.User": map[string]interface{}{
 			"user1": &directory.User{
 				Id:       "user1",
 				GroupIds: []string{"group1", "group2"},
 			},
 		},
-		"type.googleapis.com/session.Session": map[string]interface{}{},
-		"type.googleapis.com/user.User":       map[string]interface{}{},
+		"type.googleapis.com/directory.Group": map[string]interface{}{
+			"group1": &directory.Group{
+				Id:    "group1",
+				Name:  "admin",
+				Email: "admin@example.com",
+			},
+			"group2": &directory.Group{
+				Id:   "group2",
+				Name: "test",
+			},
+		},
 	}
 
 	bs, _ := json.Marshal(new(Evaluator).newInput(&Request{
@@ -51,6 +65,10 @@ func TestJSONMarshal(t *testing.T) {
 	}, true))
 	assert.JSONEq(t, `{
 		"databroker_data": {
+			"groups": ["admin", "admin@example.com", "test", "group1", "group2"],
+			"session": {
+				"user_id": "user1"
+			}
 		},
 		"http": {
 			"client_certificate": "CLIENT_CERTIFICATE",
@@ -109,6 +127,210 @@ func TestEvaluator_JWTWithKID(t *testing.T) {
 	require.NoError(t, err)
 	require.Len(t, tok.Headers, 1)
 	assert.Equal(t, "5b419ade1895fec2d2def6cd33b1b9a018df60db231dc5ecb85cbed6d942813c", tok.Headers[0].KeyID)
+}
+
+func TestEvaluator_JWTPayload(t *testing.T) {
+	nowPb := ptypes.TimestampNow()
+	now, _ := ptypes.Timestamp(nowPb)
+	tests := []struct {
+		name string
+		req  *Request
+		want map[string]interface{}
+	}{
+		{
+			"iss and aud",
+			&Request{
+				HTTP: RequestHTTP{URL: "https://example.com"},
+			},
+			map[string]interface{}{
+				"iss": "authn.example.com",
+				"aud": "example.com",
+			},
+		},
+		{
+			"with session",
+			&Request{
+				DataBrokerData: DataBrokerData{
+					"type.googleapis.com/session.Session": map[string]interface{}{
+						"SESSION_ID": &session.Session{
+							IdToken: &session.IDToken{
+								ExpiresAt: nowPb,
+								IssuedAt:  nowPb,
+							},
+						},
+					},
+				},
+				HTTP: RequestHTTP{URL: "https://example.com"},
+				Session: RequestSession{
+					ID: "SESSION_ID",
+				},
+			},
+			map[string]interface{}{
+				"iss": "authn.example.com",
+				"aud": "example.com",
+				"exp": now.Unix(),
+				"iat": now.Unix(),
+			},
+		},
+		{
+			"with user",
+			&Request{
+				DataBrokerData: DataBrokerData{
+					"type.googleapis.com/session.Session": map[string]interface{}{
+						"SESSION_ID": &session.Session{
+							UserId: "USER_ID",
+						},
+					},
+					"type.googleapis.com/user.User": map[string]interface{}{
+						"USER_ID": &user.User{
+							Id:    "USER_ID",
+							Name:  "foo",
+							Email: "foo@example.com",
+						},
+					},
+				},
+				HTTP: RequestHTTP{URL: "https://example.com"},
+				Session: RequestSession{
+					ID: "SESSION_ID",
+				},
+			},
+			map[string]interface{}{
+				"iss":   "authn.example.com",
+				"aud":   "example.com",
+				"sub":   "USER_ID",
+				"user":  "USER_ID",
+				"email": "foo@example.com",
+			},
+		},
+		{
+			"with directory user",
+			&Request{
+				DataBrokerData: DataBrokerData{
+					"type.googleapis.com/session.Session": map[string]interface{}{
+						"SESSION_ID": &session.Session{
+							UserId: "USER_ID",
+						},
+					},
+					"type.googleapis.com/directory.User": map[string]interface{}{
+						"USER_ID": &directory.User{
+							Id:       "USER_ID",
+							GroupIds: []string{"group1", "group2"},
+						},
+					},
+					"type.googleapis.com/directory.Group": map[string]interface{}{
+						"group1": &directory.Group{
+							Id:    "group1",
+							Name:  "admin",
+							Email: "admin@example.com",
+						},
+						"group2": &directory.Group{
+							Id:    "group2",
+							Name:  "test",
+							Email: "test@example.com",
+						},
+					},
+				},
+				HTTP: RequestHTTP{URL: "https://example.com"},
+				Session: RequestSession{
+					ID: "SESSION_ID",
+				},
+			},
+			map[string]interface{}{
+				"iss":    "authn.example.com",
+				"aud":    "example.com",
+				"groups": []string{"group1", "group2", "admin", "test"},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			e, err := New(&config.Options{
+				AuthenticateURL: mustParseURL("https://authn.example.com"),
+			}, NewStore())
+			require.NoError(t, err)
+			assert.Equal(t, tc.want, e.JWTPayload(tc.req))
+		})
+	}
+}
+
+func TestEvaluator_Evaluate(t *testing.T) {
+	dbd := make(DataBrokerData)
+	sessionID := uuid.New().String()
+	userID := uuid.New().String()
+	data, _ := ptypes.MarshalAny(&session.Session{
+		Version: "1",
+		Id:      sessionID,
+		UserId:  userID,
+		IdToken: &session.IDToken{
+			Issuer:   "TestEvaluatorEvaluate",
+			Subject:  userID,
+			IssuedAt: ptypes.TimestampNow(),
+		},
+		OauthToken: &session.OAuthToken{
+			AccessToken:  "ACCESS TOKEN",
+			TokenType:    "Bearer",
+			RefreshToken: "REFRESH TOKEN",
+		},
+	})
+	dbd.Update(&databroker.Record{
+		Version: "1",
+		Type:    "type.googleapis.com/session.Session",
+		Id:      sessionID,
+		Data:    data,
+	})
+	data, _ = ptypes.MarshalAny(&user.User{
+		Version: "1",
+		Id:      userID,
+		Email:   "foo@example.com",
+	})
+	dbd.Update(&databroker.Record{
+		Version: "1",
+		Type:    "type.googleapis.com/user.User",
+		Id:      userID,
+		Data:    data,
+	})
+
+	ctx := context.Background()
+	allowedPolicy := []config.Policy{{From: "https://foo.com", AllowedUsers: []string{"foo@example.com"}}}
+	forbiddenPolicy := []config.Policy{{From: "https://bar.com", AllowedUsers: []string{"bar@example.com"}}}
+
+	tests := []struct {
+		name           string
+		reqURL         string
+		policies       []config.Policy
+		customPolicies []string
+		sessionID      string
+		expectedStatus int
+	}{
+		{"allowed", "https://foo.com/path", allowedPolicy, nil, sessionID, http.StatusOK},
+		{"forbidden", "https://bar.com/path", forbiddenPolicy, nil, sessionID, http.StatusForbidden},
+		{"unauthorized", "https://foo.com/path", allowedPolicy, nil, "", http.StatusUnauthorized},
+		{"custom policy overwrite main policy", "https://foo.com/path", allowedPolicy, []string{"deny = true"}, sessionID, http.StatusForbidden},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			e, err := New(&config.Options{
+				AuthenticateURL: mustParseURL("https://authn.example.com"),
+				Policies:        tc.policies,
+			}, NewStore())
+			require.NoError(t, err)
+			res, err := e.Evaluate(ctx, &Request{
+				DataBrokerData: dbd,
+				HTTP:           RequestHTTP{Method: "GET", URL: tc.reqURL},
+				Session:        RequestSession{ID: tc.sessionID},
+				CustomPolicies: tc.customPolicies,
+			})
+			require.NoError(t, err)
+			assert.NotNil(t, res)
+			assert.Equal(t, tc.expectedStatus, res.Status)
+		})
+	}
 }
 
 func mustParseURL(str string) *url.URL {
