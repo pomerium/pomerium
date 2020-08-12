f7e7912b809d039bf7b81b2f2510c9dea8a4738e
Caleb Doxsey <cdoxsey@pomerium.com>
2020-08-12 10:16:26 -0600

    fix unnecessary firstNonEmpty

--- internal/envoy/envoy.go
+++ internal/envoy/envoy.go
@@ -157,7 +157,7 @@ func (srv *Server) update(cfg *config.Config) {
 func (srv *Server) run() error {
 	args := []string{
 		"-c", configFileName,
-		"--log-level", firstNonEmpty(srv.options.logLevel),
+		"--log-level", srv.options.logLevel,
 		"--log-format", "[LOG_FORMAT]%l--%n--%v",
 		"--log-format-escaped",
 	}
