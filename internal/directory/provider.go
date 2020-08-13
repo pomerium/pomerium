8ee4be1157e56ff8356fd6480e2b46ad1e2eaaea
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-08-13 14:23:23 +0700

    internal/directory/okta: add limiter to query okta API
    Okta only allows 100 requests per minute, so apply the default rate
    limit 1 QPS for it.
    
    Fixes #1256

--- internal/directory/provider.go
+++ internal/directory/provider.go
@@ -77,7 +77,9 @@ func GetProvider(options *config.Options) Provider {
 		if err == nil {
 			return okta.New(
 				okta.WithProviderURL(providerURL),
-				okta.WithServiceAccount(serviceAccount))
+				okta.WithServiceAccount(serviceAccount),
+				okta.WithQPS(options.QPS),
+			)
 		}
 		log.Warn().
 			Str("service", "directory").
