9e80a00f6c82054baff29966d6c0a56ce7db4d44
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2020-09-04 18:23:14 +0700

    internal/directory/okta: remove rate limiter (#1370)
    We did honor the rate limit header from okta, so don't bother to add our
    rate limiter there.

--- internal/directory/provider.go
+++ internal/directory/provider.go
@@ -86,7 +86,6 @@ func GetProvider(options *config.Options) Provider {
 			return okta.New(
 				okta.WithProviderURL(providerURL),
 				okta.WithServiceAccount(serviceAccount),
-				okta.WithQPS(1.0), // Backported default config
 			)
 		}
 		log.Warn().
