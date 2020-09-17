61d87d2b8cf39cb32c7b867c9c2faeedcaeb73fc
bobby <1544881+desimone@users.noreply.github.com>
2020-09-16 17:35:00 -0700

    controlplane: support P-384 / P-512 EC curves (#1409)
    Signed-off-by: Bobby DeSimone <bobbydesimone@gmail.com>

--- internal/controlplane/xds_clusters.go
+++ internal/controlplane/xds_clusters.go
@@ -118,6 +118,14 @@ func buildPolicyTransportSocket(policy *config.Policy) *envoy_config_core_v3.Tra
 	}
 	tlsContext := &envoy_extensions_transport_sockets_tls_v3.UpstreamTlsContext{
 		CommonTlsContext: &envoy_extensions_transport_sockets_tls_v3.CommonTlsContext{
+			TlsParams: &envoy_extensions_transport_sockets_tls_v3.TlsParameters{
+				EcdhCurves: []string{
+					"X25519",
+					"P-256",
+					"P-384",
+					"P-512",
+				},
+			},
 			AlpnProtocols: []string{"http/1.1"},
 			ValidationContextType: &envoy_extensions_transport_sockets_tls_v3.CommonTlsContext_ValidationContext{
 				ValidationContext: buildPolicyValidationContext(policy),
