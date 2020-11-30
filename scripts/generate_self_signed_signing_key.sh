a38c70910960f00ee40f33a92276cd8fbcc04b72
Bobby DeSimone <bobbydesimone@gmail.com>
2020-11-29 19:34:36 -0800

    update generate scripts for rsa / EdDSA
    Signed-off-by: Bobby DeSimone <bobbydesimone@gmail.com>

--- scripts/generate_self_signed_signing_key.sh
+++ scripts/generate_self_signed_signing_key.sh
@@ -15,3 +15,12 @@ openssl req \
 	-days 1000000 \
 	-out ec_public.pem \
 	-subj "/CN=unused"
+
+# uncomment if you want RSA / RS256
+# openssl genpkey -algorithm RSA -out rsa_private.pem -pkeyopt rsa_keygen_bits:2048
+# openssl req -x509 -nodes -newkey rsa:2048 -keyout rsa_private.pem -out rsa_cert.pem -subj "/CN=unused"
+
+# uncomment if you want EdDSA / Ed25519
+# openssl is /usr/local/opt/openssl/bin/openssl on mac with brew otherwise it is libressl
+# openssl genpkey -algorithm ED25519 -out ed25519_private.pem
+# openssl req -x509 -new -key ed25519_private.pem -days 1000000 -out ed25519_public.pem -subj "/CN=unused"
