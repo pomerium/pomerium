3eaed9bbc825f8fad415a105cd499a29ecc27263
Denis Mishin <dmishin@pomerium.com>
2021-01-21 12:04:23 -0500

    serializer test

--- config/custom_test.go
+++ config/custom_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v2"
 )
 
@@ -36,4 +37,27 @@ func TestStringSlice_UnmarshalYAML(t *testing.T) {
 `), &slc)
 		assert.Equal(t, NewStringSlice("a", "b", "c"), slc)
 	})
+}
+
+func TestSerializable(t *testing.T) {
+	data := []byte(`
+health_check:
+  timeout: 5s
+  interval: 60s
+  healthyThreshold: 1
+  unhealthyThreshold: 2
+  http_health_check: 
+    host: "http://51.15.222.790:8080"
+    path: "/"
+`)
+	var mi map[interface{}]interface{}
+
+	err := yaml.Unmarshal(data, &mi)
+	require.NoError(t, err, "unmarshal")
+
+	ms, err := serializable(mi)
+	require.NoError(t, err, "serializable")
+
+	_, err = json.Marshal(ms)
+	require.NoError(t, err, "json marshal")
 }
