81e7c1c4e32159bd250c3326ebc23ef8e4e1d023
Caleb Doxsey <cdoxsey@pomerium.com>
2021-01-26 10:04:52 -0700

    fix go:generate for envoy config

--- pkg/grpc/grpc.go
+++ pkg/grpc/grpc.go
@@ -5,4 +5,4 @@ package grpc
 //go:generate ../../scripts/protoc -I ./databroker/ --go_out=plugins=grpc,paths=source_relative:./databroker/. ./databroker/databroker.proto
 //go:generate ../../scripts/protoc -I ./directory/ --go_out=plugins=grpc,paths=source_relative:./directory/. ./directory/directory.proto
 //go:generate ../../scripts/protoc -I ./audit/ --go_out=plugins=grpc,paths=source_relative:./audit/. ./audit/audit.proto
-//go:generate ../../scripts/protoc -I ./config/ --go_out=M"envoy/config/cluster/v3/cluster.proto"="github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3",plugins=grpc,paths=source_relative:./config/. ./config/config.proto
+//go:generate ../../scripts/protoc -I ./config/ --go_out=Menvoy/config/cluster/v3/cluster.proto=github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3,plugins=grpc,paths=source_relative:./config/. ./config/config.proto
