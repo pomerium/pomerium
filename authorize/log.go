0d4184bb5a4d3c787fbecb55c62396f5c5c07095
Bobby DeSimone <bobbydesimone@gmail.com>
2021-04-27 13:19:02 -0700

    explit-y-er
    Signed-off-by: Bobby DeSimone <bobbydesimone@gmail.com>

--- authorize/log.go
+++ authorize/log.go
@@ -37,8 +37,8 @@ func (a *Authorize) logAuthorizeCheck(
 	// reply
 	if reply != nil {
 		evt = evt.Bool("allow", reply.Status == http.StatusOK)
-		evt = evt.Int("status", reply.Status)
-		evt = evt.Str("reply", reply.Message)
+		evt = evt.Int("reply-status", reply.Status)
+		evt = evt.Str("reply-message", reply.Message)
 		evt = evt.Str("user", u.GetId())
 		evt = evt.Str("email", u.GetEmail())
 		evt = evt.Uint64("databroker_server_version", reply.DataBrokerServerVersion)
