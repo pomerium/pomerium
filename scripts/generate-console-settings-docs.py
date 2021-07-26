e0b90a07505834e461d72dcfa0f8cd826461bb52
Alex Fornuto <alex@fornuto.com>
2021-07-26 14:11:04 -0500

    Enterprise settings docs (#2397)
    * init console-specific reference docs files
    
    * remove shortdoc for name
    
    * init Enterprise Reference doc

---
+++ scripts/generate-console-settings-docs.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import os.path
+from typing import Any, IO
+import yaml
+
+
+def main():
+    d = os.path.join(os.path.dirname(__file__),
+                     "..", "docs", "enterprise")
+    d = os.path.normpath(d)
+    print(f"generating {d}/reference.md")
+
+    f = open(os.path.join(d, "console-settings.yaml"))
+    doc = yaml.full_load(f)
+    f.close()
+
+    f = open(os.path.join(os.path.dirname(__file__),
+                          "..", "docs", "enterprise", "reference.md"), "w")
+    f.write(f"{doc['preamble']}\n")
+    write_setting(f, 1, doc)
+    f.write(f"{doc['postamble']}")
+    f.close()
+
+
+def write_setting(w, depth, setting):
+    if 'name' in setting:
+        w.write(f"{'#' * depth} {setting.get('name', '')}\n")
+
+    if 'attributes' in setting:
+        w.write(f"{setting.get('attributes','')}\n")
+
+    if 'doc' in setting:
+        w.write(f"{setting.get('doc', '')}\n")
+
+    w.write("\n")
+
+    for subsetting in setting.get('settings', []):
+        write_setting(w, depth+1, subsetting)
+
+
+if __name__ == "__main__":
+    main()
