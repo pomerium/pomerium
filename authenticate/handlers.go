a6f7ca5abd860c7e235edaf29f64f0ea43c9f1c5
Caleb Doxsey <cdoxsey@pomerium.com>
2021-07-29 16:11:39 -0600

    only check impersonate id on non-nil pbSession

--- authenticate/handlers.go
+++ authenticate/handlers.go
@@ -453,13 +453,18 @@ func (a *Authenticate) userInfo(w http.ResponseWriter, r *http.Request) error {
 
 	isImpersonated := false
 	pbSession, err := session.Get(ctx, state.dataBrokerClient, s.ID)
-	if pbSession.GetImpersonateSessionId() != "" {
-		pbSession, err = session.Get(ctx, state.dataBrokerClient, pbSession.GetImpersonateSessionId())
-		isImpersonated = true
-	}
 	if err != nil {
 		pbSession = &session.Session{
 			Id: s.ID,
+		}
+	}
+	if pbSession.GetImpersonateSessionId() != "" {
+		pbSession, err = session.Get(ctx, state.dataBrokerClient, pbSession.GetImpersonateSessionId())
+		isImpersonated = true
+		if err != nil {
+			pbSession = &session.Session{
+				Id: s.ID,
+			}
 		}
 	}
 
