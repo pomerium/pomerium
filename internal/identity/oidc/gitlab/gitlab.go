36dbaad577f51825ec8213bd6c7155e26e3d1a7e
Alex Fornuto <afornuto@pomerium.com>
2021-08-25 10:15:35 -0500

    rm redundant scope

--- internal/identity/oidc/gitlab/gitlab.go
+++ internal/identity/oidc/gitlab/gitlab.go
@@ -16,7 +16,7 @@ import (
 // Name identifies the GitLab identity provider.
 const Name = "gitlab"
 
-var defaultScopes = []string{oidc.ScopeOpenID, "profile", "email", "openid"}
+var defaultScopes = []string{oidc.ScopeOpenID, "profile", "email"}
 
 const (
 	defaultProviderURL = "https://gitlab.com"
