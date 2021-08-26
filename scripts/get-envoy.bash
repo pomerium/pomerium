112e93227f4943094ac9cddc4a9e00a3e008748f
Travis Groth <travisgroth@users.noreply.github.com>
2021-08-26 10:17:31 -0400

    deps: bump envoy to v0.17.4

--- scripts/get-envoy.bash
+++ scripts/get-envoy.bash
@@ -4,36 +4,36 @@ set -euo pipefail
 PATH="$PATH:$(go env GOPATH)/bin"
 export PATH
 
-_envoy_version=1.17.3
+_envoy_version=1.17.4
 _dir="${DIR:-"$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/../bin"}"
 _target="${TARGET:-"$(go env GOOS)-$(go env GOARCH)"}"
 
 is_command() {
-    command -v "$1" >/dev/null
+  command -v "$1" >/dev/null
 }
 
 hash_sha256() {
-    TARGET=${1:-/dev/stdin}
-    if is_command gsha256sum; then
-        hash=$(gsha256sum "$TARGET") || return 1
-        echo "$hash" | cut -d ' ' -f 1
-    elif is_command sha256sum; then
-        hash=$(sha256sum "$TARGET") || return 1
-        echo "$hash" | cut -d ' ' -f 1
-    elif is_command shasum; then
-        hash=$(shasum -a 256 "$TARGET" 2>/dev/null) || return 1
-        echo "$hash" | cut -d ' ' -f 1
-    elif is_command openssl; then
-        hash=$(openssl -dst openssl dgst -sha256 "$TARGET") || return 1
-        echo "$hash" | cut -d ' ' -f a
-    else
-        echo "hash_sha256 unable to find command to compute sha-256 hash"
-        return 1
-    fi
+  TARGET=${1:-/dev/stdin}
+  if is_command gsha256sum; then
+    hash=$(gsha256sum "$TARGET") || return 1
+    echo "$hash" | cut -d ' ' -f 1
+  elif is_command sha256sum; then
+    hash=$(sha256sum "$TARGET") || return 1
+    echo "$hash" | cut -d ' ' -f 1
+  elif is_command shasum; then
+    hash=$(shasum -a 256 "$TARGET" 2>/dev/null) || return 1
+    echo "$hash" | cut -d ' ' -f 1
+  elif is_command openssl; then
+    hash=$(openssl -dst openssl dgst -sha256 "$TARGET") || return 1
+    echo "$hash" | cut -d ' ' -f a
+  else
+    echo "hash_sha256 unable to find command to compute sha-256 hash"
+    return 1
+  fi
 }
 
 if [ -f "$_dir/envoy" ]; then
-    exit 0
+  exit 0
 fi
 
 mkdir -p "$_dir"
