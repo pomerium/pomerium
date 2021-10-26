baa9c2293c32457cc3a4ae0991d87c3c6a27c94c
Caleb Doxsey <cdoxsey@pomerium.com>
2021-10-26 16:57:06 -0600

    github: use GraphQL API to reduce number of API calls for directory sync

--- internal/directory/github/github_test.go
+++ internal/directory/github/github_test.go
@@ -11,6 +11,8 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/stretchr/testify/assert"
+	"github.com/vektah/gqlparser/ast"
+	"github.com/vektah/gqlparser/parser"
 
 	"github.com/pomerium/pomerium/internal/testutil"
 )
@@ -35,26 +37,198 @@ func newMockAPI(t *testing.T, srv *httptest.Server) http.Handler {
 		}
 		json.NewDecoder(r.Body).Decode(&body)
 
-		_ = json.NewEncoder(w).Encode(M{
-			"data": M{
-				"organization": M{
-					"teams": M{
-						"totalCount": 3,
-						"edges": []M{
-							{"node": M{
-								"id": "MDQ6VGVhbTE=",
-							}},
-							{"node": M{
-								"id": "MDQ6VGVhbTI=",
-							}},
-							{"node": M{
-								"id": "MDQ6VGVhbTM=",
-							}},
-						},
-					},
-				},
-			},
+		q, err := parser.ParseQuery(&ast.Source{
+			Input: body.Query,
 		})
+		if err != nil {
+			panic(err)
+		}
+
+		result := qlResult{
+			Data: &qlData{
+				Organization: &qlOrganization{},
+			},
+		}
+		handleMembersWithRole := func(orgSlug string, field *ast.Field) {
+			membersWithRole := &qlMembersWithRoleConnection{}
+
+			var cursor string
+			for _, arg := range field.Arguments {
+				if arg.Name == "after" {
+					cursor = arg.Value.Raw
+				}
+			}
+
+			switch cursor {
+			case `null`:
+				switch orgSlug {
+				case "org1":
+					membersWithRole.PageInfo = qlPageInfo{EndCursor: "TOKEN1", HasNextPage: true}
+					membersWithRole.Nodes = []qlUser{
+						{ID: "user1", Login: "user1", Name: "User 1", Email: "user1@example.com"},
+						{ID: "user2", Login: "user2", Name: "User 2", Email: "user2@example.com"},
+					}
+				case "org2":
+					membersWithRole.PageInfo = qlPageInfo{HasNextPage: false}
+					membersWithRole.Nodes = []qlUser{
+						{ID: "user4", Login: "user4", Name: "User 4", Email: "user4@example.com"},
+					}
+				default:
+					t.Errorf("unexpected org slug: %s", orgSlug)
+				}
+			case `TOKEN1`:
+				membersWithRole.PageInfo = qlPageInfo{HasNextPage: false}
+				membersWithRole.Nodes = []qlUser{
+					{ID: "user3", Login: "user3", Name: "User 3", Email: "user3@example.com"},
+				}
+			default:
+				t.Errorf("unexpected cursor: %s", cursor)
+			}
+
+			result.Data.Organization.MembersWithRole = membersWithRole
+		}
+		handleTeamMembers := func(orgSlug, teamSlug string, field *ast.Field) {
+			result.Data.Organization.Team.Members = &qlTeamMemberConnection{
+				PageInfo: qlPageInfo{HasNextPage: false},
+			}
+
+			switch teamSlug {
+			case "team3":
+				result.Data.Organization.Team.Members.Edges = []qlTeamMemberEdge{
+					{Node: qlUser{ID: "user3"}},
+				}
+			}
+		}
+		handleTeam := func(orgSlug string, field *ast.Field) {
+			result.Data.Organization.Team = &qlTeam{}
+
+			var teamSlug string
+			for _, arg := range field.Arguments {
+				if arg.Name == "slug" {
+					teamSlug = arg.Value.Raw
+				}
+			}
+
+			for _, selection := range field.SelectionSet {
+				subField, ok := selection.(*ast.Field)
+				if !ok {
+					continue
+				}
+
+				switch subField.Name {
+				case "members":
+					handleTeamMembers(orgSlug, teamSlug, subField)
+				}
+			}
+		}
+		handleTeams := func(orgSlug string, field *ast.Field) {
+			teams := &qlTeamConnection{}
+
+			var cursor string
+			var userLogin string
+			for _, arg := range field.Arguments {
+				if arg.Name == "after" {
+					cursor = arg.Value.Raw
+				}
+				if arg.Name == "userLogins" {
+					userLogin = arg.Value.Children[0].Value.Raw
+				}
+			}
+
+			switch cursor {
+			case `null`:
+				switch orgSlug {
+				case "org1":
+					teams.PageInfo = qlPageInfo{HasNextPage: true, EndCursor: "TOKEN1"}
+					teams.Edges = []qlTeamEdge{
+						{Node: qlTeam{ID: "1", Slug: "team1", Name: "Team 1", Members: &qlTeamMemberConnection{
+							PageInfo: qlPageInfo{HasNextPage: false},
+							Edges: []qlTeamMemberEdge{
+								{Node: qlUser{ID: "user1"}},
+								{Node: qlUser{ID: "user2"}},
+							},
+						}}},
+					}
+				case "org2":
+					teams.PageInfo = qlPageInfo{HasNextPage: false}
+					teams.Edges = []qlTeamEdge{
+						{Node: qlTeam{ID: "3", Slug: "team3", Name: "Team 3", Members: &qlTeamMemberConnection{
+							PageInfo: qlPageInfo{HasNextPage: true, EndCursor: "TOKEN1"},
+							Edges: []qlTeamMemberEdge{
+								{Node: qlUser{ID: "user1"}},
+								{Node: qlUser{ID: "user2"}},
+							},
+						}}},
+					}
+					if userLogin == "" || userLogin == "user4" {
+						teams.Edges = append(teams.Edges, qlTeamEdge{
+							Node: qlTeam{ID: "4", Slug: "team4", Name: "Team 4", Members: &qlTeamMemberConnection{
+								PageInfo: qlPageInfo{HasNextPage: false},
+								Edges: []qlTeamMemberEdge{
+									{Node: qlUser{ID: "user4"}},
+								},
+							}},
+						})
+					}
+				default:
+					t.Errorf("unexpected org slug: %s", orgSlug)
+				}
+			case "TOKEN1":
+				teams.PageInfo = qlPageInfo{HasNextPage: false}
+				teams.Edges = []qlTeamEdge{
+					{Node: qlTeam{ID: "2", Slug: "team2", Name: "Team 2", Members: &qlTeamMemberConnection{
+						PageInfo: qlPageInfo{HasNextPage: false},
+						Edges: []qlTeamMemberEdge{
+							{Node: qlUser{ID: "user1"}},
+						},
+					}}},
+				}
+			default:
+				t.Errorf("unexpected cursor: %s", cursor)
+			}
+
+			result.Data.Organization.Teams = teams
+		}
+		handleOrganization := func(field *ast.Field) {
+			var orgSlug string
+			for _, arg := range field.Arguments {
+				if arg.Name == "login" {
+					orgSlug = arg.Value.Raw
+				}
+			}
+			for _, orgSelection := range field.SelectionSet {
+				orgField, ok := orgSelection.(*ast.Field)
+				if !ok {
+					continue
+				}
+
+				switch orgField.Name {
+				case "teams":
+					handleTeams(orgSlug, orgField)
+				case "team":
+					handleTeam(orgSlug, orgField)
+				case "membersWithRole":
+					handleMembersWithRole(orgSlug, orgField)
+				}
+			}
+		}
+
+		for _, operation := range q.Operations {
+			for _, selection := range operation.SelectionSet {
+				field, ok := selection.(*ast.Field)
+				if !ok {
+					continue
+				}
+
+				if field.Name != "organization" {
+					continue
+				}
+
+				handleOrganization(field)
+			}
+		}
+
+		_ = json.NewEncoder(w).Encode(result)
 	})
 	r.Get("/user/orgs", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode([]M{
