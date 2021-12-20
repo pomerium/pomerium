210e7c9fc920560118c8c221b957e305ec5bfe2d
Denis Mishin <dmishin@pomerium.com>
2021-12-19 22:02:47 -0500

    lint

--- pkg/grpcutil/client.go
+++ pkg/grpcutil/client.go
@@ -85,6 +85,7 @@ func NewGRPCClientConn(ctx context.Context, opts *Options, other ...grpc.DialOpt
 		}
 
 		cert := credentials.NewTLS(&tls.Config{
+			// nolint: gosec
 			InsecureSkipVerify: opts.InsecureSkipVerify,
 			RootCAs:            rootCAs,
 			MinVersion:         tls.VersionTLS12,
