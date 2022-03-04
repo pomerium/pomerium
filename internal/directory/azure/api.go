8567b56b8da0e198d7f2051aafd0f462f71132bd
JBodkin-Amphora <james.bodkin@amphora.net>
2022-03-04 18:18:39 +0000

    Extract email for active directory users that don't have access to exchange (#3053)

--- internal/directory/azure/api.go
+++ internal/directory/azure/api.go
@@ -31,15 +31,17 @@ func (obj apiUser) getEmail() string {
 
 	// AD often doesn't have the email address returned, but we can parse it from the UPN
 
-	// UPN looks like:
+	// UPN looks like either:
 	// cdoxsey_pomerium.com#EXT#@cdoxseypomerium.onmicrosoft.com
+	// cdoxsey@pomerium.com
 	email := obj.UserPrincipalName
 	if idx := strings.Index(email, "#EXT"); idx > 0 {
 		email = email[:idx]
-	}
-	// find the last _ and replace it with @
-	if idx := strings.LastIndex(email, "_"); idx > 0 {
-		email = email[:idx] + "@" + email[idx+1:]
+
+		// find the last _ and replace it with @
+		if idx := strings.LastIndex(email, "_"); idx > 0 {
+			email = email[:idx] + "@" + email[idx+1:]
+		}
 	}
 	return email
 }
