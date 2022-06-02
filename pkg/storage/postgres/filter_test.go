a4fbbc5de9d1c04973c11a3b830deba3083e258a
Caleb Doxsey <cdoxsey@pomerium.com>
2022-06-02 12:38:04 -0600

    postgres: fix CIDR query

--- pkg/storage/postgres/filter_test.go
+++ pkg/storage/postgres/filter_test.go
@@ -21,12 +21,16 @@ func TestAddFilterExpressionToQuery(t *testing.T) {
 				Fields: []string{"$index"},
 				Value:  "v2",
 			},
+			storage.EqualsFilterExpression{
+				Fields: []string{"$index"},
+				Value:  "10.0.0.0/8",
+			},
 		},
 		storage.EqualsFilterExpression{
 			Fields: []string{"type"},
 			Value:  "v3",
 		},
 	})
-	assert.Equal(t, "( ( pomerium.records.id = $1 OR pomerium.records.index_cidr >>= $2 ) AND pomerium.records.type = $3 )", query)
-	assert.Equal(t, []any{"v1", "v2", "v3"}, args)
+	assert.Equal(t, "( ( pomerium.records.id = $1 OR  false  OR pomerium.records.index_cidr >>= $2 ) AND pomerium.records.type = $3 )", query)
+	assert.Equal(t, []any{"v1", "10.0.0.0/8", "v3"}, args)
 }
