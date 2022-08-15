a97dae0c54cb94981908803fecdedbf4cf2a21d1
github-action-benchmark <github@users.noreply.github.com>
2022-08-15 16:31:50 +0000

    add Benchmark (go) benchmark result for 99d43fe0c11a66c3e6a8c50d46a2fefdb3ccb990

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660580849565,
+  "lastUpdate": 1660581110667,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -6352,6 +6352,42 @@ window.BENCHMARK_DATA = {
             "value": 8521438,
             "unit": "ns/op",
             "extra": "139 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "99d43fe0c11a66c3e6a8c50d46a2fefdb3ccb990",
+          "message": "chore(deps): bump go.uber.org/zap from 1.21.0 to 1.22.0 (#3551)\n\nBumps [go.uber.org/zap](https://github.com/uber-go/zap) from 1.21.0 to 1.22.0.\r\n- [Release notes](https://github.com/uber-go/zap/releases)\r\n- [Changelog](https://github.com/uber-go/zap/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/uber-go/zap/compare/v1.21.0...v1.22.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: go.uber.org/zap\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2022-08-15T10:20:52-06:00",
+          "tree_id": "784d260a1f6b5290584a567d5b55259e454fef8f",
+          "url": "https://github.com/pomerium/pomerium/commit/99d43fe0c11a66c3e6a8c50d46a2fefdb3ccb990"
+        },
+        "date": 1660581109838,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 17564915,
+            "unit": "ns/op",
+            "extra": "73 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 10902181,
+            "unit": "ns/op",
+            "extra": "100 times\n2 procs"
           }
         ]
       }
