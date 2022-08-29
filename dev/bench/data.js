460049f3d4355dfa4d17fe56ee03109a935440ca
github-action-benchmark <github@users.noreply.github.com>
2022-08-29 22:22:28 +0000

    add Benchmark (go) benchmark result for e5ac784cf4fcbefb5c4450edfbf03285263daa1d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1661809739324,
+  "lastUpdate": 1661811748006,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -9036,6 +9036,42 @@ window.BENCHMARK_DATA = {
             "value": 8419602,
             "unit": "ns/op",
             "extra": "145 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cdoxsey@pomerium.com",
+            "name": "Caleb Doxsey",
+            "username": "calebdoxsey"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e5ac784cf4fcbefb5c4450edfbf03285263daa1d",
+          "message": "autocert: add support for ACME TLS-ALPN (#3590)\n\n* autocert: add support for ACME TLS-ALPN\r\n\r\n* always re-create acme tls server",
+          "timestamp": "2022-08-29T16:19:20-06:00",
+          "tree_id": "adf69421ee906a6027c98ebf10a8f1254f85d2db",
+          "url": "https://github.com/pomerium/pomerium/commit/e5ac784cf4fcbefb5c4450edfbf03285263daa1d"
+        },
+        "date": 1661811747581,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 16883872,
+            "unit": "ns/op",
+            "extra": "61 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 10984187,
+            "unit": "ns/op",
+            "extra": "100 times\n2 procs"
           }
         ]
       }
