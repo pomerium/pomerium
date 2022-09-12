2b6b6fc9dc2730ccb9f7b6707cc2365f336608fa
github-action-benchmark <github@users.noreply.github.com>
2022-09-12 20:31:21 +0000

    add Benchmark (go) benchmark result for 7e3eed85d074ca2841129d22521ac7c2bd716360

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663014463219,
+  "lastUpdate": 1663014681537,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -10444,6 +10444,42 @@ window.BENCHMARK_DATA = {
             "value": 8318219,
             "unit": "ns/op",
             "extra": "144 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7e3eed85d074ca2841129d22521ac7c2bd716360",
+          "message": "chore(deps): bump github.com/spf13/viper from 1.12.0 to 1.13.0 (#3613)\n\nBumps [github.com/spf13/viper](https://github.com/spf13/viper) from 1.12.0 to 1.13.0.\r\n- [Release notes](https://github.com/spf13/viper/releases)\r\n- [Commits](https://github.com/spf13/viper/compare/v1.12.0...v1.13.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: github.com/spf13/viper\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2022-09-12T14:12:31-06:00",
+          "tree_id": "2a0c316458a0979e27550aede86ce1396dbafcb7",
+          "url": "https://github.com/pomerium/pomerium/commit/7e3eed85d074ca2841129d22521ac7c2bd716360"
+        },
+        "date": 1663014681106,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 16416504,
+            "unit": "ns/op",
+            "extra": "74 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 10137380,
+            "unit": "ns/op",
+            "extra": "100 times\n2 procs"
           }
         ]
       }
