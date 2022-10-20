d17599f63b27e1087a409aa7a2c08d9dd46ba780
Caleb Doxsey <cdoxsey@pomerium.com>
2022-10-20 16:58:29 -0600

    fileutil: update watcher to use fsnotify and polling (#3663) (#3685)
    * fileutil: update watcher to use fsnotify and polling
    
    * raise timeout
    
    * maybe fix

---
+++ internal/chanutil/batch_test.go
@@ -0,0 +1,25 @@
+package chanutil
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBatch(t *testing.T) {
+	ch1 := make(chan int)
+	go func() {
+		for _, i := range []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10} {
+			ch1 <- i
+		}
+		close(ch1)
+	}()
+
+	ch2 := Batch(ch1, WithBatchMaxWait(time.Millisecond*10), WithBatchMaxSize(3))
+	assert.Equal(t, []int{1, 2, 3}, <-ch2)
+	assert.Equal(t, []int{4, 5, 6}, <-ch2)
+	assert.Equal(t, []int{7, 8, 9}, <-ch2)
+	assert.Equal(t, []int{10}, <-ch2)
+	assert.Equal(t, []int(nil), <-ch2)
+}
