cebe3f3b22fb9f77233e01b39f9c8910832a540b
github-action-benchmark <github@users.noreply.github.com>
2022-11-14 16:23:05 +0000

    add Benchmark (go) benchmark result for e4a9889d175f4e81e7fa4884112bd19bf739d7f2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668442920344,
+  "lastUpdate": 1668442985142,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -19596,6 +19596,42 @@ window.BENCHMARK_DATA = {
             "value": 7736936,
             "unit": "ns/op",
             "extra": "148 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e4a9889d175f4e81e7fa4884112bd19bf739d7f2",
+          "message": "chore(deps): bump golang.org/x/net from 0.1.0 to 0.2.0 (#3748)\n\nBumps [golang.org/x/net](https://github.com/golang/net) from 0.1.0 to 0.2.0.\r\n- [Release notes](https://github.com/golang/net/releases)\r\n- [Commits](https://github.com/golang/net/compare/v0.1.0...v0.2.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: golang.org/x/net\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2022-11-14T09:17:46-07:00",
+          "tree_id": "0a393159508c2fa39a4560a5b557b3aa813b3fe0",
+          "url": "https://github.com/pomerium/pomerium/commit/e4a9889d175f4e81e7fa4884112bd19bf739d7f2"
+        },
+        "date": 1668442984058,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 15618038,
+            "unit": "ns/op",
+            "extra": "73 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 10206957,
+            "unit": "ns/op",
+            "extra": "104 times\n2 procs"
           }
         ]
       }
