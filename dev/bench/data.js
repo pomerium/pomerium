f984728088b5872431ca6ce522bc3284d31e40a4
github-action-benchmark <github@users.noreply.github.com>
2022-12-02 16:16:03 +0000

    add Benchmark (go) benchmark result for 7d326ced26132957e3070ce6a6ab230259db27f6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1669997708882,
+  "lastUpdate": 1669997763034,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -21852,6 +21852,42 @@ window.BENCHMARK_DATA = {
             "value": 9699589,
             "unit": "ns/op",
             "extra": "126 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cdoxsey@pomerium.com",
+            "name": "Caleb Doxsey",
+            "username": "calebdoxsey"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7d326ced26132957e3070ce6a6ab230259db27f6",
+          "message": "ci: disable kubernetes integration test (#3778)",
+          "timestamp": "2022-12-02T09:11:32-07:00",
+          "tree_id": "f0dbad791076ab20110f2fddb0129ae826dd96da",
+          "url": "https://github.com/pomerium/pomerium/commit/7d326ced26132957e3070ce6a6ab230259db27f6"
+        },
+        "date": 1669997762198,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 12301065,
+            "unit": "ns/op",
+            "extra": "100 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 8531317,
+            "unit": "ns/op",
+            "extra": "141 times\n2 procs"
           }
         ]
       }
