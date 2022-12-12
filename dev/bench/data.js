9122f53264b9df4430976b29e8abea4c5d4a4d74
github-action-benchmark <github@users.noreply.github.com>
2022-12-12 16:07:17 +0000

    add Benchmark (go) benchmark result for 346ba28960ffb8ff6a2efd2dd033a3468c91ce24

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670861192557,
+  "lastUpdate": 1670861237859,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -23404,6 +23404,38 @@ window.BENCHMARK_DATA = {
             "value": 9558404,
             "unit": "ns/op",
             "extra": "129 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "pomerium",
+            "username": "pomerium"
+          },
+          "committer": {
+            "name": "pomerium",
+            "username": "pomerium"
+          },
+          "id": "346ba28960ffb8ff6a2efd2dd033a3468c91ce24",
+          "message": "chore(deps): bump actions/setup-python from 4.3.0 to 4.3.1",
+          "timestamp": "2022-12-11T15:40:51Z",
+          "url": "https://github.com/pomerium/pomerium/pull/3800/commits/346ba28960ffb8ff6a2efd2dd033a3468c91ce24"
+        },
+        "date": 1670861236686,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 19167969,
+            "unit": "ns/op",
+            "extra": "72 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 11457577,
+            "unit": "ns/op",
+            "extra": "98 times\n2 procs"
           }
         ]
       }
