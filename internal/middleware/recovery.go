27c94396a8f692549eb82ba297e9a26f8978b09f
Caleb Doxsey <cdoxsey@pomerium.com>
2022-12-15 14:41:29 -0700

    controlplane: remove gorilla handlers dependency (#3813)

---
+++ internal/middleware/recovery.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/pomerium/pomerium/internal/log"
+)
+
+// Recovery is an HTTP middleware function that logs any panics.
+func Recovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Error(r.Context()).Interface("error", err).Msg("middleware: panic while serving http")
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
