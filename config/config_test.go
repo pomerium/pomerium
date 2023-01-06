3f1a87727f953cca9408ed225f90ce511053fb12
Caleb Doxsey <cdoxsey@pomerium.com>
2023-01-06 12:50:40 -0700

    config: generate derived certificates instead of self-signed certificates (#3860)

---
+++ config/config_test.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"crypto/tls"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pomerium/pomerium/pkg/cryptutil"
+)
+
+func TestConfig_GetCertificateForServerName(t *testing.T) {
+	gen := func(t *testing.T, serverName string) *tls.Certificate {
+		cert, err := cryptutil.GenerateSelfSignedCertificate(serverName)
+		if !assert.NoError(t, err, "error generating certificate for: %s", serverName) {
+			t.FailNow()
+		}
+		return cert
+	}
+
+	t.Run("exact match", func(t *testing.T) {
+		cfg := &Config{Options: NewDefaultOptions(), AutoCertificates: []tls.Certificate{
+			*gen(t, "a.example.com"),
+			*gen(t, "b.example.com"),
+		}}
+
+		found, err := cfg.GetCertificateForServerName("b.example.com")
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.Equal(t, &cfg.AutoCertificates[1], found)
+	})
+	t.Run("wildcard match", func(t *testing.T) {
+		cfg := &Config{Options: NewDefaultOptions(), AutoCertificates: []tls.Certificate{
+			*gen(t, "a.example.com"),
+			*gen(t, "*.example.com"),
+		}}
+
+		found, err := cfg.GetCertificateForServerName("b.example.com")
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.Equal(t, &cfg.AutoCertificates[1], found)
+	})
+	t.Run("no name match", func(t *testing.T) {
+		cfg := &Config{Options: NewDefaultOptions(), AutoCertificates: []tls.Certificate{
+			*gen(t, "a.example.com"),
+		}}
+
+		found, err := cfg.GetCertificateForServerName("b.example.com")
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.NotNil(t, found)
+		assert.NotEqual(t, &cfg.AutoCertificates[0], found)
+	})
+	t.Run("generate", func(t *testing.T) {
+		cfg := &Config{Options: NewDefaultOptions()}
+
+		found, err := cfg.GetCertificateForServerName("b.example.com")
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.NotNil(t, found)
+	})
+}
--- pkg/cryptutil/tls_test.go
+++ pkg/cryptutil/tls_test.go
@@ -1,68 +1,11 @@
 package cryptutil
 
 import (
-	"crypto/tls"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-
-func TestGetCertificateForServerName(t *testing.T) {
-	gen := func(t *testing.T, serverName string) *tls.Certificate {
-		cert, err := GenerateSelfSignedCertificate(serverName)
-		if !assert.NoError(t, err, "error generating certificate for: %s", serverName) {
-			t.FailNow()
-		}
-		return cert
-	}
-
-	t.Run("exact match", func(t *testing.T) {
-		certs := []tls.Certificate{
-			*gen(t, "a.example.com"),
-			*gen(t, "b.example.com"),
-		}
-
-		found, err := GetCertificateForServerName(certs, "b.example.com")
-		if !assert.NoError(t, err) {
-			return
-		}
-		assert.Equal(t, &certs[1], found)
-	})
-	t.Run("wildcard match", func(t *testing.T) {
-		certs := []tls.Certificate{
-			*gen(t, "a.example.com"),
-			*gen(t, "*.example.com"),
-		}
-
-		found, err := GetCertificateForServerName(certs, "b.example.com")
-		if !assert.NoError(t, err) {
-			return
-		}
-		assert.Equal(t, &certs[1], found)
-	})
-	t.Run("no name match", func(t *testing.T) {
-		certs := []tls.Certificate{
-			*gen(t, "a.example.com"),
-		}
-
-		found, err := GetCertificateForServerName(certs, "b.example.com")
-		if !assert.NoError(t, err) {
-			return
-		}
-		assert.NotNil(t, found)
-		assert.NotEqual(t, &certs[0], found)
-	})
-	t.Run("generate", func(t *testing.T) {
-		certs := []tls.Certificate{}
-
-		found, err := GetCertificateForServerName(certs, "b.example.com")
-		if !assert.NoError(t, err) {
-			return
-		}
-		assert.NotNil(t, found)
-	})
-}
 
 func TestGetCertificateServerNames(t *testing.T) {
 	cert, err := GenerateSelfSignedCertificate("www.example.com")
