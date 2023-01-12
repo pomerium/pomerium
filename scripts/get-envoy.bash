e23aa3faa3bd6bccbd2aa495b166912e00ed053b
Caleb Doxsey <cdoxsey@pomerium.com>
2023-01-12 14:47:59 -0700

    scripts: update get-envoy script to download all binaries

--- scripts/get-envoy.bash
+++ scripts/get-envoy.bash
@@ -7,26 +7,27 @@ export PATH
 _project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/.."
 _envoy_version=1.24.0
 _dir="$_project_root/pkg/envoy/files"
-_target="${TARGET:-"$(go env GOOS)-$(go env GOARCH)"}"
 
-_url="https://github.com/pomerium/envoy-binaries/releases/download/v${_envoy_version}/envoy-${_target}"
+for _target in darwin-amd64 darwin-arm64 linux-amd64 linux-arm64; do
+  _url="https://github.com/pomerium/envoy-binaries/releases/download/v${_envoy_version}/envoy-${_target}"
 
-curl \
-  --silent \
-  --fail \
-  --compressed \
-  --location \
-  --time-cond "$_dir/envoy-$_target" \
-  --output "$_dir/envoy-$_target" \
-  "$_url"
+  curl \
+    --silent \
+    --fail \
+    --compressed \
+    --location \
+    --time-cond "$_dir/envoy-$_target" \
+    --output "$_dir/envoy-$_target" \
+    "$_url"
 
-curl \
-  --silent \
-  --fail \
-  --compressed \
-  --location \
-  --time-cond "$_dir/envoy-$_target.sha256" \
-  --output "$_dir/envoy-$_target.sha256" \
-  "$_url.sha256"
+  curl \
+    --silent \
+    --fail \
+    --compressed \
+    --location \
+    --time-cond "$_dir/envoy-$_target.sha256" \
+    --output "$_dir/envoy-$_target.sha256" \
+    "$_url.sha256"
 
-echo "$_envoy_version" >"$_dir/envoy-$_target.version"
+  echo "$_envoy_version" >"$_dir/envoy-$_target.version"
+done
