f3c505514d0084a472f87d797405b347737cff49
github-action-benchmark <github@users.noreply.github.com>
2023-01-17 02:50:38 +0000

    add Benchmark (go) benchmark result for e5bbb0c3247e688186b663b911256d4ed8bd3afd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673921141780,
+  "lastUpdate": 1673923838221,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -29880,6 +29880,42 @@ window.BENCHMARK_DATA = {
             "value": 9955064,
             "unit": "ns/op",
             "extra": "114 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e5bbb0c3247e688186b663b911256d4ed8bd3afd",
+          "message": "chore(deps): bump google.golang.org/api from 0.105.0 to 0.107.0 (#3897)\n\nBumps [google.golang.org/api](https://github.com/googleapis/google-api-go-client) from 0.105.0 to 0.107.0.\r\n- [Release notes](https://github.com/googleapis/google-api-go-client/releases)\r\n- [Changelog](https://github.com/googleapis/google-api-go-client/blob/main/CHANGES.md)\r\n- [Commits](https://github.com/googleapis/google-api-go-client/compare/v0.105.0...v0.107.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: google.golang.org/api\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-01-16T21:47:58-05:00",
+          "tree_id": "0f5d84e22e73e6fc403fbc4a141fee9a859c1435",
+          "url": "https://github.com/pomerium/pomerium/commit/e5bbb0c3247e688186b663b911256d4ed8bd3afd"
+        },
+        "date": 1673923837576,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 12782628,
+            "unit": "ns/op",
+            "extra": "87 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 8245843,
+            "unit": "ns/op",
+            "extra": "145 times\n2 procs"
           }
         ]
       }
