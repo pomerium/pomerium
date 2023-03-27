bd497c2758246611410b1975d85a6c0a8d3e75e4
github-action-benchmark <github@users.noreply.github.com>
2023-03-27 23:02:40 +0000

    add Benchmark (go) benchmark result for b96def70bab7bdfe4f4f38603f076e067ba49a1a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679956885544,
+  "lastUpdate": 1679958160211,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -39644,6 +39644,42 @@ window.BENCHMARK_DATA = {
             "value": 8174997,
             "unit": "ns/op",
             "extra": "150 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b96def70bab7bdfe4f4f38603f076e067ba49a1a",
+          "message": "chore(deps): bump github.com/minio/minio-go/v7 from 7.0.47 to 7.0.50 (#4081)\n\nBumps [github.com/minio/minio-go/v7](https://github.com/minio/minio-go) from 7.0.47 to 7.0.50.\r\n- [Release notes](https://github.com/minio/minio-go/releases)\r\n- [Commits](https://github.com/minio/minio-go/compare/v7.0.47...v7.0.50)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: github.com/minio/minio-go/v7\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-03-27T17:00:29-06:00",
+          "tree_id": "087cb3e2037c5077d69fa1f3e74715fb35272e2f",
+          "url": "https://github.com/pomerium/pomerium/commit/b96def70bab7bdfe4f4f38603f076e067ba49a1a"
+        },
+        "date": 1679958159801,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 12893773,
+            "unit": "ns/op",
+            "extra": "100 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 7849382,
+            "unit": "ns/op",
+            "extra": "146 times\n2 procs"
           }
         ]
       }
