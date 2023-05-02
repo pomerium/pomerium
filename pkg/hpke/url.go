9e467c43a083c8d250a35fa61cd697e28bf53ad2
Caleb Doxsey <cdoxsey@pomerium.com>
2023-05-02 09:05:54 -0600

    hpke: compress query string

--- pkg/hpke/url.go
+++ pkg/hpke/url.go
@@ -3,17 +3,30 @@ package hpke
 import (
 	"fmt"
 	"net/url"
+
+	"github.com/klauspost/compress/zstd"
 )
 
 // URL Parameters
 const (
-	ParamSenderPublicKey = "pomerium_hpke_sender_pub"
-	ParamQuery           = "pomerium_hpke_query"
+	paramSenderPublicKey = "pomerium_hpke_sender_pub"
+	paramQuery           = "pomerium_hpke_query"
+
+	paramSenderPublicKeyV2 = "k"
+	paramQueryV2           = "q"
 )
 
 // IsEncryptedURL returns true if the url.Values contain an HPKE encrypted query.
 func IsEncryptedURL(values url.Values) bool {
-	return values.Has(ParamSenderPublicKey) && values.Has(ParamQuery)
+	return isEncryptedURLV1(values) || isEncryptedURLV2(values)
+}
+
+func isEncryptedURLV1(values url.Values) bool {
+	return values.Has(paramSenderPublicKey) && values.Has(paramQuery)
+}
+
+func isEncryptedURLV2(values url.Values) bool {
+	return values.Has(paramSenderPublicKeyV2) && values.Has(paramQueryV2)
 }
 
 // EncryptURLValues encrypts URL values using the Seal method.
@@ -22,16 +35,46 @@ func EncryptURLValues(
 	receiverPublicKey *PublicKey,
 	values url.Values,
 ) (encrypted url.Values, err error) {
+	return encryptURLValuesV2(senderPrivateKey, receiverPublicKey, values)
+}
+
+func encryptURLValuesV1(
+	senderPrivateKey *PrivateKey,
+	receiverPublicKey *PublicKey,
+	values url.Values,
+) (encrypted url.Values, err error) {
 	values = withoutHPKEParams(values)
 
-	sealed, err := Seal(senderPrivateKey, receiverPublicKey, []byte(values.Encode()))
+	encoded := encodeQueryStringV1(values)
+
+	sealed, err := Seal(senderPrivateKey, receiverPublicKey, encoded)
 	if err != nil {
 		return nil, fmt.Errorf("hpke: failed to seal URL values %w", err)
 	}
 
 	return url.Values{
-		ParamSenderPublicKey: {senderPrivateKey.PublicKey().String()},
-		ParamQuery:           {encode(sealed)},
+		paramSenderPublicKey: {senderPrivateKey.PublicKey().String()},
+		paramQuery:           {encode(sealed)},
+	}, nil
+}
+
+func encryptURLValuesV2(
+	senderPrivateKey *PrivateKey,
+	receiverPublicKey *PublicKey,
+	values url.Values,
+) (encrypted url.Values, err error) {
+	values = withoutHPKEParams(values)
+
+	encoded := encodeQueryStringV2(values)
+
+	sealed, err := Seal(senderPrivateKey, receiverPublicKey, encoded)
+	if err != nil {
+		return nil, fmt.Errorf("hpke: failed to seal URL values %w", err)
+	}
+
+	return url.Values{
+		paramSenderPublicKeyV2: {senderPrivateKey.PublicKey().String()},
+		paramQueryV2:           {encode(sealed)},
 	}, nil
 }
 
@@ -40,31 +83,50 @@ func DecryptURLValues(
 	receiverPrivateKey *PrivateKey,
 	encrypted url.Values,
 ) (senderPublicKey *PublicKey, values url.Values, err error) {
-	if !encrypted.Has(ParamSenderPublicKey) {
-		return nil, nil, fmt.Errorf("hpke: missing sender public key in query parameters")
-	}
-	if !encrypted.Has(ParamQuery) {
-		return nil, nil, fmt.Errorf("hpke: missing encrypted query in query parameters")
-	}
+	var decrypted url.Values
+	switch {
+	case isEncryptedURLV1(encrypted):
+		senderPublicKey, err = PublicKeyFromString(encrypted.Get(paramSenderPublicKey))
+		if err != nil {
+			return nil, nil, fmt.Errorf("hpke: invalid sender public key parameter: %w", err)
+		}
 
-	senderPublicKey, err = PublicKeyFromString(encrypted.Get(ParamSenderPublicKey))
-	if err != nil {
-		return nil, nil, fmt.Errorf("hpke: invalid sender public key parameter: %w", err)
-	}
+		sealed, err := decode(encrypted.Get(paramQuery))
+		if err != nil {
+			return nil, nil, fmt.Errorf("hpke: failed decoding query parameter: %w", err)
+		}
 
-	sealed, err := decode(encrypted.Get(ParamQuery))
-	if err != nil {
-		return nil, nil, fmt.Errorf("hpke: failed decoding query parameter: %w", err)
-	}
+		message, err := Open(receiverPrivateKey, senderPublicKey, sealed)
+		if err != nil {
+			return nil, nil, fmt.Errorf("hpke: failed to open sealed message: %w", err)
+		}
 
-	message, err := Open(receiverPrivateKey, senderPublicKey, sealed)
-	if err != nil {
-		return nil, nil, fmt.Errorf("hpke: failed to open sealed message: %w", err)
-	}
+		decrypted, err = decodeQueryStringV1(message)
+		if err != nil {
+			return nil, nil, fmt.Errorf("hpke: invalid query parameter: %w", err)
+		}
+	case isEncryptedURLV2(encrypted):
+		senderPublicKey, err = PublicKeyFromString(encrypted.Get(paramSenderPublicKeyV2))
+		if err != nil {
+			return nil, nil, fmt.Errorf("hpke: invalid sender public key parameter: %w", err)
+		}
 
-	decrypted, err := url.ParseQuery(string(message))
-	if err != nil {
-		return nil, nil, fmt.Errorf("hpke: invalid query parameter: %w", err)
+		sealed, err := decode(encrypted.Get(paramQueryV2))
+		if err != nil {
+			return nil, nil, fmt.Errorf("hpke: failed decoding query parameter: %w", err)
+		}
+
+		message, err := Open(receiverPrivateKey, senderPublicKey, sealed)
+		if err != nil {
+			return nil, nil, fmt.Errorf("hpke: failed to open sealed message: %w", err)
+		}
+
+		decrypted, err = decodeQueryStringV2(message)
+		if err != nil {
+			return nil, nil, fmt.Errorf("hpke: invalid query parameter: %w", err)
+		}
+	default:
+		return nil, nil, fmt.Errorf("hpke: missing query parameters")
 	}
 
 	values = withoutHPKEParams(encrypted)
@@ -78,9 +140,33 @@ func DecryptURLValues(
 func withoutHPKEParams(values url.Values) url.Values {
 	filtered := make(url.Values)
 	for k, vs := range values {
-		if k != ParamSenderPublicKey && k != ParamQuery {
+		if k != paramSenderPublicKey && k != paramQuery && k != paramSenderPublicKeyV2 && k != paramQueryV2 {
 			filtered[k] = vs
 		}
 	}
 	return filtered
+}
+
+var zstdEncoder, _ = zstd.NewWriter(nil, zstd.WithEncoderLevel(zstd.SpeedBestCompression))
+
+func encodeQueryStringV1(values url.Values) []byte {
+	return []byte(values.Encode())
+}
+
+func encodeQueryStringV2(values url.Values) []byte {
+	return zstdEncoder.EncodeAll([]byte(values.Encode()), nil)
+}
+
+var zstdDecoder, _ = zstd.NewReader(nil)
+
+func decodeQueryStringV1(raw []byte) (url.Values, error) {
+	return url.ParseQuery(string(raw))
+}
+
+func decodeQueryStringV2(raw []byte) (url.Values, error) {
+	bs, err := zstdDecoder.DecodeAll(raw, nil)
+	if err != nil {
+		return nil, err
+	}
+	return url.ParseQuery(string(bs))
 }
