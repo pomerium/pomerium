b97e479d9c9f831f34b6858e459125dd3d073808
github-action-benchmark <github@users.noreply.github.com>
2023-05-08 16:40:23 +0000

    add Benchmark (go) benchmark result for a83c4e547077ab8a8f4c1bc823e157d46780c7ab

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1683564013535,
+  "lastUpdate": 1683564023214,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -44144,6 +44144,42 @@ window.BENCHMARK_DATA = {
             "value": 9537559,
             "unit": "ns/op",
             "extra": "120 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a83c4e547077ab8a8f4c1bc823e157d46780c7ab",
+          "message": "chore(deps): bump github.com/envoyproxy/protoc-gen-validate from 0.10.1 to 1.0.0 (#4155)\n\nchore(deps): bump github.com/envoyproxy/protoc-gen-validate\r\n\r\nBumps [github.com/envoyproxy/protoc-gen-validate](https://github.com/envoyproxy/protoc-gen-validate) from 0.10.1 to 1.0.0.\r\n- [Release notes](https://github.com/envoyproxy/protoc-gen-validate/releases)\r\n- [Changelog](https://github.com/bufbuild/protoc-gen-validate/blob/main/.goreleaser.yaml)\r\n- [Commits](https://github.com/envoyproxy/protoc-gen-validate/compare/v0.10.1...v1.0.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: github.com/envoyproxy/protoc-gen-validate\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-major\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-05-08T10:36:29-06:00",
+          "tree_id": "3458696b734e3473406593a9f5cfa354cebdcc1b",
+          "url": "https://github.com/pomerium/pomerium/commit/a83c4e547077ab8a8f4c1bc823e157d46780c7ab"
+        },
+        "date": 1683564022316,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 13156424,
+            "unit": "ns/op",
+            "extra": "84 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 8354523,
+            "unit": "ns/op",
+            "extra": "139 times\n2 procs"
           }
         ]
       }
