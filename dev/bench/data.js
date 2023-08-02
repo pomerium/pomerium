128a096b21767b42c12b1cfe0a0e0229565b54d6
github-action-benchmark <github@users.noreply.github.com>
2023-08-02 15:35:41 +0000

    add Benchmark (go) benchmark result for 3836928c97e7f77f75b6b552bcddee184769f5d8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690990504179,
+  "lastUpdate": 1690990541424,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -60912,6 +60912,42 @@ window.BENCHMARK_DATA = {
             "value": 9808645,
             "unit": "ns/op",
             "extra": "123 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3836928c97e7f77f75b6b552bcddee184769f5d8",
+          "message": "chore(deps): bump github.com/peterbourgon/ff/v3 from 3.3.2 to 3.4.0 (#4399)\n\nBumps [github.com/peterbourgon/ff/v3](https://github.com/peterbourgon/ff) from 3.3.2 to 3.4.0.\r\n- [Release notes](https://github.com/peterbourgon/ff/releases)\r\n- [Commits](https://github.com/peterbourgon/ff/compare/v3.3.2...v3.4.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: github.com/peterbourgon/ff/v3\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-08-02T09:29:32-06:00",
+          "tree_id": "279e5cd446bcad9ac748c688588aeb770b00a1d8",
+          "url": "https://github.com/pomerium/pomerium/commit/3836928c97e7f77f75b6b552bcddee184769f5d8"
+        },
+        "date": 1690990535248,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 19270333,
+            "unit": "ns/op",
+            "extra": "70 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 75324523,
+            "unit": "ns/op",
+            "extra": "78 times\n2 procs"
           }
         ]
       }
