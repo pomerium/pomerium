8eb102b0fcff04d9dc7e741a09634cab7ece0884
github-action-benchmark <github@users.noreply.github.com>
2023-08-02 15:09:39 +0000

    add Benchmark (go) benchmark result for fa78140033fa1a54ad6e2b0942133b86e80f7f3b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690988817177,
+  "lastUpdate": 1690988979850,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -60480,6 +60480,42 @@ window.BENCHMARK_DATA = {
             "value": 12158069,
             "unit": "ns/op",
             "extra": "91 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fa78140033fa1a54ad6e2b0942133b86e80f7f3b",
+          "message": "chore(deps): bump github.com/open-policy-agent/opa from 0.54.0 to 0.55.0 (#4404)\n\nBumps [github.com/open-policy-agent/opa](https://github.com/open-policy-agent/opa) from 0.54.0 to 0.55.0.\r\n- [Release notes](https://github.com/open-policy-agent/opa/releases)\r\n- [Changelog](https://github.com/open-policy-agent/opa/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/open-policy-agent/opa/compare/v0.54.0...v0.55.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: github.com/open-policy-agent/opa\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-08-02T09:04:40-06:00",
+          "tree_id": "5c72c96410c6b8d5c0444d0a80a5f1778e409d50",
+          "url": "https://github.com/pomerium/pomerium/commit/fa78140033fa1a54ad6e2b0942133b86e80f7f3b"
+        },
+        "date": 1690988974329,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 15928159,
+            "unit": "ns/op",
+            "extra": "81 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 11138646,
+            "unit": "ns/op",
+            "extra": "100 times\n2 procs"
           }
         ]
       }
