c167ac9d40b37c3a0840116fda1544051d579108
Denis Mishin <dmishin@pomerium.com>
2023-08-16 15:42:30 -0400

    add test for bundles

--- internal/zero/reconciler/bundles.go
+++ internal/zero/reconciler/bundles.go
@@ -1,80 +1,124 @@
 package reconciler
 
-/*
- * Bundle is a representation of a bundle resource
- *
- */
-
 import (
+	"container/heap"
 	"sync"
 )
 
 type bundle struct {
-	id     string
-	synced bool
+	id       string
+	synced   bool
+	priority int
 }
 
-// Bundles is a list of bundles to sync
+type bundleHeap []bundle
+
+func (h bundleHeap) Len() int { return len(h) }
+func (h bundleHeap) Less(i, j int) bool {
+	// If one is synced and the other is not, the unsynced one comes first
+	if h[i].synced != h[j].synced {
+		return !h[i].synced
+	}
+	// Otherwise, the one with the lower priority comes first
+	return h[i].priority < h[j].priority
+}
+
+func (h bundleHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *bundleHeap) Push(x interface{}) {
+	item := x.(bundle)
+	*h = append(*h, item)
+}
+
+func (h *bundleHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// Bundles is a priority queue of bundles to sync.
 type Bundles struct {
 	sync.Mutex
-	bundles []bundle
+	bundles bundleHeap
+	counter int // to assign priorities based on order of insertion
 }
 
-// Set sets the list of bundles to sync.
-// bundles would be synced in the order they are provided.
+// Set sets the bundles to be synced. This will reset the sync status of all bundles.
 func (b *Bundles) Set(bundles []string) {
 	b.Lock()
 	defer b.Unlock()
 
-	b.bundles = make([]bundle, len(bundles))
+	b.bundles = make(bundleHeap, len(bundles))
+	b.counter = len(bundles)
 	for i, id := range bundles {
-		b.bundles[i] = bundle{id: id, synced: false}
+		b.bundles[i] = bundle{
+			id:       id,
+			synced:   false,
+			priority: i,
+		}
 	}
+	heap.Init(&b.bundles)
 }
 
-// MarkForSync marks the bundle with the given ID for synchronization
-// if bundle does not exist, it is added to the end of the list as we do not know its relative priority.
-// we will have just a handful of bundles, so it is not a big deal to scan the list on each (infrequent) update.
+// MarkForSync marks the bundle with the given ID for syncing.
 func (b *Bundles) MarkForSync(id string) {
 	b.Lock()
 	defer b.Unlock()
 
-	for i := range b.bundles {
-		if b.bundles[i].id == id {
+	for i, bundle := range b.bundles {
+		if bundle.id == id {
 			b.bundles[i].synced = false
+			heap.Fix(&b.bundles, i)
 			return
 		}
 	}
 
-	b.bundles = append(b.bundles, bundle{id: id, synced: false})
+	newBundle := bundle{id: id, synced: false, priority: b.counter}
+	heap.Push(&b.bundles, newBundle)
+	b.counter++
 }
 
-// MarkForSyncLater marks the bundle with the given ID for synchronization
-// by moving it to the end of the list
+// MarkForSyncLater marks the bundle with the given ID for syncing later (after all other bundles).
 func (b *Bundles) MarkForSyncLater(id string) {
 	b.Lock()
 	defer b.Unlock()
 
-	for i := range b.bundles {
-		if b.bundles[i].id == id {
-			b.bundles[i].synced = false
-			b.bundles[i], b.bundles[len(b.bundles)-1] = b.bundles[len(b.bundles)-1], b.bundles[i]
-			return
+	for i, bundle := range b.bundles {
+		if bundle.id != id {
+			continue
 		}
+
+		// Increase the counter first to ensure that this bundle has the highest (last) priority.
+		b.counter++
+		b.bundles[i].synced = false
+		b.bundles[i].priority = b.counter
+		heap.Fix(&b.bundles, i)
+		return
 	}
 }
 
-// GetNextBundleToSync returns the next bundle to sync.
-// If there is no bundle to sync, it returns false.
+// GetNextBundleToSync returns the ID of the next bundle to sync and whether there is one.
 func (b *Bundles) GetNextBundleToSync() (string, bool) {
 	b.Lock()
 	defer b.Unlock()
 
-	for i, bundle := range b.bundles {
-		if !bundle.synced {
-			b.bundles[i].synced = true
-			return bundle.id, true
-		}
+	if len(b.bundles) == 0 {
+		return "", false
 	}
-	return "", false
+
+	// Check the top bundle without popping
+	if b.bundles[0].synced {
+		return "", false
+	}
+
+	// Mark the top bundle as synced and push it to the end
+	id := b.bundles[0].id
+	b.bundles[0].synced = true
+	b.bundles[0].priority = b.counter
+	heap.Fix(&b.bundles, 0)
+	b.counter++
+
+	return id, true
 }
