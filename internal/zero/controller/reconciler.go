9ef67eff915a842c36b634fd71d70343b30b2099
Denis Mishin <dmishin@pomerium.com>
2023-08-17 14:20:49 -0400

    rm debug log

--- internal/zero/controller/reconciler.go
+++ internal/zero/controller/reconciler.go
@@ -2,26 +2,18 @@ package controller
 
 import (
 	"context"
-	"time"
 
-	"github.com/pomerium/pomerium/internal/log"
 	"github.com/pomerium/pomerium/internal/zero/reconciler"
 	"github.com/pomerium/pomerium/pkg/grpc/databroker"
 )
 
-var now time.Time // for testing
-
 func (c *controller) RunReconciler(ctx context.Context) error {
-	now = time.Now()
 	leaser := databroker.NewLeaser("zero-reconciler", c.cfg.reconcilerLeaseDuration, c)
 	return leaser.Run(ctx)
 }
 
 // RunLeased implements the databroker.Leaser interface.
 func (c *controller) RunLeased(ctx context.Context) error {
-	log.Ctx(ctx).Info().
-		Str("lease acquired in", time.Since(now).String()).
-		Msg("starting reconciler (lease acquired)")
 	return reconciler.Run(ctx,
 		reconciler.WithAPI(c.api),
 		reconciler.WithDataBrokerClient(c.GetDataBrokerServiceClient()),
