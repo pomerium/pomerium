751be55896c8f9159a666241e1277ce3303c4344
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2023-09-14 10:15:03 -0700

    cryptutil: remove unused methods
    Remove the unused methods Sign() and Verify().

--- pkg/cryptutil/sign.go
+++ pkg/cryptutil/sign.go
@@ -4,8 +4,6 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"crypto/sha256"
-	"math/big"
 )
 
 // NewSigningKey generates a random P-256 ECDSA private key.
@@ -13,38 +11,4 @@ import (
 // while its P-384 and P-521 are not.
 func NewSigningKey() (*ecdsa.PrivateKey, error) {
 	return ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-}
-
-// Sign signs arbitrary data using ECDSA.
-func Sign(data []byte, privkey *ecdsa.PrivateKey) ([]byte, error) {
-	// hash message
-	digest := sha256.Sum256(data)
-
-	// sign the hash
-	r, s, err := ecdsa.Sign(rand.Reader, privkey, digest[:])
-	if err != nil {
-		return nil, err
-	}
-
-	// encode the signature {R, S}
-	// big.Int.Bytes() will need padding in the case of leading zero bytes
-	params := privkey.Curve.Params()
-	curveOrderByteSize := params.P.BitLen() / 8
-	rBytes, sBytes := r.Bytes(), s.Bytes()
-	signature := make([]byte, curveOrderByteSize*2)
-	copy(signature[curveOrderByteSize-len(rBytes):], rBytes)
-	copy(signature[curveOrderByteSize*2-len(sBytes):], sBytes)
-	return signature, nil
-}
-
-// Verify checks a raw ECDSA signature.
-// Returns true if it's valid and false if not.
-func Verify(data, signature []byte, pubkey *ecdsa.PublicKey) bool {
-	// hash message
-	digest := sha256.Sum256(data)
-	curveOrderByteSize := pubkey.Curve.Params().P.BitLen() / 8
-	r, s := new(big.Int), new(big.Int)
-	r.SetBytes(signature[:curveOrderByteSize])
-	s.SetBytes(signature[curveOrderByteSize:])
-	return ecdsa.Verify(pubkey, digest[:], r, s)
 }
