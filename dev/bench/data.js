869f5355fd1bd303fcf46fd3a14d60522bda33d3
github-action-benchmark <github@users.noreply.github.com>
2023-09-20 14:28:19 +0000

    add Benchmark (go) benchmark result for f313ff104b1827c1ab9ffccfaf3def56c40eac3f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695168859872,
+  "lastUpdate": 1695220099276,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -70782,6 +70782,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkLoggedOutUserAccess",
             "value": 11170401,
+            "unit": "ns/op",
+            "extra": "100 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f313ff104b1827c1ab9ffccfaf3def56c40eac3f",
+          "message": "chore(deps): bump @mui/icons-material from 5.3.1 to 5.14.9 in /ui (#4567)\n\nBumps [@mui/icons-material](https://github.com/mui/material-ui/tree/HEAD/packages/mui-icons-material) from 5.3.1 to 5.14.9.\r\n- [Release notes](https://github.com/mui/material-ui/releases)\r\n- [Changelog](https://github.com/mui/material-ui/blob/master/CHANGELOG.md)\r\n- [Commits](https://github.com/mui/material-ui/commits/v5.14.9/packages/mui-icons-material)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: \"@mui/icons-material\"\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-09-20T08:22:23-06:00",
+          "tree_id": "2de6a661653ae03a9eab6bd649a7f859d662dfea",
+          "url": "https://github.com/pomerium/pomerium/commit/f313ff104b1827c1ab9ffccfaf3def56c40eac3f"
+        },
+        "date": 1695220088734,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 16004162,
+            "unit": "ns/op",
+            "extra": "72 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 10903566,
             "unit": "ns/op",
             "extra": "100 times\n2 procs"
           }
