5e42bfbb342c7c8acc69fd2e0036c3ab76eafa2c
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2023-10-31 09:27:33 -0700

    storage/inmemory: implement patch operation
    Add a new Patch() method that updates specific fields of an existing
    record's data, based on a field mask.
    
    Extract some logic from the existing Get() and Put() methods so it can
    be shared with the new Patch() method.

---
+++ pkg/storage/patch.go
@@ -0,0 +1,35 @@
+package storage
+
+import (
+	"fmt"
+
+	"github.com/pomerium/pomerium/pkg/grpc/databroker"
+	"github.com/pomerium/pomerium/pkg/protoutil"
+	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+)
+
+// PatchRecord extracts the data from existing and record, updates the existing
+// data subject to the provided field mask, and stores the result back into
+// record. The existing record is not modified.
+func PatchRecord(existing, record *databroker.Record, fields *fieldmaskpb.FieldMask) error {
+	dst, err := existing.GetData().UnmarshalNew()
+	if err != nil {
+		return fmt.Errorf("could not unmarshal existing record data: %w", err)
+	}
+
+	src, err := record.GetData().UnmarshalNew()
+	if err != nil {
+		return fmt.Errorf("could not unmarshal new record data: %w", err)
+	}
+
+	if err := protoutil.OverwriteMasked(dst, src, fields); err != nil {
+		return fmt.Errorf("cannot patch record: %w", err)
+	}
+
+	record.Data, err = anypb.New(dst)
+	if err != nil {
+		return fmt.Errorf("could not marshal new record data: %w", err)
+	}
+	return nil
+}
