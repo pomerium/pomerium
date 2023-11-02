3b0281c89f4c2dcb01e8d343aa8294fec7724822
github-action-benchmark <github@users.noreply.github.com>
2023-11-02 17:34:13 +0000

    add Benchmark (go) benchmark result for 5f4e13e1300461508491056ad784e504c5c6ccfe

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698946290198,
+  "lastUpdate": 1698946453350,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -77216,6 +77216,42 @@ window.BENCHMARK_DATA = {
             "value": 9286678,
             "unit": "ns/op",
             "extra": "133 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cdoxsey@pomerium.com",
+            "name": "Caleb Doxsey",
+            "username": "calebdoxsey"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5f4e13e1300461508491056ad784e504c5c6ccfe",
+          "message": "core/events: fix flaky test (#4700)\n\n* core/events: use synchronous channel to fix flaky test\r\n\r\n* switch to mutexes\r\n\r\n* remove test",
+          "timestamp": "2023-11-02T11:28:28-06:00",
+          "tree_id": "ce9c0a6e740e37e00bdbe6bc685cdd9a54bc9129",
+          "url": "https://github.com/pomerium/pomerium/commit/5f4e13e1300461508491056ad784e504c5c6ccfe"
+        },
+        "date": 1698946438176,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 18705627,
+            "unit": "ns/op",
+            "extra": "67 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 11968520,
+            "unit": "ns/op",
+            "extra": "98 times\n2 procs"
           }
         ]
       }
