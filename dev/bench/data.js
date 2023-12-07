13a865d5a05ad0c887f03d1822d6ebfa6466bf8a
github-action-benchmark <github@users.noreply.github.com>
2023-12-07 21:48:25 +0000

    add Benchmark (go) benchmark result for b9c56074aa11af90130472d75f731e21bbe33195

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701985485921,
+  "lastUpdate": 1701985705531,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -88352,6 +88352,42 @@ window.BENCHMARK_DATA = {
             "value": 6765037,
             "unit": "ns/op",
             "extra": "164 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b9c56074aa11af90130472d75f731e21bbe33195",
+          "message": "chore(deps): bump github.com/coreos/go-oidc/v3 from 3.6.0 to 3.8.0 (#4791)\n\nBumps [github.com/coreos/go-oidc/v3](https://github.com/coreos/go-oidc) from 3.6.0 to 3.8.0.\r\n- [Release notes](https://github.com/coreos/go-oidc/releases)\r\n- [Commits](https://github.com/coreos/go-oidc/compare/v3.6.0...v3.8.0)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: github.com/coreos/go-oidc/v3\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2023-12-07T14:45:25-07:00",
+          "tree_id": "762f92a19836d7c723dff437e9fd4218f37fb834",
+          "url": "https://github.com/pomerium/pomerium/commit/b9c56074aa11af90130472d75f731e21bbe33195"
+        },
+        "date": 1701985694297,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 10201216,
+            "unit": "ns/op",
+            "extra": "100 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 6236833,
+            "unit": "ns/op",
+            "extra": "187 times\n4 procs"
           }
         ]
       }
