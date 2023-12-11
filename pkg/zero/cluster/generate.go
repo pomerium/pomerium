c5f7519c061640b4ea21a4b97c036d537532bca9
Denis Mishin <dmishin@pomerium.com>
2023-12-11 18:24:44 -0500

    zero/openapi: pin v1.0.0 of a runtime (#4851)

--- pkg/zero/cluster/generate.go
+++ pkg/zero/cluster/generate.go
@@ -1,5 +1,5 @@
 package cluster
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=models.yaml openapi.yaml
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=server.yaml openapi.yaml
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=client.yaml openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.16.2 --config=models.yaml openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.16.2 --config=server.yaml openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.16.2 --config=client.yaml openapi.yaml
