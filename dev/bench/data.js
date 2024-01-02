47fe2a541b77e8badbfb883ef8a0c296adea76c9
github-action-benchmark <github@users.noreply.github.com>
2024-01-02 18:09:07 +0000

    add Benchmark (go) benchmark result for 4e9119d156680d084381858130a813f1855e21ce

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1704218915485,
+  "lastUpdate": 1704218947753,
   "repoUrl": "https://github.com/pomerium/pomerium",
   "entries": {
     "Benchmark": [
@@ -92236,6 +92236,42 @@ window.BENCHMARK_DATA = {
             "value": 7223272,
             "unit": "ns/op",
             "extra": "163 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4e9119d156680d084381858130a813f1855e21ce",
+          "message": "chore(deps): bump node from `445acd9` to `8d0f16f` (#4881)\n\nBumps node from `445acd9` to `8d0f16f`.\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: node\r\n  dependency-type: direct:production\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-01-02T11:06:05-07:00",
+          "tree_id": "742a2a3262d70f8b8cf99ab93901561c774b6d33",
+          "url": "https://github.com/pomerium/pomerium/commit/4e9119d156680d084381858130a813f1855e21ce"
+        },
+        "date": 1704218940939,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLoggedInUserAccess",
+            "value": 8782864,
+            "unit": "ns/op",
+            "extra": "138 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkLoggedOutUserAccess",
+            "value": 6202485,
+            "unit": "ns/op",
+            "extra": "190 times\n4 procs"
           }
         ]
       }
