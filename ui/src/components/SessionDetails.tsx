2dbcf32cc68c804de50a8a819f06eba5f6d70c9f
Caleb Doxsey <cdoxsey@pomerium.com>
2024-01-08 07:03:49 -0700

    core/proxy: handle missing session for user info endpoint (#4769)

--- ui/src/components/SessionDetails.tsx
+++ ui/src/components/SessionDetails.tsx
@@ -1,3 +1,5 @@
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 import Stack from "@mui/material/Stack";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -20,50 +22,66 @@ export const SessionDetails: FC<SessionDetailsProps> = ({
   profile,
 }) => {
   return (
-    <Section title="User Details">
-      <Stack spacing={3}>
-        <TableContainer>
-          <Table size="small">
-            <TableBody>
-              <TableRow>
-                <TableCell width={"18%"} variant="head">
-                  Session ID
-                </TableCell>
-                <TableCell align="left">
-                  <IDField value={session?.id} />
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell variant="head">User ID</TableCell>
-                <TableCell align="left">
-                  <IDField
-                    value={session?.userId || `${profile?.claims?.sub}`}
-                  />
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell variant="head">Expires At</TableCell>
-                <TableCell align="left">{session?.expiresAt || ""}</TableCell>
-              </TableRow>
-              {Object.entries(session?.claims || {}).map(([key, values]) => (
-                <ClaimRow
-                  key={`session/${key}`}
-                  claimKey={key}
-                  claimValue={values}
-                />
-              ))}
-              {Object.entries(profile?.claims || {}).map(([key, value]) => (
-                <ClaimRow
-                  key={`profile/${key}`}
-                  claimKey={key}
-                  claimValue={value}
-                />
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Stack>
-    </Section>
+    <>
+      {session?.id ? (
+        <Section title="User Details">
+          <Stack spacing={3}>
+            <TableContainer>
+              <Table size="small">
+                <TableBody>
+                  <TableRow>
+                    <TableCell width={"18%"} variant="head">
+                      Session ID
+                    </TableCell>
+                    <TableCell align="left">
+                      <IDField value={session?.id} />
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell variant="head">User ID</TableCell>
+                    <TableCell align="left">
+                      <IDField
+                        value={
+                          session?.userId || `${profile?.claims?.sub || ""}`
+                        }
+                      />
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell variant="head">Expires At</TableCell>
+                    <TableCell align="left">
+                      {session?.expiresAt || ""}
+                    </TableCell>
+                  </TableRow>
+                  {Object.entries(session?.claims || {}).map(
+                    ([key, values]) => (
+                      <ClaimRow
+                        key={`session/${key}`}
+                        claimKey={key}
+                        claimValue={values}
+                      />
+                    )
+                  )}
+                  {Object.entries(profile?.claims || {}).map(([key, value]) => (
+                    <ClaimRow
+                      key={`profile/${key}`}
+                      claimKey={key}
+                      claimValue={value}
+                    />
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Stack>
+        </Section>
+      ) : (
+        <Alert severity="warning">
+          <AlertTitle>User Details Not Available</AlertTitle>
+          Have you signed in yet? <br />
+          <a href="/">{location.origin}</a>.
+        </Alert>
+      )}
+    </>
   );
 };
 export default SessionDetails;
