1080a33443c1966eb710ed5c0c8f810c841e547c
Caleb Doxsey <cdoxsey@pomerium.com>
2024-01-12 13:28:33 -0700

    core/zero: update oapi-codegen (#4898)

--- pkg/zero/cluster/generate.go
+++ pkg/zero/cluster/generate.go
@@ -1,5 +1,5 @@
 package cluster
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.16.2 --config=models.yaml openapi.yaml
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.16.2 --config=server.yaml openapi.yaml
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.16.2 --config=client.yaml openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.0.0 --config=models.yaml openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.0.0 --config=server.yaml openapi.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.0.0 --config=client.yaml openapi.yaml
