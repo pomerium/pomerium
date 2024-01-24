2a9e76eb7ca05c1c762c3a473b651e6cfeae06ae
Caleb Doxsey <cdoxsey@pomerium.com>
2024-01-24 15:53:44 -0700

    core/envoy: clean up temporary directory on start (#4914)

---
+++ pkg/envoy/extract_test.go
@@ -0,0 +1,31 @@
+package envoy
+
+import (
+	"io/fs"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClean(t *testing.T) {
+	t.Parallel()
+
+	tmpDir := t.TempDir()
+	d1, err := os.MkdirTemp(tmpDir, envoyPrefix)
+	require.NoError(t, err)
+	d2, err := os.MkdirTemp(tmpDir, envoyPrefix)
+	require.NoError(t, err)
+	d3, err := os.MkdirTemp(tmpDir, envoyPrefix)
+	require.NoError(t, err)
+
+	cleanTempDir(tmpDir)
+
+	_, err = os.Stat(d1)
+	assert.ErrorIs(t, err, fs.ErrNotExist)
+	_, err = os.Stat(d2)
+	assert.ErrorIs(t, err, fs.ErrNotExist)
+	_, err = os.Stat(d3)
+	assert.ErrorIs(t, err, fs.ErrNotExist)
+}
