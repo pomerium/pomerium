45776825931a501a92c40266884896fbc6c88490
Denis Mishin <dmishin@pomerium.com>
2024-05-01 14:47:30 -0400

    fix level check

--- internal/log/grpc.go
+++ internal/log/grpc.go
@@ -14,55 +14,55 @@ func init() {
 type grpcLogger struct{}
 
 func (c *grpcLogger) Info(args ...interface{}) {
-	if GetLevel() == zerolog.DebugLevel {
+	if GetLevel() <= zerolog.DebugLevel {
 		Logger().Info().Msg(fmt.Sprint(args...))
 	}
 }
 
 func (c *grpcLogger) Infoln(args ...interface{}) {
-	if GetLevel() == zerolog.DebugLevel {
+	if GetLevel() <= zerolog.DebugLevel {
 		Logger().Info().Msg(fmt.Sprintln(args...))
 	}
 }
 
 func (c *grpcLogger) Infof(format string, args ...interface{}) {
-	if GetLevel() == zerolog.DebugLevel {
+	if GetLevel() <= zerolog.DebugLevel {
 		Logger().Info().Msg(fmt.Sprintf(format, args...))
 	}
 }
 
 func (c *grpcLogger) Warning(args ...interface{}) {
-	if GetLevel() == zerolog.DebugLevel {
+	if GetLevel() <= zerolog.DebugLevel {
 		Logger().Warn().Msg(fmt.Sprint(args...))
 	}
 }
 
 func (c *grpcLogger) Warningln(args ...interface{}) {
-	if GetLevel() == zerolog.DebugLevel {
+	if GetLevel() <= zerolog.DebugLevel {
 		Logger().Warn().Msg(fmt.Sprintln(args...))
 	}
 }
 
 func (c *grpcLogger) Warningf(format string, args ...interface{}) {
-	if GetLevel() == zerolog.DebugLevel {
+	if GetLevel() <= zerolog.DebugLevel {
 		Logger().Warn().Msg(fmt.Sprintf(format, args...))
 	}
 }
 
 func (c *grpcLogger) Error(args ...interface{}) {
-	if GetLevel() == zerolog.DebugLevel {
+	if GetLevel() <= zerolog.DebugLevel {
 		Logger().Error().Msg(fmt.Sprint(args...))
 	}
 }
 
 func (c *grpcLogger) Errorln(args ...interface{}) {
-	if GetLevel() == zerolog.DebugLevel {
+	if GetLevel() <= zerolog.DebugLevel {
 		Logger().Error().Msg(fmt.Sprintln(args...))
 	}
 }
 
 func (c *grpcLogger) Errorf(format string, args ...interface{}) {
-	if GetLevel() == zerolog.DebugLevel {
+	if GetLevel() <= zerolog.DebugLevel {
 		Logger().Error().Msg(fmt.Sprintf(format, args...))
 	}
 }
