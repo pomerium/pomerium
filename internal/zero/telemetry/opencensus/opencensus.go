4591539bfdb0bebb230aaabfaade13280a8c082a
Denis Mishin <dmishin@pomerium.com>
2024-06-18 19:06:18 -0400

    use sets.Hash

--- internal/zero/telemetry/opencensus/opencensus.go
+++ internal/zero/telemetry/opencensus/opencensus.go
@@ -8,11 +8,13 @@ import (
 	"go.opentelemetry.io/otel/bridge/opencensus"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
+
+	"github.com/pomerium/pomerium/internal/sets"
 )
 
 type Producer struct {
 	producer metric.Producer
-	filter   atomic.Value
+	filter   atomic.Pointer[sets.Hash[string]]
 }
 
 var _ metric.Producer = (*Producer)(nil)
@@ -26,7 +28,7 @@ func New() *Producer {
 }
 
 func (p *Producer) Produce(ctx context.Context) ([]metricdata.ScopeMetrics, error) {
-	filter := p.loadFilter()
+	filter := p.filter.Load()
 	metrics, err := p.producer.Produce(ctx)
 	if err != nil {
 		return nil, err
@@ -35,7 +37,7 @@ func (p *Producer) Produce(ctx context.Context) ([]metricdata.ScopeMetrics, erro
 	for _, sm := range metrics {
 		var m []metricdata.Metrics
 		for _, metric := range sm.Metrics {
-			if _, ok := filter[metric.Name]; ok {
+			if filter.Has(metric.Name) {
 				m = append(m, metric)
 			}
 		}
@@ -49,15 +51,6 @@ func (p *Producer) Produce(ctx context.Context) ([]metricdata.ScopeMetrics, erro
 	return out, nil
 }
 
-func (p *Producer) loadFilter() map[string]struct{} {
-	filter, _ := p.filter.Load().(map[string]struct{})
-	return filter
-}
-
 func (p *Producer) SetFilter(names []string) {
-	filter := make(map[string]struct{}, len(names))
-	for _, name := range names {
-		filter[name] = struct{}{}
-	}
-	p.filter.Store(filter)
+	p.filter.Store(sets.NewHash(names...))
 }
