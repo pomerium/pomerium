554e77bc7c82a761eeda4b0afc5c502c99385249
Joe Kralicky <joekralicky@gmail.com>
2024-08-09 14:05:10 -0400

    envoy: log mtls failures (#5210)
    envoy: log mtls failures
    
    This implements limited listener-based access logging for downstream
    transport failures, only enabled when downstream_mtls.enforcement is
    set to 'reject_connection'. Client certificate details and the error
    message will be logged.
    
    Additionally, the new key 'client-certificate' can be set in the
    access_log_fields list in the configuration, which will add peer
    certificate properties (issuer, subject, SANs) to the existing
    per-request http logs.
    
    ---------
    
    Co-authored-by: Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>

--- config/envoyconfig/listeners.go
+++ config/envoyconfig/listeners.go
@@ -12,9 +12,11 @@ import (
 	"strings"
 	"time"
 
+	envoy_config_accesslog_v3 "github.com/envoyproxy/go-control-plane/envoy/config/accesslog/v3"
 	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	envoy_config_listener_v3 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	envoy_config_route_v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	envoy_extensions_access_loggers_grpc_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/access_loggers/grpc/v3"
 	envoy_http_connection_manager "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	envoy_extensions_transport_sockets_tls_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
 	envoy_type_v3 "github.com/envoyproxy/go-control-plane/envoy/type/v3"
@@ -123,6 +125,28 @@ func (b *Builder) buildTLSSocket(ctx context.Context, cfg *config.Config, certs 
 	}, nil
 }
 
+func listenerAccessLog() []*envoy_config_accesslog_v3.AccessLog {
+	cc := &envoy_extensions_access_loggers_grpc_v3.CommonGrpcAccessLogConfig{
+		LogName: "ingress-http-listener",
+		GrpcService: &envoy_config_core_v3.GrpcService{
+			TargetSpecifier: &envoy_config_core_v3.GrpcService_EnvoyGrpc_{
+				EnvoyGrpc: &envoy_config_core_v3.GrpcService_EnvoyGrpc{
+					ClusterName: "pomerium-control-plane-grpc",
+				},
+			},
+		},
+		TransportApiVersion: envoy_config_core_v3.ApiVersion_V3,
+	}
+	tcp := marshalAny(
+		&envoy_extensions_access_loggers_grpc_v3.TcpGrpcAccessLogConfig{CommonConfig: cc})
+	return []*envoy_config_accesslog_v3.AccessLog{
+		{
+			Name:       "envoy.access_loggers.tcp_grpc",
+			ConfigType: &envoy_config_accesslog_v3.AccessLog_TypedConfig{TypedConfig: tcp},
+		},
+	}
+}
+
 func (b *Builder) buildMainListener(
 	ctx context.Context,
 	cfg *config.Config,
@@ -131,6 +155,10 @@ func (b *Builder) buildMainListener(
 	li := newEnvoyListener("http-ingress")
 	if cfg.Options.UseProxyProtocol {
 		li.ListenerFilters = append(li.ListenerFilters, ProxyProtocolFilter())
+	}
+
+	if cfg.Options.DownstreamMTLS.Enforcement == config.MTLSEnforcementRejectConnection {
+		li.AccessLog = listenerAccessLog()
 	}
 
 	if cfg.Options.InsecureServer {
