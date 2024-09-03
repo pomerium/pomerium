2b8411105883dea4895a26e7985b0d9361ac5df5
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2024-09-03 13:37:19 -0700

    config: sort runtime flags, name consistency (#5255)
    Sort the runtime flag definitions alphabetically. Rename
    envoy_resource_manager_enabled to just envoy_resource_manager for
    consistency with the other flag names. (This flag hasn't been released
    yet, so it should be OK to rename it.) Also add a doc comment.

--- config/runtime_flags.go
+++ config/runtime_flags.go
@@ -3,20 +3,22 @@ package config
 import "maps"
 
 var (
-	// RuntimeFlagGRPCDatabrokerKeepalive enables gRPC keepalive to the databroker service
-	RuntimeFlagGRPCDatabrokerKeepalive = runtimeFlag("grpc_databroker_keepalive", false)
-
-	// RuntimeFlagMatchAnyIncomingPort enables ignoring the incoming port when matching routes
-	RuntimeFlagMatchAnyIncomingPort = runtimeFlag("match_any_incoming_port", true)
-
-	// RuntimeFlagLegacyIdentityManager enables the legacy identity manager
-	RuntimeFlagLegacyIdentityManager = runtimeFlag("legacy_identity_manager", false)
-
 	// RuntimeFlagConfigHotReload enables the hot-reloading mechanism for the config file
 	// and any other files referenced within it
 	RuntimeFlagConfigHotReload = runtimeFlag("config_hot_reload", true)
 
-	RuntimeFlagEnvoyResourceManagerEnabled = runtimeFlag("envoy_resource_manager_enabled", true)
+	// RuntimeFlagEnvoyResourceManager enables Envoy overload settings based on
+	// process cgroup limits (Linux only).
+	RuntimeFlagEnvoyResourceManager = runtimeFlag("envoy_resource_manager", true)
+
+	// RuntimeFlagGRPCDatabrokerKeepalive enables gRPC keepalive to the databroker service
+	RuntimeFlagGRPCDatabrokerKeepalive = runtimeFlag("grpc_databroker_keepalive", false)
+
+	// RuntimeFlagLegacyIdentityManager enables the legacy identity manager
+	RuntimeFlagLegacyIdentityManager = runtimeFlag("legacy_identity_manager", false)
+
+	// RuntimeFlagMatchAnyIncomingPort enables ignoring the incoming port when matching routes
+	RuntimeFlagMatchAnyIncomingPort = runtimeFlag("match_any_incoming_port", true)
 )
 
 // RuntimeFlag is a runtime flag that can flip on/off certain features
