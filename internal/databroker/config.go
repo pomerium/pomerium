71fbe5bb00243d1ec0c5d6e4812afad0f3560666
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2024-10-04 12:04:59 -0700

    databroker: remove unused serverConfig fields (#5314)
    The databroker.serverConfig struct has a few fields which are written
    to but never read.

--- internal/databroker/config.go
+++ internal/databroker/config.go
@@ -2,37 +2,24 @@ package databroker
 
 import (
 	"time"
-
-	"github.com/pomerium/pomerium/internal/log"
-	"github.com/pomerium/pomerium/pkg/cryptutil"
 )
 
 var (
-	// DefaultDeletePermanentlyAfter is the default amount of time to wait before deleting
-	// a record permanently.
-	DefaultDeletePermanentlyAfter = time.Hour
 	// DefaultStorageType is the default storage type that Server use
 	DefaultStorageType = "memory"
-	// DefaultGetAllPageSize is the default page size for GetAll calls.
-	DefaultGetAllPageSize = 50
 	// DefaultRegistryTTL is the default registry time to live.
 	DefaultRegistryTTL = time.Minute
 )
 
 type serverConfig struct {
-	deletePermanentlyAfter  time.Duration
-	secret                  []byte
 	storageType             string
 	storageConnectionString string
-	getAllPageSize          int
 	registryTTL             time.Duration
 }
 
 func newServerConfig(options ...ServerOption) *serverConfig {
 	cfg := new(serverConfig)
-	WithDeletePermanentlyAfter(DefaultDeletePermanentlyAfter)(cfg)
 	WithStorageType(DefaultStorageType)(cfg)
-	WithGetAllPageSize(DefaultGetAllPageSize)(cfg)
 	WithRegistryTTL(DefaultRegistryTTL)(cfg)
 	for _, option := range options {
 		option(cfg)
@@ -43,38 +30,10 @@ func newServerConfig(options ...ServerOption) *serverConfig {
 // A ServerOption customizes the server.
 type ServerOption func(*serverConfig)
 
-// WithDeletePermanentlyAfter sets the deletePermanentlyAfter duration.
-// If a record is deleted via Delete, it will be permanently deleted after
-// the given duration.
-func WithDeletePermanentlyAfter(dur time.Duration) ServerOption {
-	return func(cfg *serverConfig) {
-		cfg.deletePermanentlyAfter = dur
-	}
-}
-
-// WithGetAllPageSize sets the page size for GetAll calls.
-func WithGetAllPageSize(pageSize int) ServerOption {
-	return func(cfg *serverConfig) {
-		cfg.getAllPageSize = pageSize
-	}
-}
-
 // WithRegistryTTL sets the registry time to live in the config.
 func WithRegistryTTL(ttl time.Duration) ServerOption {
 	return func(cfg *serverConfig) {
 		cfg.registryTTL = ttl
-	}
-}
-
-// WithGetSharedKey sets the secret in the config.
-func WithGetSharedKey(getSharedKey func() ([]byte, error)) ServerOption {
-	return func(cfg *serverConfig) {
-		sharedKey, err := getSharedKey()
-		if err != nil {
-			log.Error().Err(err).Msgf("shared key is required and must be %d bytes long", cryptutil.DefaultKeySize)
-			return
-		}
-		cfg.secret = sharedKey
 	}
 }
 
