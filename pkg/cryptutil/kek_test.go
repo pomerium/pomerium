2c44631c9ac285541dcc52d624a33e9d6248933a
Caleb Doxsey <cdoxsey@pomerium.com>
2024-12-02 12:36:33 -0700

    update not same call

--- pkg/cryptutil/kek_test.go
+++ pkg/cryptutil/kek_test.go
@@ -49,10 +49,8 @@ func TestKeyEncryptionKey(t *testing.T) {
 		private, err := GenerateKeyEncryptionKey()
 		require.NoError(t, err)
 		assert.Equal(t, private.data[:], private.KeyBytes())
-		assert.NotSame(t, private.data[:], private.KeyBytes())
 		public := private.Public()
 		assert.Equal(t, public.data[:], public.KeyBytes())
-		assert.NotSame(t, public.data[:], public.KeyBytes())
 	})
 	t.Run("GetKeyEncryptionKeyID", func(t *testing.T) {
 		id := GetKeyEncryptionKeyID([]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31})
