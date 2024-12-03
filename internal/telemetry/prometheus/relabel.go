699679bc574c9000d40fffb79b4bc46054c868e9
Denis Mishin <dmishin@pomerium.com>
2024-12-03 11:17:34 -0500

    core/metrics: improve memory usage (#5364)

---
+++ internal/telemetry/prometheus/relabel.go
@@ -0,0 +1,36 @@
+package prometheus
+
+import (
+	"iter"
+
+	dto "github.com/prometheus/client_model/go"
+)
+
+func AddLabels(
+	src iter.Seq2[*dto.MetricFamily, error],
+	addLabels map[string]string,
+) iter.Seq2[*dto.MetricFamily, error] {
+	var extra []*dto.LabelPair
+	for k, v := range addLabels {
+		k, v := k, v
+		extra = append(extra, &dto.LabelPair{
+			Name:  &k,
+			Value: &v,
+		})
+	}
+
+	return func(yield func(*dto.MetricFamily, error) bool) {
+		for mf, err := range src {
+			if err != nil {
+				yield(nil, err)
+				return
+			}
+			for _, metric := range mf.Metric {
+				metric.Label = append(metric.Label, extra...)
+			}
+			if !yield(mf, nil) {
+				return
+			}
+		}
+	}
+}
--- internal/telemetry/prometheus/stream.go
+++ internal/telemetry/prometheus/stream.go
@@ -2,61 +2,80 @@ package prometheus
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
 	"io"
-	"strings"
+	"iter"
 
 	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/common/expfmt"
 )
 
-type MetricFamilyStream struct {
+type metricFamilyStream struct {
 	reader  io.Reader
 	scanner *bufio.Scanner
-	buffer  strings.Builder
+	buffer  bytes.Buffer
+	parser  expfmt.TextParser
 }
 
-func NewMetricFamilyStream(reader io.Reader) *MetricFamilyStream {
-	return &MetricFamilyStream{
+func NewMetricFamilyStream(reader io.Reader) iter.Seq2[*dto.MetricFamily, error] {
+	mfs := &metricFamilyStream{
 		reader:  reader,
 		scanner: bufio.NewScanner(reader),
 	}
+	return func(yield func(*dto.MetricFamily, error) bool) {
+		for {
+			m, err := mfs.Next()
+			if errors.Is(err, io.EOF) {
+				return
+			}
+			if err != nil {
+				yield(nil, err)
+				return
+			} else if !yield(m, nil) {
+				return
+			}
+		}
+	}
 }
 
-func (mfs *MetricFamilyStream) Next() (*dto.MetricFamily, error) {
+func (mfs *metricFamilyStream) Next() (*dto.MetricFamily, error) {
 	var afterHeader bool
-	var block *strings.Reader
-	for block == nil && mfs.scanner.Scan() {
-		line := mfs.scanner.Text()
-		if line == "" {
+	for mfs.scanner.Scan() {
+		line := mfs.scanner.Bytes()
+		if len(line) == 0 {
 			continue
 		}
 
 		if line[0] == '#' {
 			if afterHeader {
-				block = strings.NewReader(mfs.buffer.String())
+				result, err := mfs.parseMetricFamilyBlock(&mfs.buffer)
 				mfs.buffer.Reset()
+				mfs.buffer.Write(line)
+				mfs.buffer.WriteRune('\n')
+				return result, err
 			}
 		} else {
 			afterHeader = true
 		}
-		mfs.buffer.WriteString(line)
-		mfs.buffer.WriteString("\n")
+		mfs.buffer.Write(line)
+		mfs.buffer.WriteRune('\n')
 	}
 
-	if block == nil {
-		if err := mfs.scanner.Err(); err != nil {
-			return nil, err
-		}
-		if mfs.buffer.Len() == 0 {
-			return nil, io.EOF
-		}
-		block = strings.NewReader(mfs.buffer.String())
-		mfs.buffer.Reset()
+	if err := mfs.scanner.Err(); err != nil {
+		return nil, err
 	}
+	if mfs.buffer.Len() == 0 {
+		return nil, io.EOF
+	}
+	result, err := mfs.parseMetricFamilyBlock(&mfs.buffer)
+	mfs.buffer.Reset()
+	return result, err
+}
 
-	var parser expfmt.TextParser
-	families, err := parser.TextToMetricFamilies(block)
+func (mfs *metricFamilyStream) parseMetricFamilyBlock(r io.Reader) (*dto.MetricFamily, error) {
+	families, err := mfs.parser.TextToMetricFamilies(r)
 	if err != nil {
 		return nil, err
 	}
