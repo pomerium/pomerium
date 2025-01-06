fb7b61a6771a691e97b556777cdecb389929cf4b
Caleb Doxsey <cdoxsey@pomerium.com>
2025-01-06 09:52:29 -0700

    cleanup headers (#5408)
    * cleanup headers
    
    * return issuer format errors
    
    * go mod

--- authorize/evaluator/headers_evaluator_evaluation.go
+++ authorize/evaluator/headers_evaluator_evaluation.go
@@ -12,6 +12,7 @@ import (
 	"strings"
 	"time"
 
+	envoy_config_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	"github.com/go-jose/go-jose/v3"
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/structpb"
@@ -26,7 +27,7 @@ import (
 // A headersEvaluatorEvaluation is a single evaluation of the headers evaluator.
 type headersEvaluatorEvaluation struct {
 	evaluator *HeadersEvaluator
-	request   *HeadersRequest
+	request   *Request
 	response  *HeadersResponse
 	now       time.Time
 
@@ -50,7 +51,7 @@ type headersEvaluatorEvaluation struct {
 	cachedSignedJWT string
 }
 
-func newHeadersEvaluatorEvaluation(evaluator *HeadersEvaluator, request *HeadersRequest, now time.Time) *headersEvaluatorEvaluation {
+func newHeadersEvaluatorEvaluation(evaluator *HeadersEvaluator, request *Request, now time.Time) *headersEvaluatorEvaluation {
 	return &headersEvaluatorEvaluation{
 		evaluator: evaluator,
 		request:   request,
@@ -60,16 +61,19 @@ func newHeadersEvaluatorEvaluation(evaluator *HeadersEvaluator, request *Headers
 }
 
 func (e *headersEvaluatorEvaluation) execute(ctx context.Context) (*HeadersResponse, error) {
-	e.fillHeaders(ctx)
-	return e.response, nil
+	err := e.fillHeaders(ctx)
+	return e.response, err
 }
 
 func (e *headersEvaluatorEvaluation) fillJWTAssertionHeader(ctx context.Context) {
 	e.response.Headers.Add("x-pomerium-jwt-assertion", e.getSignedJWT(ctx))
 }
 
-func (e *headersEvaluatorEvaluation) fillJWTClaimHeaders(ctx context.Context) {
-	claims := e.getJWTPayload(ctx)
+func (e *headersEvaluatorEvaluation) fillJWTClaimHeaders(ctx context.Context) error {
+	claims, err := e.getJWTPayload(ctx)
+	if err != nil {
+		return err
+	}
 	for headerName, claimKey := range e.evaluator.store.GetJWTClaimHeaders() {
 		claim, ok := claims[claimKey]
 		if !ok {
@@ -78,14 +82,20 @@ func (e *headersEvaluatorEvaluation) fillJWTClaimHeaders(ctx context.Context) {
 		}
 		e.response.Headers.Add(headerName, getHeaderStringValue(claim))
 	}
+	return nil
 }
 
 func (e *headersEvaluatorEvaluation) fillKubernetesHeaders(ctx context.Context) {
-	if e.request.KubernetesServiceAccountToken == "" {
+	if e.request.Policy == nil {
 		return
 	}
 
-	e.response.Headers.Add("Authorization", "Bearer "+e.request.KubernetesServiceAccountToken)
+	token, err := e.request.Policy.GetKubernetesServiceAccountToken()
+	if err != nil || token == "" {
+		return
+	}
+
+	e.response.Headers.Add("Authorization", "Bearer "+token)
 	impersonateUser := e.getJWTPayloadEmail(ctx)
 	if impersonateUser != "" {
 		e.response.Headers.Add("Impersonate-User", impersonateUser)
@@ -97,26 +107,42 @@ func (e *headersEvaluatorEvaluation) fillKubernetesHeaders(ctx context.Context) 
 }
 
 func (e *headersEvaluatorEvaluation) fillGoogleCloudServerlessHeaders(ctx context.Context) {
-	if e.request.EnableGoogleCloudServerlessAuthentication {
-		h, err := getGoogleCloudServerlessHeaders(e.evaluator.store.GetGoogleCloudServerlessAuthenticationServiceAccount(), e.request.ToAudience)
-		if err != nil {
-			log.Ctx(ctx).Error().Err(err).Msg("authorize/header-evaluator: error retrieving google cloud serverless headers")
-			return
-		}
-		for k, v := range h {
-			e.response.Headers.Add(k, v)
-		}
+	if e.request.Policy == nil || !e.request.Policy.EnableGoogleCloudServerlessAuthentication {
+		return
+	}
+
+	var toAudience string
+	for _, wu := range e.request.Policy.To {
+		toAudience = "https://" + wu.URL.Hostname()
+	}
+
+	h, err := getGoogleCloudServerlessHeaders(e.evaluator.store.GetGoogleCloudServerlessAuthenticationServiceAccount(), toAudience)
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("authorize/header-evaluator: error retrieving google cloud serverless headers")
+		return
+	}
+	for k, v := range h {
+		e.response.Headers.Add(k, v)
 	}
 }
 
 func (e *headersEvaluatorEvaluation) fillRoutingKeyHeaders() {
-	if e.request.EnableRoutingKey {
+	if e.request.Policy == nil {
+		return
+	}
+
+	if e.request.Policy.EnvoyOpts.GetLbPolicy() == envoy_config_cluster_v3.Cluster_RING_HASH ||
+		e.request.Policy.EnvoyOpts.GetLbPolicy() == envoy_config_cluster_v3.Cluster_MAGLEV {
 		e.response.Headers.Add("x-pomerium-routing-key", cryptoSHA256(e.request.Session.ID))
 	}
 }
 
 func (e *headersEvaluatorEvaluation) fillSetRequestHeaders(ctx context.Context) {
-	for k, v := range e.request.SetRequestHeaders {
+	if e.request.Policy == nil {
+		return
+	}
+
+	for k, v := range e.request.Policy.SetRequestHeaders {
 		e.response.Headers.Add(k, os.Expand(v, func(name string) string {
 			switch name {
 			case "$":
@@ -138,13 +164,16 @@ func (e *headersEvaluatorEvaluation) fillSetRequestHeaders(ctx context.Context) 
 	}
 }
 
-func (e *headersEvaluatorEvaluation) fillHeaders(ctx context.Context) {
+func (e *headersEvaluatorEvaluation) fillHeaders(ctx context.Context) error {
 	e.fillJWTAssertionHeader(ctx)
-	e.fillJWTClaimHeaders(ctx)
+	if err := e.fillJWTClaimHeaders(ctx); err != nil {
+		return err
+	}
 	e.fillKubernetesHeaders(ctx)
 	e.fillGoogleCloudServerlessHeaders(ctx)
 	e.fillRoutingKeyHeaders()
 	e.fillSetRequestHeaders(ctx)
+	return nil
 }
 
 func (e *headersEvaluatorEvaluation) getSessionOrServiceAccount(ctx context.Context) (*session.Session, *user.ServiceAccount) {
@@ -182,7 +211,7 @@ func (e *headersEvaluatorEvaluation) getUser(ctx context.Context) *user.User {
 }
 
 func (e *headersEvaluatorEvaluation) getClientCertFingerprint() string {
-	cert, err := cryptutil.ParsePEMCertificate([]byte(e.request.ClientCertificate.Leaf))
+	cert, err := cryptutil.ParsePEMCertificate([]byte(e.request.HTTP.ClientCertificate.Leaf))
 	if err != nil {
 		return ""
 	}
@@ -212,12 +241,23 @@ func (e *headersEvaluatorEvaluation) getGroupIDs(ctx context.Context) []string {
 	return make([]string, 0)
 }
 
-func (e *headersEvaluatorEvaluation) getJWTPayloadIss() string {
-	return e.request.Issuer
+func (e *headersEvaluatorEvaluation) getJWTPayloadIss() (string, error) {
+	var issuerFormat string
+	if e.request.Policy != nil {
+		issuerFormat = e.request.Policy.JWTIssuerFormat
+	}
+	switch issuerFormat {
+	case "uri":
+		return fmt.Sprintf("https://%s/", e.request.HTTP.Hostname), nil
+	case "", "hostOnly":
+		return e.request.HTTP.Hostname, nil
+	default:
+		return "", fmt.Errorf("unsupported JWT issuer format: %s", issuerFormat)
+	}
 }
 
 func (e *headersEvaluatorEvaluation) getJWTPayloadAud() string {
-	return e.request.Audience
+	return e.request.HTTP.Hostname
 }
 
 func (e *headersEvaluatorEvaluation) getJWTPayloadJTI() string {
@@ -307,14 +347,19 @@ func (e *headersEvaluatorEvaluation) getJWTPayloadName(ctx context.Context) stri
 	return ""
 }
 
-func (e *headersEvaluatorEvaluation) getJWTPayload(ctx context.Context) map[string]any {
+func (e *headersEvaluatorEvaluation) getJWTPayload(ctx context.Context) (map[string]any, error) {
 	if e.gotJWTPayload {
-		return e.cachedJWTPayload
+		return e.cachedJWTPayload, nil
+	}
+
+	iss, err := e.getJWTPayloadIss()
+	if err != nil {
+		return nil, err
 	}
 
 	e.gotJWTPayload = true
 	e.cachedJWTPayload = map[string]any{
-		"iss":    e.getJWTPayloadIss(),
+		"iss":    iss,
 		"aud":    e.getJWTPayloadAud(),
 		"jti":    e.getJWTPayloadJTI(),
 		"iat":    e.getJWTPayloadIAT(),
@@ -342,7 +387,7 @@ func (e *headersEvaluatorEvaluation) getJWTPayload(ctx context.Context) map[stri
 			e.cachedJWTPayload[claimKey] = strings.Join(vs, ",")
 		}
 	}
-	return e.cachedJWTPayload
+	return e.cachedJWTPayload, nil
 }
 
 func (e *headersEvaluatorEvaluation) getSignedJWT(ctx context.Context) string {
@@ -371,7 +416,11 @@ func (e *headersEvaluatorEvaluation) getSignedJWT(ctx context.Context) string {
 		return ""
 	}
 
-	jwtPayload := e.getJWTPayload(ctx)
+	jwtPayload, err := e.getJWTPayload(ctx)
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("authorize/header-evaluator: error creating JWT payload")
+		return ""
+	}
 	bs, err := json.Marshal(jwtPayload)
 	if err != nil {
 		log.Ctx(ctx).Error().Err(err).Msg("authorize/header-evaluator: error marshaling JWT payload")
