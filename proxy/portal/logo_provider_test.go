2564c80d1a361704783fb386dbe99038b8420f32
Caleb Doxsey <cdoxsey@pomerium.com>
2025-02-27 16:50:42 -0700

    proxy: add short timeout for logo discovery

--- proxy/portal/logo_provider_test.go
+++ proxy/portal/logo_provider_test.go
@@ -1,6 +1,7 @@
-package portal_test
+package portal
 
 import (
+	"context"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -10,7 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pomerium/pomerium/internal/testutil"
-	"github.com/pomerium/pomerium/proxy/portal"
 )
 
 func TestLogoProvider(t *testing.T) {
@@ -30,8 +30,27 @@ func TestLogoProvider(t *testing.T) {
 	t.Cleanup(srv.Close)
 
 	ctx := testutil.GetContext(t, time.Minute)
-	p := portal.NewLogoProvider()
+	p := NewLogoProvider()
 	u, err := p.GetLogoURL(ctx, "", srv.URL)
 	assert.NoError(t, err)
 	assert.Equal(t, "data:image/vnd.microsoft.icon;base64,Tk9UIEFDVFVBTExZIEFOIElDT04=", u)
+}
+
+func TestLogoProvider_Timeout(t *testing.T) {
+	t.Parallel()
+
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		select {
+		case <-r.Context().Done():
+		case <-time.After(time.Second):
+		}
+		http.NotFound(w, r)
+	}))
+	t.Cleanup(srv.Close)
+
+	ctx := testutil.GetContext(t, time.Minute)
+	p := newFaviconDiscoveryLogoProvider()
+	p.discoveryTimeout = time.Millisecond
+	_, err := p.GetLogoURL(ctx, "", srv.URL)
+	assert.ErrorIs(t, err, context.DeadlineExceeded)
 }
