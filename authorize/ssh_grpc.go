9d23532bd15e96923cd312bc1c57fee0a167dd4c
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2025-04-01 12:32:34 -0700

    send empty methods list for final deny response

--- authorize/ssh_grpc.go
+++ authorize/ssh_grpc.go
@@ -617,19 +617,11 @@ func handleEvaluatorResponseForSSH(
 	// XXX: do we want to send an equivalent to the "show error details" output
 	//      in the case of a deny result?
 
-	// XXX: this is not quite right -- needs to exactly match the last list of methods
-	methods := []string{"publickey"}
-	if slices.Contains(state.MethodsAuthenticated, "keyboard-interactive") {
-		methods = append(methods, "keyboard-interactive")
-	}
-
 	return &extensions_ssh.ServerMessage{
 		Message: &extensions_ssh.ServerMessage_AuthResponse{
 			AuthResponse: &extensions_ssh.AuthenticationResponse{
 				Response: &extensions_ssh.AuthenticationResponse_Deny{
-					Deny: &extensions_ssh.DenyResponse{
-						Methods: methods,
-					},
+					Deny: &extensions_ssh.DenyResponse{},
 				},
 			},
 		},
