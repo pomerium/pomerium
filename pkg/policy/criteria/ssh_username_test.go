3a935235bf91b50c0d518be6b1ca6769d4715fdf
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2025-06-18 10:31:31 -0700

    policy: add ssh PPL criteria
    Add four new PPL criteria for use with SSH, matching username and
    public key.

---
+++ pkg/policy/criteria/ssh_username_test.go
@@ -0,0 +1,149 @@
+package criteria
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/structpb"
+
+	"github.com/pomerium/pomerium/pkg/grpc/databroker"
+	"github.com/pomerium/pomerium/pkg/grpc/session"
+	"github.com/pomerium/pomerium/pkg/grpc/user"
+)
+
+func TestSSHUsername(t *testing.T) {
+	t.Run("ok", func(t *testing.T) {
+		res, err := evaluate(t, `
+allow:
+  and:
+    - ssh_username:
+        is: example-username
+`, []*databroker.Record{}, Input{SSH: InputSSH{Username: "example-username"}})
+		require.NoError(t, err)
+		assert.Equal(t, A{true, A{ReasonSSHUsernameOK}, M{}}, res["allow"])
+		assert.Equal(t, A{false, A{}}, res["deny"])
+	})
+	t.Run("shorthand", func(t *testing.T) {
+		res, err := evaluate(t, `
+allow:
+  and:
+    - ssh_username: example-username
+`, []*databroker.Record{}, Input{SSH: InputSSH{Username: "example-username"}})
+		require.NoError(t, err)
+		assert.Equal(t, A{true, A{ReasonSSHUsernameOK}, M{}}, res["allow"])
+		assert.Equal(t, A{false, A{}}, res["deny"])
+	})
+	t.Run("multiple ok", func(t *testing.T) {
+		res, err := evaluate(t, `
+allow:
+  and:
+    - ssh_username:
+        in: [user1, user2, user3]
+`, []*databroker.Record{}, Input{SSH: InputSSH{Username: "user2"}})
+		require.NoError(t, err)
+		assert.Equal(t, A{true, A{ReasonSSHUsernameOK}, M{}}, res["allow"])
+		assert.Equal(t, A{false, A{}}, res["deny"])
+	})
+	t.Run("multiple unauthorized", func(t *testing.T) {
+		res, err := evaluate(t, `
+allow:
+  and:
+    - ssh_username:
+        in: [user1, user2, user3]
+`, []*databroker.Record{}, Input{SSH: InputSSH{Username: "user4"}})
+		require.NoError(t, err)
+		assert.Equal(t, A{false, A{ReasonSSHUsernameUnauthorized}, M{}}, res["allow"])
+		assert.Equal(t, A{false, A{}}, res["deny"])
+	})
+}
+
+func TestSSHUsernameFromClaim(t *testing.T) {
+	t.Run("session claim ok", func(t *testing.T) {
+		res, err := evaluate(t, `
+allow:
+  and:
+    - ssh_username_from_claim: username
+`,
+			[]*databroker.Record{
+				makeRecord(&session.Session{
+					Id:     "SESSION_ID",
+					UserId: "USER_ID",
+					Claims: map[string]*structpb.ListValue{
+						"username": {Values: []*structpb.Value{
+							structpb.NewStringValue("root"),
+							structpb.NewStringValue("admin"),
+						}},
+					},
+				}),
+				makeRecord(&user.User{
+					Id:    "USER_ID",
+					Email: "test@example.com",
+				}),
+			},
+			Input{
+				SSH:     InputSSH{Username: "admin"},
+				Session: InputSession{ID: "SESSION_ID"},
+			},
+		)
+		require.NoError(t, err)
+		assert.Equal(t, A{true, A{ReasonSSHUsernameOK}, M{}}, res["allow"])
+		assert.Equal(t, A{false, A{}}, res["deny"])
+	})
+	t.Run("session claim unauthorized", func(t *testing.T) {
+		res, err := evaluate(t, `
+allow:
+  and:
+    - ssh_username_from_claim: username
+`,
+			[]*databroker.Record{
+				makeRecord(&session.Session{
+					Id:     "SESSION_ID",
+					UserId: "USER_ID",
+					Claims: map[string]*structpb.ListValue{
+						"username": {Values: []*structpb.Value{
+							structpb.NewStringValue("root"),
+							structpb.NewStringValue("admin"),
+						}},
+					},
+				}),
+				makeRecord(&user.User{
+					Id:    "USER_ID",
+					Email: "test@example.com",
+				}),
+			},
+			Input{
+				SSH:     InputSSH{Username: "other-username"},
+				Session: InputSession{ID: "SESSION_ID"},
+			},
+		)
+		require.NoError(t, err)
+		assert.Equal(t, A{false, A{ReasonSSHUsernameUnauthorized}, M{}}, res["allow"])
+		assert.Equal(t, A{false, A{}}, res["deny"])
+	})
+	t.Run("claim missing", func(t *testing.T) {
+		res, err := evaluate(t, `
+allow:
+  and:
+    - ssh_username_from_claim: username
+`,
+			[]*databroker.Record{
+				makeRecord(&session.Session{
+					Id:     "SESSION_ID",
+					UserId: "USER_ID",
+				}),
+				makeRecord(&user.User{
+					Id:    "USER_ID",
+					Email: "test@example.com",
+				}),
+			},
+			Input{
+				SSH:     InputSSH{Username: "admin"},
+				Session: InputSession{ID: "SESSION_ID"},
+			},
+		)
+		require.NoError(t, err)
+		assert.Equal(t, A{false, A{ReasonSSHUsernameUnauthorized}, M{}}, res["allow"])
+		assert.Equal(t, A{false, A{}}, res["deny"])
+	})
+}
