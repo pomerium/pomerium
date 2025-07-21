23d4edba571ff0ed7c1d110bd10125cdd32fd952
Ryota <rytswd@gmail.com>
2025-07-21 01:24:12 +0100

    Add missing test coverage
    It is also arguable whether we should be using atomicutil package at
    all given Pomerium codebase has moved up to go 1.24. This generic
    handling may have served well in the past, but could be retired and we
    can instead use std lib with atomic.Pointer[T] instead.

--- internal/atomicutil/value_test.go
+++ internal/atomicutil/value_test.go
@@ -1,6 +1,7 @@
 package atomicutil
 
 import (
+	"sync/atomic"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,4 +19,40 @@ func TestValue(t *testing.T) {
 		var v Value[int]
 		assert.Equal(t, 0, v.Load())
 	})
+}
+
+func TestStore(t *testing.T) {
+	v := NewValue(5)
+	v.Store(42)
+	assert.Equal(t, 42, v.Load())
+}
+
+func TestSwap(t *testing.T) {
+	v := NewValue(42)
+	v.Swap(33)
+	assert.Equal(t, 33, v.Load())
+}
+
+func TestCompareAndSwap(t *testing.T) {
+	v := NewValue(42)
+
+	swapped := v.CompareAndSwap(42, 33)
+	assert.Equal(t, true, swapped)
+	assert.Equal(t, 33, v.Load())
+
+	swapped = v.CompareAndSwap(42, 33)
+	assert.Equal(t, false, swapped)
+	assert.Equal(t, 33, v.Load())
+}
+
+// This is just to illustrate how we should use atomic.Pointer[T] instead.
+func TestWithPointer(t *testing.T) {
+	var withUtil *Value[int]
+	var withPointer atomic.Pointer[int]
+
+	// This becomes the zero value.
+	assert.NotNil(t, withUtil.Load())
+
+	// This is nil.
+	assert.Nil(t, withPointer.Load())
 }
