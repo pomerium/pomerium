8b83987b9bb4469c79ef14495518d635588f4e4b
Caleb Doxsey <cdoxsey@pomerium.com>
2025-08-04 08:12:24 -0600

    databroker: move gc to public interface, abort sync calls with invalid record versions (#5753)
    The databroker maintains a list of record changes over time. Those
    changes are retrieved via the databroker `Sync` call. Both the in-memory
    and postgres storage backends periodically remove old changes.
    
    Since this logic is necessary for any storage backend, add a `Clean`
    method to the public interface and start the background cleaner in the
    databroker server rather than the storage backend itself. This makes it
    easier to test.
    
    Currently if a client attempts to `Sync` changes, but they've fallen too
    far behind, we would not detect this and the client would never receive
    the missing changes. The correct behavior is to abort the `Sync`, which
    signals the client to do a full re-sync via `SyncLatest`. This PR
    attempts to implement that behavior.
    
    There is some risk with this change. If the assumptions I'm making about
    how clients work turn out not to be true, we could end up with clients
    continuously attempting to call `Sync` and never recovering.
    
    -
    [ENG-2640](https://linear.app/pomerium/issue/ENG-2640/core-databroker-sync-may-miss-changes)
    
    - [x] reference any related issues
    - [x] updated unit tests
    - [x] add appropriate label (`enhancement`, `bug`, `breaking`,
    `dependencies`, `ci`)
    - [x] ready for review

--- pkg/storage/postgres/option.go
+++ pkg/storage/postgres/option.go
@@ -7,25 +7,16 @@ import (
 )
 
 const (
-	defaultExpiry      = time.Hour
 	defaultRegistryTTL = time.Second * 30
 )
 
 type config struct {
-	expiry         time.Duration
 	registryTTL    time.Duration
 	tracerProvider oteltrace.TracerProvider
 }
 
 // Option customizes a Backend.
 type Option func(*config)
-
-// WithExpiry sets the expiry for changes.
-func WithExpiry(expiry time.Duration) Option {
-	return func(cfg *config) {
-		cfg.expiry = expiry
-	}
-}
 
 // WithRegistryTTL sets the default registry TTL.
 func WithRegistryTTL(ttl time.Duration) Option {
@@ -42,7 +33,6 @@ func WithTracerProvider(tracerProvider oteltrace.TracerProvider) Option {
 
 func getConfig(options ...Option) *config {
 	cfg := new(config)
-	WithExpiry(defaultExpiry)(cfg)
 	WithRegistryTTL(defaultRegistryTTL)(cfg)
 	for _, o := range options {
 		o(cfg)
