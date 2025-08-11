5453d88c51b963b56e54cbf1d1fd868d4a17bff9
Denis Mishin <dmishin@pomerium.com>
2025-08-11 15:27:10 +0200

    add instrumentation

--- pkg/grpc/databroker/fast_forward.go
+++ pkg/grpc/databroker/fast_forward.go
@@ -2,18 +2,29 @@ package databroker
 
 import (
 	"context"
+	"errors"
 	"sync"
 
+	"github.com/rs/zerolog"
+	"go.opentelemetry.io/otel/attribute"
+	otelmetric "go.opentelemetry.io/otel/metric"
+
 	"github.com/pomerium/pomerium/internal/log"
+	"github.com/pomerium/pomerium/internal/telemetry"
+	internalmetrics "github.com/pomerium/pomerium/internal/telemetry/metrics"
+	metrics "github.com/pomerium/pomerium/pkg/metrics"
 	"github.com/pomerium/pomerium/pkg/slices"
 )
 
 // fastForwardHandler will skip
 type fastForwardHandler struct {
+	id      string
 	handler SyncerHandler
 	pending chan ffCmd
 
 	mu sync.Mutex
+
+	c *telemetry.Component
 }
 
 type ffCmd struct {
@@ -22,28 +33,41 @@ type ffCmd struct {
 	records       []*Record
 }
 
-func newFastForwardHandler(ctx context.Context, handler SyncerHandler) SyncerHandler {
+func newFastForwardHandler(ctx context.Context, id string, handler SyncerHandler) SyncerHandler {
 	ff := &fastForwardHandler{
+		id:      id,
 		handler: handler,
 		pending: make(chan ffCmd, 1),
+		c:       telemetry.NewComponent(zerolog.DebugLevel, "databroker.fastforward", attribute.String(metrics.SyncerIDLabel, id)),
 	}
 	go ff.run(ctx)
 	return ff
 }
 
 func (ff *fastForwardHandler) run(ctx context.Context) {
+	ctx, active := ff.c.Active(ctx, "active")
+	defer active.Done()
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case cmd := <-ff.pending:
-			if cmd.clearRecords {
-				ff.handler.ClearRecords(ctx)
-			} else {
-				ff.handler.UpdateRecords(ctx, cmd.serverVersion, cmd.records)
-			}
+			ff.processCmd(ctx, cmd)
 		}
 	}
+}
+
+func (ff *fastForwardHandler) processCmd(ctx context.Context, cmd ffCmd) {
+	if cmd.clearRecords {
+		ctx, op := ff.c.Start(ctx, "ClearRecords")
+		ff.handler.ClearRecords(ctx)
+		op.Complete()
+		return
+	}
+
+	ctx, op := ff.c.Start(ctx, "UpdateRecords", attribute.Int("records", len(cmd.records)))
+	ff.handler.UpdateRecords(ctx, cmd.serverVersion, cmd.records)
+	op.Complete()
 }
 
 func (ff *fastForwardHandler) GetDataBrokerServiceClient() DataBrokerServiceClient {
@@ -64,20 +88,28 @@ func (ff *fastForwardHandler) ClearRecords(ctx context.Context) {
 	cmd.clearRecords = true
 	cmd.records = nil
 
+	_, op := ff.c.Start(ctx, "Enqueue/ClearRecords")
 	select {
 	case <-ctx.Done():
+		_ = op.Failure(errors.New("cancelled"))
 	case ff.pending <- cmd:
+		op.Complete()
 	}
 }
 
 func (ff *fastForwardHandler) UpdateRecords(ctx context.Context, serverVersion uint64, records []*Record) {
+	ctx, op := ff.c.Start(ctx, "Enqueue/UpdateRecords",
+		attribute.Int("records-in", len(records)),
+	)
+	defer op.Complete()
+
 	ff.mu.Lock()
 	defer ff.mu.Unlock()
 
 	var cmd ffCmd
 	select {
 	case <-ctx.Done():
-		return
+		_ = op.Failure(errors.New("cancelled"))
 	case cmd = <-ff.pending:
 	default:
 	}
@@ -91,7 +123,14 @@ func (ff *fastForwardHandler) UpdateRecords(ctx context.Context, serverVersion u
 	})
 	dropped := cnt - len(records)
 	if dropped > 0 {
-		log.Ctx(ctx).Info().Msgf("databroker: fast-forwarded %d records", dropped)
+		log.Ctx(ctx).Info().Str("syncer-id", ff.id).Msgf("databroker: fast-forwarded %d records", dropped)
+		internalmetrics.Int64Counter(metrics.DatabrokerFastForwardDropped).Add(
+			ctx,
+			int64(dropped),
+			otelmetric.WithAttributes(
+				attribute.String(metrics.SyncerIDLabel, ff.id),
+			),
+		)
 	}
 	// reverse back so they appear in the order they were delivered
 	slices.Reverse(records)
@@ -102,6 +141,11 @@ func (ff *fastForwardHandler) UpdateRecords(ctx context.Context, serverVersion u
 
 	select {
 	case <-ctx.Done():
+		_ = op.Failure(errors.New("cancelled"))
 	case ff.pending <- cmd:
+		op.Complete(
+			attribute.Int("records-enqueued", len(records)),
+			attribute.Int("records-dropped", dropped),
+		)
 	}
 }
