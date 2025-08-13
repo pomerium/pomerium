f8ca2a1f24e207e4297294e3891735dfcce6b8ef
Caleb Doxsey <cdoxsey@pomerium.com>
2025-08-13 10:15:37 -0600

    authenticate: cache authenticators (#5780)
    ## Summary
    Currently we create a new authenticator on every request. When creating
    sessions from incoming IdP tokens this results in HTTP requests to the
    OIDC discovery document and JWKS keys every time. These are unnecessary
    HTTP requests and should be cached.
    
    Update the `defaultGetIdentityProvider` so that it will cache
    authenticators and re-use them. We actually have very similar
    functionality in the identity manager, but I wanted to minimize the
    number of changes since this will need to be backported. Future
    improvements would be to unify this into a single authenticator cache
    and also handle frequent changes to IdP settings. (as of now they will
    just stay cached until restart)
    
    A separate access token validator is now being used. This lets us
    properly trace the requests and adds a default 10 second timeout when
    fetching keys. Because of the way the OIDC `RemoteKeySet` works, it
    doesn't use the passed-in context, but rather uses a background context,
    which will never be cancelled. As a result, if an HTTP request for keys
    were to get stuck, we'd never be able to resolve. With a timeout we
    should be able to recover. A future improvement would be to implement a
    new custom `KeySet` that uses the passed-in context rather than a
    background context for fetching keys. This would allow the spans to
    occur in the proper trace.
    
    ## Related issues
    -
    [ENG-2701](https://linear.app/pomerium/issue/ENG-2701/authenticate-idp-access-token-verification-always-retrieves-the-openid)
    
    
    ## Checklist
    
    - [x] reference any related issues
    - [x] updated unit tests
    - [x] add appropriate label (`enhancement`, `bug`, `breaking`,
    `dependencies`, `ci`)
    - [x] ready for review

--- authenticate/identity.go
+++ authenticate/identity.go
@@ -1,25 +1,100 @@
 package authenticate
 
 import (
+	"cmp"
 	"context"
+	"net/url"
+	"sync"
 
+	"github.com/google/btree"
 	oteltrace "go.opentelemetry.io/otel/trace"
 
 	"github.com/pomerium/pomerium/config"
+	identitypb "github.com/pomerium/pomerium/pkg/grpc/identity"
 	"github.com/pomerium/pomerium/pkg/identity"
 )
 
+type cachedAuthenticator struct {
+	redirectURL               *url.URL
+	idp                       *identitypb.Provider
+	overwriteIDTokenOnRefresh bool
+	authenticator             identity.Authenticator
+}
+
+var cachedAuthenticators = struct {
+	sync.RWMutex
+	*btree.BTreeG[cachedAuthenticator]
+}{
+	BTreeG: btree.NewG(2, func(a, b cachedAuthenticator) bool {
+		return cmp.Or(
+			compareURLs(a.redirectURL, b.redirectURL),
+			cmp.Compare(a.idp.Hash(), b.idp.Hash()),
+			compareBools(a.overwriteIDTokenOnRefresh, b.overwriteIDTokenOnRefresh),
+		) < 0
+	}),
+}
+
 func defaultGetIdentityProvider(ctx context.Context, tracerProvider oteltrace.TracerProvider, options *config.Options, idpID string) (identity.Authenticator, error) {
-	redirectURL, err := options.GetAuthenticateRedirectURL()
+	var err error
+	key := cachedAuthenticator{}
+
+	key.redirectURL, err = options.GetAuthenticateRedirectURL()
 	if err != nil {
 		return nil, err
 	}
 
-	idp, err := options.GetIdentityProviderForID(idpID)
+	key.idp, err = options.GetIdentityProviderForID(idpID)
 	if err != nil {
 		return nil, err
 	}
 
-	return identity.GetIdentityProvider(ctx, tracerProvider, idp, redirectURL,
-		options.RuntimeFlags[config.RuntimeFlagRefreshSessionAtIDTokenExpiration])
+	key.overwriteIDTokenOnRefresh = options.RuntimeFlags[config.RuntimeFlagRefreshSessionAtIDTokenExpiration]
+
+	cachedAuthenticators.RLock()
+	value, ok := cachedAuthenticators.Get(key)
+	cachedAuthenticators.RUnlock()
+	if ok {
+		return value.authenticator, nil
+	}
+
+	cachedAuthenticators.Lock()
+	defer cachedAuthenticators.Unlock()
+
+	value, ok = cachedAuthenticators.Get(key)
+	if ok {
+		return value.authenticator, nil
+	}
+
+	key.authenticator, err = identity.GetIdentityProvider(ctx, tracerProvider, key.idp, key.redirectURL, key.overwriteIDTokenOnRefresh)
+	if err != nil {
+		return nil, err
+	}
+
+	cachedAuthenticators.ReplaceOrInsert(key)
+
+	return key.authenticator, nil
+}
+
+func compareBools(a, b bool) int {
+	switch {
+	case a && !b:
+		return -1
+	case b && !a:
+		return 1
+	default:
+		return 0
+	}
+}
+
+func compareURLs(a, b *url.URL) int {
+	switch {
+	case a == nil && b == nil:
+		return 0
+	case a == nil:
+		return 1
+	case b == nil:
+		return -1
+	}
+
+	return cmp.Compare(a.String(), b.String())
 }
