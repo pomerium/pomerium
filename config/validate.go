7caf5a7d07580ba0e53de1d31db3af839c404052
Caleb Doxsey <cdoxsey@pomerium.com>
2025-08-19 13:19:00 -0600

    config: add dns options (#5795)
    Backport #5789 to v0.30.

--- config/validate.go
+++ config/validate.go
@@ -5,42 +5,7 @@ import (
 	"net"
 	"strconv"
 	"strings"
-
-	envoy_extensions_clusters_common_dns_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/clusters/common/dns/v3"
 )
-
-// DNSLookupFamily values.
-const (
-	DNSLookupFamilyAuto        = "AUTO"
-	DNSLookupFamilyV4Only      = "V4_ONLY"
-	DNSLookupFamilyV6Only      = "V6_ONLY"
-	DNSLookupFamilyV4Preferred = "V4_PREFERRED"
-	DNSLookupFamilyAll         = "ALL"
-)
-
-// AllDNSLookupFamilies are all the available DNSLookupFamily values.
-var AllDNSLookupFamilies = []string{
-	DNSLookupFamilyAuto,
-	DNSLookupFamilyV4Only,
-	DNSLookupFamilyV6Only,
-	DNSLookupFamilyV4Preferred,
-	DNSLookupFamilyAll,
-}
-
-// ValidateDNSLookupFamily validates the value to confirm its one of the available DNS lookup families.
-func ValidateDNSLookupFamily(value string) error {
-	switch value {
-	case "",
-		DNSLookupFamilyAuto,
-		DNSLookupFamilyV4Only,
-		DNSLookupFamilyV6Only,
-		DNSLookupFamilyV4Preferred,
-		DNSLookupFamilyAll:
-		return nil
-	}
-
-	return fmt.Errorf("unknown dns_lookup_family: %s, known families are: %s", value, strings.Join(AllDNSLookupFamilies, ", "))
-}
 
 // ValidateCookieSameSite validates the cookie same site option.
 func ValidateCookieSameSite(value string) error {
@@ -50,25 +15,6 @@ func ValidateCookieSameSite(value string) error {
 		return nil
 	}
 	return fmt.Errorf("unknown cookie_same_site: %s", value)
-}
-
-// GetEnvoyDNSLookupFamily gets the envoy DNS lookup family.
-func GetEnvoyDNSLookupFamily(value string) envoy_extensions_clusters_common_dns_v3.DnsLookupFamily {
-	switch value {
-	case DNSLookupFamilyAuto:
-		return envoy_extensions_clusters_common_dns_v3.DnsLookupFamily_AUTO
-	case DNSLookupFamilyV4Only:
-		return envoy_extensions_clusters_common_dns_v3.DnsLookupFamily_V4_ONLY
-	case DNSLookupFamilyV6Only:
-		return envoy_extensions_clusters_common_dns_v3.DnsLookupFamily_V6_ONLY
-	case DNSLookupFamilyV4Preferred:
-		return envoy_extensions_clusters_common_dns_v3.DnsLookupFamily_V4_PREFERRED
-	case DNSLookupFamilyAll:
-		return envoy_extensions_clusters_common_dns_v3.DnsLookupFamily_ALL
-	}
-
-	// default
-	return envoy_extensions_clusters_common_dns_v3.DnsLookupFamily_V4_PREFERRED
 }
 
 // ValidateMetricsAddress validates address for the metrics
