e4d53e37e30a846c5294d6a026dd7a6db3c89b4d
Caleb Doxsey <caleb@doxsey.net>
2025-08-26 15:43:08 -0600

    wip

---
+++ internal/databroker/pool.go
@@ -0,0 +1,245 @@
+package databroker
+
+import (
+	"context"
+	"sync"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/pomerium/pomerium/config"
+	databrokerpb "github.com/pomerium/pomerium/pkg/grpc/databroker"
+	registrypb "github.com/pomerium/pomerium/pkg/grpc/registry"
+)
+
+type Client interface {
+	databrokerpb.DataBrokerServiceClient
+	registrypb.RegistryClient
+}
+
+type ClientConnManager interface {
+	GetClient(target string) Client
+	OnConfigChange(ctx context.Context, cfg *config.Config)
+	Stop()
+}
+
+type clientConnManager struct {
+	mu    sync.RWMutex
+	conns map[string]*grpc.ClientConn
+}
+
+// NewClientConnManager craetes a new ClientConnManager
+func NewClientConnManager() ClientConnManager {
+	return &clientConnManager{}
+}
+
+// GetClient returns a client for the given target.
+func (pool *clientConnManager) GetClient(target string) Client {
+	return clientPoolClient{pool, target}
+}
+
+// OnConfigChange reacts to any changes in configuration.
+func (pool *clientConnManager) OnConfigChange(ctx context.Context, cfg *config.Config) {
+}
+
+func (pool *clientConnManager) Stop() {
+}
+
+func (pool *clientConnManager) withClientConn(target string, fn func(*grpc.ClientConn) error) error {
+	pool.mu.RLock()
+	cc, ok := pool.conns[target]
+	pool.mu.RUnlock()
+
+	if ok {
+		return fn(cc)
+	}
+
+	pool.mu.Lock()
+	defer pool.mu.Unlock()
+
+	cc, ok = pool.conns[target]
+	if ok {
+		return fn(cc)
+	}
+
+	cc, err := grpc.NewClient(target)
+	if err != nil {
+		return err
+	}
+	pool.conns[target] = cc
+
+	return fn(cc)
+}
+
+type clientPoolClient struct {
+	*clientConnManager
+	target string
+}
+
+func (client clientPoolClient) AcquireLease(
+	ctx context.Context,
+	req *databrokerpb.AcquireLeaseRequest,
+	opts ...grpc.CallOption,
+) (res *databrokerpb.AcquireLeaseResponse, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).AcquireLease(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) Get(
+	ctx context.Context,
+	req *databrokerpb.GetRequest,
+	opts ...grpc.CallOption,
+) (res *databrokerpb.GetResponse, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).Get(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) List(
+	ctx context.Context,
+	req *registrypb.ListRequest,
+	opts ...grpc.CallOption,
+) (res *registrypb.ServiceList, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = registrypb.NewRegistryClient(cc).List(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) ListTypes(
+	ctx context.Context,
+	req *emptypb.Empty,
+	opts ...grpc.CallOption,
+) (res *databrokerpb.ListTypesResponse, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).ListTypes(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) Put(
+	ctx context.Context,
+	req *databrokerpb.PutRequest,
+	opts ...grpc.CallOption,
+) (res *databrokerpb.PutResponse, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).Put(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) Patch(
+	ctx context.Context,
+	req *databrokerpb.PatchRequest,
+	opts ...grpc.CallOption,
+) (res *databrokerpb.PatchResponse, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).Patch(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) Query(
+	ctx context.Context,
+	req *databrokerpb.QueryRequest,
+	opts ...grpc.CallOption,
+) (res *databrokerpb.QueryResponse, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).Query(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) ReleaseLease(
+	ctx context.Context,
+	req *databrokerpb.ReleaseLeaseRequest,
+	opts ...grpc.CallOption,
+) (res *emptypb.Empty, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).ReleaseLease(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) RenewLease(
+	ctx context.Context,
+	req *databrokerpb.RenewLeaseRequest,
+	opts ...grpc.CallOption,
+) (res *emptypb.Empty, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).RenewLease(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) Report(
+	ctx context.Context,
+	req *registrypb.RegisterRequest,
+	opts ...grpc.CallOption,
+) (res *registrypb.RegisterResponse, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = registrypb.NewRegistryClient(cc).Report(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) ServerInfo(
+	ctx context.Context,
+	req *emptypb.Empty,
+	opts ...grpc.CallOption,
+) (res *databrokerpb.ServerInfoResponse, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).ServerInfo(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) SetOptions(
+	ctx context.Context,
+	req *databrokerpb.SetOptionsRequest,
+	opts ...grpc.CallOption,
+) (res *databrokerpb.SetOptionsResponse, err error) {
+	return res, client.withClientConn(client.target, func(cc *grpc.ClientConn) error {
+		var err error
+		res, err = databrokerpb.NewDataBrokerServiceClient(cc).SetOptions(ctx, req, opts...)
+		return err
+	})
+}
+
+func (client clientPoolClient) Sync(
+	ctx context.Context,
+	req *databrokerpb.SyncRequest,
+	opts ...grpc.CallOption,
+) (stream grpc.ServerStreamingClient[databrokerpb.SyncResponse], err error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (client clientPoolClient) SyncLatest(
+	ctx context.Context,
+	req *databrokerpb.SyncLatestRequest,
+	opts ...grpc.CallOption,
+) (stream grpc.ServerStreamingClient[databrokerpb.SyncLatestResponse], err error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (client clientPoolClient) Watch(
+	ctx context.Context,
+	req *registrypb.ListRequest,
+	opts ...grpc.CallOption,
+) (stream grpc.ServerStreamingClient[registrypb.ServiceList], err error) {
+	panic("not implemented") // TODO: Implement
+}
--- internal/databroker/server_clustered.go
+++ internal/databroker/server_clustered.go
@@ -12,6 +12,7 @@ import (
 	"github.com/hashicorp/go-set/v3"
 	"github.com/rs/zerolog"
 	oteltrace "go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/pomerium/pomerium/config"
@@ -145,7 +146,7 @@ func (mapper *clusterTopologyMapper) Update(cfg *config.Config) {
 
 	remainingURLs := set.From(rawURLs)
 outer:
-	for i := 0; i < len(rawURLs); i++ {
+	for range len(rawURLs) {
 		result := <-results
 		if result.err != nil {
 			log.Ctx(ctx).Error().Err(result.err).Msg("error querying databroker for server info")
@@ -203,9 +204,6 @@ type clusteredServer struct {
 	cfg      *config.Config
 	leader   Server
 	leaderID uint64
-
-	databrokerpb.UnimplementedDataBrokerServiceServer
-	registrypb.UnimplementedRegistryServer
 }
 
 func NewClusteredServer(tracerProvider oteltrace.TracerProvider, local Server) Server {
@@ -216,8 +214,128 @@ func NewClusteredServer(tracerProvider oteltrace.TracerProvider, local Server) S
 	}
 }
 
+func (srv *clusteredServer) AcquireLease(ctx context.Context, req *databrokerpb.AcquireLeaseRequest) (res *databrokerpb.AcquireLeaseResponse, err error) {
+	err = srv.withReadWriteNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.AcquireLease(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) Get(ctx context.Context, req *databrokerpb.GetRequest) (res *databrokerpb.GetResponse, err error) {
+	err = srv.withReadOnlyNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.Get(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) List(ctx context.Context, req *registrypb.ListRequest) (res *registrypb.ServiceList, err error) {
+	err = srv.withReadOnlyNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.List(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) ListTypes(ctx context.Context, req *emptypb.Empty) (res *databrokerpb.ListTypesResponse, err error) {
+	err = srv.withReadOnlyNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.ListTypes(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) Patch(ctx context.Context, req *databrokerpb.PatchRequest) (res *databrokerpb.PatchResponse, err error) {
+	err = srv.withReadWriteNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.Patch(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) Put(ctx context.Context, req *databrokerpb.PutRequest) (res *databrokerpb.PutResponse, err error) {
+	err = srv.withReadWriteNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.Put(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) Query(ctx context.Context, req *databrokerpb.QueryRequest) (res *databrokerpb.QueryResponse, err error) {
+	err = srv.withReadOnlyNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.Query(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) ReleaseLease(ctx context.Context, req *databrokerpb.ReleaseLeaseRequest) (res *emptypb.Empty, err error) {
+	err = srv.withReadWriteNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.ReleaseLease(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) RenewLease(ctx context.Context, req *databrokerpb.RenewLeaseRequest) (res *emptypb.Empty, err error) {
+	err = srv.withReadWriteNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.RenewLease(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) Report(ctx context.Context, req *registrypb.RegisterRequest) (res *registrypb.RegisterResponse, err error) {
+	err = srv.withReadWriteNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.Report(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
 func (srv *clusteredServer) ServerInfo(ctx context.Context, req *emptypb.Empty) (res *databrokerpb.ServerInfoResponse, err error) {
-	err = srv.withReadOnlyNode(ctx, func(s Server) error {
+	err = srv.withReadOnlyNode(ctx, func(ctx context.Context, s Server) error {
 		var err error
 		res, err = s.ServerInfo(ctx, req)
 		return err
@@ -225,8 +343,37 @@ func (srv *clusteredServer) ServerInfo(ctx context.Context, req *emptypb.Empty) 
 	if err != nil {
 		return nil, err
 	}
-
 	return res, err
+}
+
+func (srv *clusteredServer) SetOptions(ctx context.Context, req *databrokerpb.SetOptionsRequest) (res *databrokerpb.SetOptionsResponse, err error) {
+	err = srv.withReadWriteNode(ctx, func(ctx context.Context, s Server) error {
+		var err error
+		res, err = s.SetOptions(ctx, req)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
+
+func (srv *clusteredServer) Sync(req *databrokerpb.SyncRequest, stream grpc.ServerStreamingServer[databrokerpb.SyncResponse]) error {
+	return srv.withReadOnlyNode(stream.Context(), func(ctx context.Context, s Server) error {
+		return s.Sync(req, newStreamWithContext(ctx, stream))
+	})
+}
+
+func (srv *clusteredServer) SyncLatest(req *databrokerpb.SyncLatestRequest, stream grpc.ServerStreamingServer[databrokerpb.SyncLatestResponse]) error {
+	return srv.withReadOnlyNode(stream.Context(), func(ctx context.Context, s Server) error {
+		return s.SyncLatest(req, newStreamWithContext(ctx, stream))
+	})
+}
+
+func (srv *clusteredServer) Watch(req *registrypb.ListRequest, stream grpc.ServerStreamingServer[registrypb.ServiceList]) error {
+	return srv.withReadOnlyNode(stream.Context(), func(ctx context.Context, s Server) error {
+		return s.Watch(req, newStreamWithContext(ctx, stream))
+	})
 }
 
 func (srv *clusteredServer) Stop() {
@@ -247,12 +394,12 @@ func (srv *clusteredServer) OnConfigChange(ctx context.Context, cfg *config.Conf
 	srv.mu.Unlock()
 }
 
-func (srv *clusteredServer) withReadOnlyNode(ctx context.Context, fn func(Server) error) error {
+func (srv *clusteredServer) withReadOnlyNode(ctx context.Context, fn func(ctx context.Context, srv Server) error) error {
 	mode := GetIncomingClusterRequestMode(ctx)
 
 	// for local mode we don't have to find the leader
 	if mode == ClusterRequestModeLocal {
-		return fn(srv.local)
+		return fn(ctx, srv.local)
 	}
 
 	// find the leader
@@ -265,11 +412,34 @@ func (srv *clusteredServer) withReadOnlyNode(ctx context.Context, fn func(Server
 
 	switch mode {
 	case ClusterRequestModeDefault:
-		return fn(leader)
+		return fn(WithOutgoingClusterRequestMode(ctx, ClusterRequestModeLeader), leader)
 	case ClusterRequestModeLeader:
 		// in leader mode we only allow calls if we're the leader
 		if leader == srv.local {
-			return fn(leader)
+			return fn(ctx, leader)
+		}
+		return databrokerpb.ErrNodeIsNotLeader
+	default:
+		return databrokerpb.ErrUnknownClusterRequestMode
+	}
+}
+
+func (srv *clusteredServer) withReadWriteNode(ctx context.Context, fn func(ctx context.Context, srv Server) error) error {
+	// find the leader
+	leader, err := srv.getLeader(ctx)
+	if err != nil {
+		return err
+	}
+
+	// leader is either a forwarding server or the local server
+
+	switch GetIncomingClusterRequestMode(ctx) {
+	case ClusterRequestModeDefault:
+		return fn(WithOutgoingClusterRequestMode(ctx, ClusterRequestModeLeader), leader)
+	case ClusterRequestModeLocal, ClusterRequestModeLeader:
+		// in leader and local modes we only allow calls if we're the leader
+		if leader == srv.local {
+			return fn(ctx, leader)
 		}
 		return databrokerpb.ErrNodeIsNotLeader
 	default:
@@ -317,4 +487,17 @@ func (srv *clusteredServer) getLeader(ctx context.Context) (Server, error) {
 			}
 		}
 	}
+}
+
+type streamWithContext[Res any] struct {
+	grpc.ServerStreamingServer[Res]
+	ctx context.Context
+}
+
+func newStreamWithContext[Res any](ctx context.Context, stream grpc.ServerStreamingServer[Res]) grpc.ServerStreamingServer[Res] {
+	return streamWithContext[Res]{stream, ctx}
+}
+
+func (stream streamWithContext[Res]) Context() context.Context {
+	return stream.ctx
 }
