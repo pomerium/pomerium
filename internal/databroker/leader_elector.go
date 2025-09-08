5968303062d44c3f678107790ad39243972539a2
Caleb Doxsey <caleb@doxsey.net>
2025-09-08 09:04:38 -0600

    wip

--- internal/databroker/leader_elector.go
+++ internal/databroker/leader_elector.go
@@ -1,7 +1,10 @@
 package databroker
 
 import (
+	"context"
 	"math/rand/v2"
+	"sync"
+	"time"
 
 	"github.com/volatiletech/null/v9"
 )
@@ -30,15 +33,21 @@ func (e *staticLeaderElector) Stop() {}
 type randomLeaderElector struct {
 	nodeIDs         []string
 	currentLeaderID null.String
+
+	closeCtx context.Context
+	close    context.CancelFunc
+
+	mu  sync.Mutex
+	fns []func(leaderID null.String)
 }
 
 func NewRandomLeaderElector(nodeIDs []string) LeaderElector {
 	e := &randomLeaderElector{
 		nodeIDs: nodeIDs,
 	}
-	if len(e.nodeIDs) > 0 {
-		e.currentLeaderID = null.StringFrom(e.nodeIDs[rand.IntN(len(e.nodeIDs))]) //nolint:gosec
-	}
+	e.closeCtx, e.close = context.WithCancel(context.Background())
+	e.pickRandomLeaderLocked()
+	go e.run()
 	return e
 }
 
@@ -46,4 +55,34 @@ func (e *randomLeaderElector) OnLeaderChange(fn func(leaderID null.String)) {
 	go fn(e.currentLeaderID)
 }
 
-func (e *randomLeaderElector) Stop() {}
+func (e *randomLeaderElector) Stop() {
+	e.close()
+}
+
+func (e *randomLeaderElector) run() {
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		e.mu.Lock()
+		e.pickRandomLeaderLocked()
+		for _, fn := range e.fns {
+			go fn(e.currentLeaderID)
+		}
+		e.mu.Unlock()
+
+		select {
+		case <-e.closeCtx.Done():
+			return
+		case <-ticker.C:
+		}
+	}
+}
+
+func (e *randomLeaderElector) pickRandomLeaderLocked() {
+	if len(e.nodeIDs) == 0 {
+		e.currentLeaderID = null.StringFromPtr(nil)
+	} else {
+		e.currentLeaderID = null.StringFrom(e.nodeIDs[rand.IntN(len(e.nodeIDs))]) //nolint:gosec
+	}
+}
