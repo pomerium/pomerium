b2eafedfb150066d79d8dd49297aa6920fa3e644
Caleb Doxsey <caleb@doxsey.net>
2025-09-12 15:06:37 -0600

    updates

--- internal/databroker/client.go
+++ internal/databroker/client.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"context"
 	"crypto/tls"
+	"fmt"
 	"net/url"
 	"strings"
 	"sync"
 
+	"github.com/rs/zerolog"
 	oteltrace "go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -15,47 +17,71 @@ import (
 
 	"github.com/pomerium/pomerium/config"
 	"github.com/pomerium/pomerium/internal/log"
+	"github.com/pomerium/pomerium/internal/telemetry"
 	"github.com/pomerium/pomerium/pkg/grpcutil"
 )
 
 // A ClientManager manages client connections for gRPC.
 type ClientManager struct {
+	telemetry telemetry.Component
 	grpcutil.ClientManager
 
 	mu        sync.RWMutex
 	sharedKey []byte
+	caPEM     []byte
 }
 
 // NewClientManager creates a new ClientManager.
 func NewClientManager(tracerProvider oteltrace.TracerProvider) *ClientManager {
 	return &ClientManager{
+		telemetry:     *telemetry.NewComponent(tracerProvider, zerolog.TraceLevel, "databroker-grpc-client-manager"),
 		ClientManager: grpcutil.NewClientManager(tracerProvider),
 	}
 }
 
 func (mgr *ClientManager) OnConfigChange(ctx context.Context, cfg *config.Config) {
+	ctx, op := mgr.telemetry.Start(ctx, "OnConfigChange")
+	defer op.Complete()
+
 	sharedKey, err := cfg.Options.GetSharedKey()
 	if err != nil {
-		log.Ctx(ctx).Error().Err(err).Msg("databroker-client-manager: error getting shared key")
+		log.Ctx(ctx).Error().Err(err).Msg("error getting shared key")
+		_ = op.Failure(err)
+		return
+	}
+
+	caPEM, err := cfg.AllCertificateAuthoritiesPEM()
+	if err != nil {
+		log.Ctx(ctx).Error().Err(err).Msg("error getting combined certificate authority pem")
+		_ = op.Failure(err)
 		return
 	}
 
 	mgr.mu.Lock()
 	defer mgr.mu.Unlock()
 
-	// if the shared key hasn't changed, there's not anything to update
-	if bytes.Equal(mgr.sharedKey, sharedKey) {
+	// if the shared key and the certificate authority hasn't changed,
+	// there's not anything to update
+	if bytes.Equal(mgr.sharedKey, sharedKey) &&
+		bytes.Equal(mgr.caPEM, caPEM) {
 		return
 	}
 	mgr.sharedKey = sharedKey
+	mgr.caPEM = caPEM
 
 	cfg = cfg.Clone()
 	mgr.UpdateOptions(grpcutil.WithClientManagerNewClient(func(target string, options ...grpc.DialOption) (*grpc.ClientConn, error) {
-		return NewClientForConfig(cfg, target, options...)
+		return mgr.NewClientForConfig(cfg, target, options...)
 	}))
 }
 
-func NewClientForConfig(cfg *config.Config, rawURL string, options ...grpc.DialOption) (*grpc.ClientConn, error) {
+// NewClientForConfig creates a new client for the given config.
+func (mgr *ClientManager) NewClientForConfig(cfg *config.Config, rawURL string, options ...grpc.DialOption) (*grpc.ClientConn, error) {
+	ctx, op := mgr.telemetry.Start(context.Background(), "NewClientForConfig")
+	defer op.Complete()
+
+	log.Ctx(ctx).Info().Msgf("connecting to %s", rawURL)
+
 	sharedKey, err := cfg.Options.GetSharedKey()
 	if err != nil {
 		return nil, err
@@ -90,8 +116,13 @@ func NewClientForConfig(cfg *config.Config, rawURL string, options ...grpc.DialO
 	if u.Scheme == "http" {
 		options = append(options, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	} else {
+		rootCAs, err := cfg.GetCertificatePool()
+		if err != nil {
+			return nil, fmt.Errorf("error loading certificate pool for gRPC connection")
+		}
 		options = append(options, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
-			InsecureSkipVerify: true,
+			RootCAs:    rootCAs,
+			MinVersion: tls.VersionTLS12,
 		})))
 	}
 
