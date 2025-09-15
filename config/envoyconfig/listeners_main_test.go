3e5fb5a4291329ce969948af70344b5b20697f14
Caleb Doxsey <caleb@doxsey.net>
2025-09-15 12:12:19 -0600

    envoyconfig: disable proxy protocol for the quic listener

--- config/envoyconfig/listeners_main_test.go
+++ config/envoyconfig/listeners_main_test.go
@@ -7,11 +7,13 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/pomerium/pomerium/config"
+	"github.com/pomerium/pomerium/config/envoyconfig/filemgr"
 	"github.com/pomerium/pomerium/internal/testutil"
+	"github.com/pomerium/pomerium/pkg/cryptutil"
 )
 
 func Test_requireProxyProtocol(t *testing.T) {
-	b := New("local-grpc", "local-http", "local-metrics", nil, nil, true)
+	b := New("local-grpc", "local-http", "local-metrics", filemgr.NewManager(), nil, true)
 	t.Run("required", func(t *testing.T) {
 		li, err := b.buildMainListener(t.Context(), &config.Config{Options: &config.Options{
 			UseProxyProtocol: true,
@@ -32,6 +34,15 @@ func Test_requireProxyProtocol(t *testing.T) {
 			UseProxyProtocol: false,
 			InsecureServer:   true,
 		}}, false, false)
+		require.NoError(t, err)
+		assert.Len(t, li.GetListenerFilters(), 0)
+	})
+	t.Run("disabled for quic", func(t *testing.T) {
+		li, err := b.buildMainListener(t.Context(), &config.Config{Options: &config.Options{
+			SharedKey:        cryptutil.NewBase64Key(),
+			UseProxyProtocol: true,
+			CodecType:        config.CodecTypeHTTP3,
+		}}, false, true)
 		require.NoError(t, err)
 		assert.Len(t, li.GetListenerFilters(), 0)
 	})
