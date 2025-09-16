815e6a2a38f0bda32d1671b5abf808519cc37cfe
Denis Mishin <dmishin@pomerium.com>
2025-09-16 11:20:02 -0400

    databroker/file: enforce strict permissions on files (0600) and directories (0700)

--- pkg/pebbleutil/pebbleutil.go
+++ pkg/pebbleutil/pebbleutil.go
@@ -2,7 +2,9 @@ package pebbleutil
 
 import (
 	"context"
+	"fmt"
 	"iter"
+	"os"
 	"slices"
 
 	"github.com/cockroachdb/pebble/v2"
@@ -90,6 +92,9 @@ func Open(dirname string, options *pebble.Options) (*pebble.DB, error) {
 		options = new(pebble.Options)
 	}
 	options.LoggerAndTracer = pebbleLogger{}
+	if options.FS == nil {
+		options.FS = secureFS{FS: vfs.Default}
+	}
 	if options.Levels == nil {
 		options.Levels = []pebble.LevelOptions{{Compression: func() pebble.Compression {
 			return pebble.NoCompression
@@ -118,3 +123,37 @@ func (pebbleLogger) Errorf(_ string, _ ...any)                    {}
 func (pebbleLogger) Fatalf(_ string, _ ...any)                    {}
 func (pebbleLogger) Eventf(_ context.Context, _ string, _ ...any) {}
 func (pebbleLogger) IsTracingEnabled(_ context.Context) bool      { return false }
+
+// enforce strict permissions on files (0600) and directories (0700)
+type secureFS struct{ vfs.FS }
+
+func (s secureFS) Create(name string, category vfs.DiskWriteCategory) (vfs.File, error) {
+	f, err := s.FS.Create(name, category)
+	if err != nil {
+		return nil, fmt.Errorf("create %q: %w", name, err)
+	}
+	err = os.Chmod(name, 0o600)
+	if err != nil {
+		_ = f.Close()
+		_ = os.Remove(name)
+		return nil, fmt.Errorf("chmod %q: %w", name, err)
+	}
+	return f, nil
+}
+
+func (s secureFS) MkdirAll(path string, _ os.FileMode) error {
+	return s.FS.MkdirAll(path, 0o700)
+}
+
+func (s secureFS) ReuseForWrite(name, oldname string, category vfs.DiskWriteCategory) (vfs.File, error) {
+	f, err := s.FS.ReuseForWrite(name, oldname, category)
+	if err != nil {
+		return nil, err
+	}
+	err = os.Chmod(name, 0o600)
+	if err != nil {
+		_ = f.Close()
+		return nil, fmt.Errorf("chmod %q: %w", name, err)
+	}
+	return f, nil
+}
