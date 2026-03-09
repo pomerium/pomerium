6033a4b2cf48b28ca98eab60ad1200f6d1bf5364
Joe Kralicky <joekralicky@gmail.com>
2026-03-09 22:11:10 +0000

    update get-envoy tool to fetch binaries from oci images

--- pkg/envoy/envoyversion/envoyversion.go
+++ pkg/envoy/envoyversion/envoyversion.go
@@ -9,6 +9,31 @@ import (
 	_ "github.com/pomerium/envoy-custom/api/extensions/filters/network/ssh" // to get version info
 )
 
+func PseudoVersionTimeRev() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return ""
+	}
+
+	var modVersion string
+	for _, dep := range info.Deps {
+		if dep.Path == "github.com/pomerium/envoy-custom" {
+			if dep.Replace != nil {
+				modVersion = dep.Replace.Version
+			} else {
+				modVersion = dep.Version
+			}
+			break
+		}
+	}
+	if module.IsPseudoVersion(modVersion) {
+		t, _ := module.PseudoVersionTime(modVersion)
+		rev, _ := module.PseudoVersionRev(modVersion)
+		return t.UTC().Format(module.PseudoVersionTimestampFormat) + "-" + rev
+	}
+	return ""
+}
+
 // Version returns the envoy version.
 func Version() string {
 	info, ok := debug.ReadBuildInfo()
