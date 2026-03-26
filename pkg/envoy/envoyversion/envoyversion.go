15e81cc8c41409aca3082c654ccbf234e711856d
Joe Kralicky <joekralicky@gmail.com>
2026-03-26 21:42:40 +0000

    handle tagged module versions

--- pkg/envoy/envoyversion/envoyversion.go
+++ pkg/envoy/envoyversion/envoyversion.go
@@ -2,37 +2,11 @@ package envoyversion
 
 import (
 	"runtime/debug"
-	"strings"
 
 	"golang.org/x/mod/module"
 
 	_ "github.com/pomerium/envoy-custom/api/extensions/filters/network/ssh" // to get version info
 )
-
-func PseudoVersionTimeRev() string {
-	info, ok := debug.ReadBuildInfo()
-	if !ok {
-		return ""
-	}
-
-	var modVersion string
-	for _, dep := range info.Deps {
-		if dep.Path == "github.com/pomerium/envoy-custom" {
-			if dep.Replace != nil {
-				modVersion = dep.Replace.Version
-			} else {
-				modVersion = dep.Version
-			}
-			break
-		}
-	}
-	if module.IsPseudoVersion(modVersion) {
-		t, _ := module.PseudoVersionTime(modVersion)
-		rev, _ := module.PseudoVersionRev(modVersion)
-		return t.UTC().Format(module.PseudoVersionTimestampFormat) + "-" + rev
-	}
-	return ""
-}
 
 // Version returns the envoy version.
 func Version() string {
@@ -53,11 +27,14 @@ func Version() string {
 		}
 	}
 
+	// If the version is tagged, the corresponding image will have that exact tag.
+	// If the version is a pseudo-version, the image tag will exactly match the
+	// "yyyymmddhhmmss-abcdef123456" part of the pseudo-version string.
 	if module.IsPseudoVersion(modVersion) {
-		if base, err := module.PseudoVersionBase(modVersion); err == nil {
-			modVersion = base
-		}
+		t, _ := module.PseudoVersionTime(modVersion)
+		rev, _ := module.PseudoVersionRev(modVersion)
+		return t.UTC().Format(module.PseudoVersionTimestampFormat) + "-" + rev
 	}
 
-	return strings.TrimPrefix(modVersion, "v")
+	return modVersion
 }
