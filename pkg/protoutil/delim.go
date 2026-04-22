e1a98fd6067f827885f83faa8a9aa13011e61ba5
Alexandre Lamarre <alamarre@pomerium.com>
2026-04-21 20:18:51 -0400

    proto length marshal delimited use built in `protodelim` library
    use bytes.Cut (index) instead of bufio.Scanner, which will fail on large lines
    
    force no randomness in protojson encoding for delim_test.go
    
    use protojson.MarshalAppend in MarshalNewLineDelimitedProtoJSON

--- pkg/protoutil/delim.go
+++ pkg/protoutil/delim.go
@@ -1,17 +1,15 @@
 package protoutil
 
 import (
-	"bufio"
 	"bytes"
-	"fmt"
 	"reflect"
 
+	"google.golang.org/protobuf/encoding/protodelim"
 	"google.golang.org/protobuf/encoding/protojson"
-	"google.golang.org/protobuf/encoding/protowire"
 	"google.golang.org/protobuf/proto"
 )
 
-func MarshalLenghDelimitedProtojson[T proto.Message](msgs []T) ([]byte, error) {
+func MarshalNewLineDelimitedProtoJSON[T proto.Message](msgs []T) ([]byte, error) {
 	var buf []byte
 	opts := protojson.MarshalOptions{
 		Multiline:         false,
@@ -23,21 +21,25 @@ func MarshalLenghDelimitedProtojson[T proto.Message](msgs []T) ([]byte, error) {
 		EmitDefaultValues: true,
 	}
 	for _, msg := range msgs {
-		data, err := opts.Marshal(msg)
+		var err error
+		buf, err = opts.MarshalAppend(buf, msg)
 		if err != nil {
 			return nil, err
 		}
-		buf = append(buf, data...)
-		buf = append(buf, []byte("\n")...)
+		buf = append(buf, '\n')
 	}
 	return buf, nil
 }
 
-func UnmarshalLengthDelimitedProtojson[T proto.Message](data []byte) ([]T, error) {
-	s := bufio.NewScanner(bytes.NewReader(data))
+func UnmarshalNewLineDelimitedProtoJSON[T proto.Message](data []byte) ([]T, error) {
 	var msgs []T
-	for s.Scan() {
-		line := s.Bytes()
+	for len(data) > 0 {
+		var line []byte
+		if before, after, ok := bytes.Cut(data, []byte{'\n'}); ok {
+			line, data = before, after
+		} else {
+			line, data = data, nil
+		}
 		if len(bytes.TrimSpace(line)) == 0 {
 			continue
 		}
@@ -53,37 +55,26 @@ func UnmarshalLengthDelimitedProtojson[T proto.Message](data []byte) ([]T, error
 // MarshalLengthDelimited encodes a slice of proto messages into a single byte
 // buffer using varint-length-delimited framing.
 func MarshalLengthDelimited[T proto.Message](msgs []T) ([]byte, error) {
-	var buf []byte
+	var w bytes.Buffer
 	for _, msg := range msgs {
-		data, err := proto.Marshal(msg)
-		if err != nil {
+		if _, err := protodelim.MarshalTo(&w, msg); err != nil {
 			return nil, err
 		}
-		buf = protowire.AppendVarint(buf, uint64(len(data)))
-		buf = append(buf, data...)
 	}
-	return buf, nil
+	return w.Bytes(), nil
 }
 
 // UnmarshalLengthDelimited decodes a varint-length-delimited byte buffer into
 // a slice of proto messages.
 func UnmarshalLengthDelimited[T proto.Message](buf []byte) ([]T, error) {
 	var msgs []T
-	for len(buf) > 0 {
-		size, n := protowire.ConsumeVarint(buf)
-		if n < 0 {
-			return nil, fmt.Errorf("invalid varint in length-delimited stream")
-		}
-		buf = buf[n:]
-		if uint64(len(buf)) < size {
-			return nil, fmt.Errorf("truncated message: need %d bytes, have %d", size, len(buf))
-		}
+	r := bytes.NewReader(buf)
+	for r.Len() > 0 {
 		msg := newProtoMessage[T]()
-		if err := proto.Unmarshal(buf[:size], msg); err != nil {
+		if err := protodelim.UnmarshalFrom(r, msg); err != nil {
 			return nil, err
 		}
 		msgs = append(msgs, msg)
-		buf = buf[size:]
 	}
 	return msgs, nil
 }
--- pkg/protoutil/delim_test.go
+++ pkg/protoutil/delim_test.go
@@ -2,6 +2,7 @@ package protoutil_test
 
 import (
 	"testing"
+	_ "unsafe"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
@@ -11,6 +12,16 @@ import (
 	"github.com/pomerium/pomerium/pkg/grpc/testproto"
 	"github.com/pomerium/pomerium/pkg/protoutil"
 )
+
+//go:linkname protobufRandSeed google.golang.org/protobuf/internal/detrand.randSeed
+var protobufRandSeed int
+
+// Forces the protojson format to be encoded in stable manner for testing:
+//
+//	https://github.com/golang/protobuf/issues/1121
+func init() {
+	protobufRandSeed = 0
+}
 
 func TestProtoDelim(t *testing.T) {
 	msgs := []*testproto.Test{
@@ -27,7 +38,6 @@ func TestProtoDelim(t *testing.T) {
 			},
 		},
 	}
-
 	data, err := protoutil.MarshalLengthDelimited(msgs)
 	require.NoError(t, err)
 
@@ -52,15 +62,16 @@ func TestProtoJSON(t *testing.T) {
 		},
 	}
 
-	data, err := protoutil.MarshalLenghDelimitedProtojson(msgs)
+	data, err := protoutil.MarshalNewLineDelimitedProtoJSON(msgs)
 	require.NoError(t, err)
+
+	retmsgs, err := protoutil.UnmarshalNewLineDelimitedProtoJSON[*testproto.Test](data)
+	require.NoError(t, err)
+	assert.Empty(t, cmp.Diff(msgs, retmsgs, protocmp.Transform()))
+
 	// should look like ndjson
 	assert.Equal(t, string(data),
 		`{"string_field":"foo","proto_field":{"another_string_field":"bar"}}
 {"string_field":"bar","proto_field":{"another_string_field":"foo"}}
 `)
-
-	retmsgs, err := protoutil.UnmarshalLengthDelimitedProtojson[*testproto.Test](data)
-	require.NoError(t, err)
-	assert.Empty(t, cmp.Diff(msgs, retmsgs, protocmp.Transform()))
 }
