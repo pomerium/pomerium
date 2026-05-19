483510c3dce5f032bdfd418b14bba7023e83ad5c
Alexandre Lamarre <alamarre@pomerium.com>
2026-05-19 10:22:00 -0400

    fix: darwin builds

--- pkg/envoy/misc.go
+++ pkg/envoy/misc.go
@@ -1,5 +1,19 @@
 package envoy
 
+import (
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+func marshalAny(msg proto.Message) *anypb.Any {
+	data := new(anypb.Any)
+	_ = anypb.MarshalFrom(data, msg, proto.MarshalOptions{
+		AllowPartial:  true,
+		Deterministic: true,
+	})
+	return data
+}
+
 func firstNonEmpty[T interface{ ~string }](args ...T) T {
 	for _, a := range args {
 		if a != "" {
