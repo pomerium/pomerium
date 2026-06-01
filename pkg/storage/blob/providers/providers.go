94c9f034b33597da8b7d6ccfd8936d6fb9457f64
Alexandre Lamarre <alamarre@pomerium.com>
2026-06-01 11:32:10 -0400

    feat(blob/gcs): custom credentials loader

--- pkg/storage/blob/providers/providers.go
+++ pkg/storage/blob/providers/providers.go
@@ -17,6 +17,47 @@ import (
 	_ "gocloud.dev/blob/s3blob"
 )
 
+type Provider struct {
+	credentialLoaders []CredentialsLoader
+}
+
+func NewProvider(credentialLoaders []CredentialsLoader) Provider {
+	return Provider{
+		credentialLoaders: credentialLoaders,
+	}
+}
+
+func (p Provider) OpenBucket(ctx context.Context, bucketURI string) (*blob.Bucket, error) {
+	if bucketURI == "" {
+		return nil, fmt.Errorf("blob storage bucket URI is not set")
+	}
+
+	u, err := url.Parse(bucketURI)
+	if err != nil {
+		return nil, fmt.Errorf("parse bucket URI: %w", err)
+	}
+
+	switch u.Scheme {
+	case "file":
+		q := u.Query()
+		if !q.Has("no_tmp_dir") {
+			q.Set("no_tmp_dir", "true")
+			u.RawQuery = q.Encode()
+			bucketURI = u.String()
+		}
+	case "azblob":
+		// TODO :
+	case "s3":
+		// TODO :
+	case "gs":
+		bucketOpener := &gcsCredsWrapper{
+			credentialLoaders: p.credentialLoaders,
+		}
+		return bucketOpener.OpenBucketURL(ctx, u)
+	}
+	return blob.OpenBucket(ctx, bucketURI)
+}
+
 // OpenBucket creates a *blob.Bucket from a blob storage URI
 // (e.g. "s3://bucket", "gs://bucket", "azblob://container").
 //
