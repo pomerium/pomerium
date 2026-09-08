f701218a8cb2f4eef2222e4aa5b6c98ebef6b622
Joe Kralicky <joekralicky@gmail.com>
2026-09-08 18:01:34 +0000

    Revert "rename UpdateAuthorizedRoutes to UpdateTunnelAuthorizedRoutes"
    This reverts commit 9a592bb9830e4a8755862e08be133443fdcad75f.

--- pkg/ssh/policy_index_inmemory.go
+++ pkg/ssh/policy_index_inmemory.go
@@ -282,7 +282,7 @@ func newKnownSession(ctx context.Context, record *session.Session) *knownSession
 		Record: record, // can be nil
 		tunnelAuthorizedRoutesCache: newAuthorizedRoutesCache(ctx, "upstream tunnel",
 			SSHEvaluator.EvaluateUpstreamTunnel,
-			PolicyIndexSubscriber.UpdateTunnelAuthorizedRoutes,
+			PolicyIndexSubscriber.UpdateAuthorizedRoutes,
 			func(i *inMemoryIndexerState) []knownRoute { return i.AllTunnelEnabledRoutes }),
 	}
 }
