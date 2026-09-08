70ecc7b81f32030091d86866af8b650ab60926fd
Joe Kralicky <joekralicky@gmail.com>
2026-09-08 18:01:34 +0000

    Revert "rename UpdateAuthorizedRoutes to UpdateTunnelAuthorizedRoutes"
    This reverts commit 9a592bb9830e4a8755862e08be133443fdcad75f.

--- pkg/ssh/mock/mock_policy_index.go
+++ pkg/ssh/mock/mock_policy_index.go
@@ -40,6 +40,42 @@ func (m *MockPolicyIndexSubscriber) EXPECT() *MockPolicyIndexSubscriberMockRecor
 	return m.recorder
 }
 
+// UpdateAuthorizedRoutes mocks base method.
+func (m *MockPolicyIndexSubscriber) UpdateAuthorizedRoutes(routes []common.RouteInfo) {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "UpdateAuthorizedRoutes", routes)
+}
+
+// UpdateAuthorizedRoutes indicates an expected call of UpdateAuthorizedRoutes.
+func (mr *MockPolicyIndexSubscriberMockRecorder) UpdateAuthorizedRoutes(routes any) *MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall {
+	mr.mock.ctrl.T.Helper()
+	call := mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateAuthorizedRoutes", reflect.TypeOf((*MockPolicyIndexSubscriber)(nil).UpdateAuthorizedRoutes), routes)
+	return &MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall{Call: call}
+}
+
+// MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall wrap *gomock.Call
+type MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall struct {
+	*gomock.Call
+}
+
+// Return rewrite *gomock.Call.Return
+func (c *MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall) Return() *MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall {
+	c.Call = c.Call.Return()
+	return c
+}
+
+// Do rewrite *gomock.Call.Do
+func (c *MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall) Do(f func([]common.RouteInfo)) *MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall {
+	c.Call = c.Call.Do(f)
+	return c
+}
+
+// DoAndReturn rewrite *gomock.Call.DoAndReturn
+func (c *MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall) DoAndReturn(f func([]common.RouteInfo)) *MockPolicyIndexSubscriberUpdateAuthorizedRoutesCall {
+	c.Call = c.Call.DoAndReturn(f)
+	return c
+}
+
 // UpdateEnabledStaticPorts mocks base method.
 func (m *MockPolicyIndexSubscriber) UpdateEnabledStaticPorts(allowedStaticPorts []uint) {
 	m.ctrl.T.Helper()
@@ -72,42 +108,6 @@ func (c *MockPolicyIndexSubscriberUpdateEnabledStaticPortsCall) Do(f func([]uint
 
 // DoAndReturn rewrite *gomock.Call.DoAndReturn
 func (c *MockPolicyIndexSubscriberUpdateEnabledStaticPortsCall) DoAndReturn(f func([]uint)) *MockPolicyIndexSubscriberUpdateEnabledStaticPortsCall {
-	c.Call = c.Call.DoAndReturn(f)
-	return c
-}
-
-// UpdateTunnelAuthorizedRoutes mocks base method.
-func (m *MockPolicyIndexSubscriber) UpdateTunnelAuthorizedRoutes(routes []common.RouteInfo) {
-	m.ctrl.T.Helper()
-	m.ctrl.Call(m, "UpdateTunnelAuthorizedRoutes", routes)
-}
-
-// UpdateTunnelAuthorizedRoutes indicates an expected call of UpdateTunnelAuthorizedRoutes.
-func (mr *MockPolicyIndexSubscriberMockRecorder) UpdateTunnelAuthorizedRoutes(routes any) *MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall {
-	mr.mock.ctrl.T.Helper()
-	call := mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateTunnelAuthorizedRoutes", reflect.TypeOf((*MockPolicyIndexSubscriber)(nil).UpdateTunnelAuthorizedRoutes), routes)
-	return &MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall{Call: call}
-}
-
-// MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall wrap *gomock.Call
-type MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall struct {
-	*gomock.Call
-}
-
-// Return rewrite *gomock.Call.Return
-func (c *MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall) Return() *MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall {
-	c.Call = c.Call.Return()
-	return c
-}
-
-// Do rewrite *gomock.Call.Do
-func (c *MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall) Do(f func([]common.RouteInfo)) *MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall {
-	c.Call = c.Call.Do(f)
-	return c
-}
-
-// DoAndReturn rewrite *gomock.Call.DoAndReturn
-func (c *MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall) DoAndReturn(f func([]common.RouteInfo)) *MockPolicyIndexSubscriberUpdateTunnelAuthorizedRoutesCall {
 	c.Call = c.Call.DoAndReturn(f)
 	return c
 }
