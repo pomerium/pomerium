b00caa57526cdc61951c9074583637a035563291
Joe Kralicky <joekralicky@gmail.com>
2026-09-08 21:15:32 +0000

    remove ssh_access_request_not_approved criteria
    The ssh_access_request_not_approved criteria is redundant, because it is
    equivalent to ssh_access_request_approved used in a not/nor block
    instead.

--- pkg/policy/criteria/ssh_access_request.go
+++ pkg/policy/criteria/ssh_access_request.go
@@ -12,32 +12,6 @@ var accessRequestApprovedBody = ast.Body{
 	ast.MustParseExpr(`input.ssh.access_request_approved`),
 }
 
-type sshAccessRequestNotApprovedCriterion struct {
-	g *Generator
-}
-
-func (sshAccessRequestNotApprovedCriterion) DataType() generator.CriterionDataType {
-	return generator.CriterionDataTypeUnused
-}
-
-func (sshAccessRequestNotApprovedCriterion) Name() string {
-	return "ssh_access_request_not_approved"
-}
-
-func (c sshAccessRequestNotApprovedCriterion) GenerateRule(_ string, _ parser.Value) (*ast.Rule, []*ast.Rule, error) {
-	r1 := c.g.NewRule(c.Name())
-	r1.Head.Value = NewCriterionTerm(false, ReasonSSHAccessRequestOK)
-	r1.Body = accessRequestApprovedBody
-	r1.Else = &ast.Rule{
-		Head: generator.NewHead("", NewCriterionTerm(true, ReasonSSHAccessRequestRequired)),
-	}
-	return r1, nil, nil
-}
-
-func SSHAccessRequestNotApprovedCriterion(generator *Generator) Criterion {
-	return sshAccessRequestNotApprovedCriterion{g: generator}
-}
-
 type sshAccessRequestApprovedCriterion struct {
 	g *Generator
 }
@@ -65,6 +39,5 @@ func SSHAccessRequestApprovedCriterion(generator *Generator) Criterion {
 }
 
 func init() {
-	Register(SSHAccessRequestNotApprovedCriterion)
 	Register(SSHAccessRequestApprovedCriterion)
 }
